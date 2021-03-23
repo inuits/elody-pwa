25e6e5bfbdae0dee92aab2decc34160599f418ea
Jeroen Wouters <jeroenwouters@inuits.eu>
2021-03-23 15:40:26 +0100

    add single view

---
+++ src/helpers.ts
@@ -0,0 +1,13 @@
+import { RouteLocationNormalizedLoaded } from "vue-router"
+
+export const getRouteParams = (
+  route: RouteLocationNormalizedLoaded,
+  key: string
+): string => {
+  if (Array.isArray(route.params[key])) {
+    return route.params[key][0]
+  } else {
+    const routeParam: string = route.params[key] as string
+    return routeParam
+  }
+}
