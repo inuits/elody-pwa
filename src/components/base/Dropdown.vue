9a5c9bf1a08d5b5c69ea1cf8f813543a3e77bb1c
jonasvanhoof <jonasvanhoof@inuits.eu>
2021-08-13 15:58:03 +0200

    #80605  Created base dropdown component

---
+++ src/components/base/Dropdown.vue
@@ -0,0 +1,55 @@
+<template>
+  <label class="block text-sm text-gray-700 ml-3">Items</label>
+  <div class="block flex justify-center ml-3">
+    <select class="w-48 rounded h-12" @click="toggleMenu">
+      <option
+        v-for="option in options"
+        :key="option"
+        value="option"
+        @click="setSelectedItem(option)"
+      >
+        {{ option }}
+      </option>
+    </select>
+  </div>
+</template>
+<script lang="ts">
+  import { defineComponent, PropType, reactive } from 'vue';
+  import { Unicons } from '@/types';
+
+  export default defineComponent({
+    name: 'Dropdown',
+    props: {
+      options: {
+        type: Array as PropType<String[]>,
+        required: true,
+        default: undefined,
+      },
+      selected: {
+        type: String,
+        required: false,
+        default: undefined,
+      },
+    },
+    emits: ['update:selected'],
+    setup(props, { emit }) {
+      const state = reactive({
+        isOpen: false,
+        options: props.options,
+        selected: '4',
+      });
+
+      function toggleMenu() {
+        state.isOpen = !state.isOpen;
+      }
+
+      function setSelectedItem(option: string) {
+        state.selected = option;
+        emit('update:selected', state.selected);
+        toggleMenu;
+      }
+
+      return { Unicons, state, props, toggleMenu, setSelectedItem };
+    },
+  });
+</script>
