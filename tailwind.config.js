759e8303dba2abe971118ba50769a200b8270fcc
jonasvanhoof <jonasvanhoof@inuits.eu>
2021-08-17 13:11:28 +0200

    Feedback on #80603 Added styling for the base components like the given design. All field for search,filter and items/page are displayed.

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,7 +39,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+  plugins: [require('@tailwindcss/forms')({ useFormClasses: true })],
 };
