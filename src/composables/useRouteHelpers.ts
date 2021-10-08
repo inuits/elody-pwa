736dd0789a1d5202c98f8f4f83a0db2bce0efc69
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2021-10-08 14:30:00 +0200

    #80025 Pagination is set and get from url for HISTORY  You can set page=1 and items=20 in the url  Couldn't check forthe asset library...

--- src/composables/useRouteHelpers.ts
+++ src/composables/useRouteHelpers.ts
@@ -2,15 +2,20 @@ import { useRoute } from 'vue-router';
 import { computed, ComputedRef } from 'vue';
 import { urlParams } from '@/views';
 import { asString } from '@/helpers';
+import { PaginationInfo } from "@/queries";
+import { useRouter } from "vue-router";
 
-export type noId = 'NOID';
+export type noId = "NOID";
 
 const useRouteHelpers = (): {
   getParam: (input: urlParams) => string | noId;
   isSingle: ComputedRef<boolean>;
+  getPaginationInfoFromUrl: (info: PaginationInfo) => PaginationInfo;
+  updatePaginationInfoQueryParams: (info: PaginationInfo) => void;
 } => {
+  const router = useRouter();
   const route = useRoute();
-  const isSingle = computed<boolean>(() => route.name === 'SingleEntity');
+  const isSingle = computed<boolean>(() => route.name === "SingleEntity");
 
   const getParam = (input: urlParams) => {
     const param = useRoute().params[input];
@@ -18,13 +23,30 @@ const useRouteHelpers = (): {
     if (param) {
       return asString(param);
     } else {
-      return 'NOID';
+      return "NOID";
     }
+  };
+
+  const getPaginationInfoFromUrl = (info: PaginationInfo) => {
+    if (route.query.page || route.query.items) {
+      info.skip = Number(route.query.page);
+      info.limit = Number(route.query.items);
+    }
+    return { limit: info.limit, skip: info.skip } as PaginationInfo;
+  };
+
+  const updatePaginationInfoQueryParams = (info: PaginationInfo) => {
+    if (info.skip == 0) {
+      router.replace({ query: { items: info.limit, page: info.skip + 1 } });
+    }
+    router.replace({ query: { items: info.limit, page: info.skip } });
   };
 
   return {
     getParam,
     isSingle,
+    getPaginationInfoFromUrl,
+    updatePaginationInfoQueryParams,
   };
 };
 
