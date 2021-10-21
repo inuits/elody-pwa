ccfb4e130c65f2439607a4d9c991c9758f3b288d
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2021-10-21 08:16:57 +0200

    Running frontend with docker-compose script
     NOTE: When running the `build-run-container.sh` script it gives an npm warning
    
     #11 10.71 npm WARN tar ENOENT: no such file or directory, lstat '/app/node_modules/.staging/typescript-2d0ca0bc/loc'

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e
+
+echo "Starting dashboard"
+npm link ./session-vue-3-oidc-library &&
+exec npm run serve
---
+++ scripts/build-dev-container.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -a
+
+__DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+DOCKER_BUILDKIT=1
+
+DOCKER=docker
+if [ -x "$(command -v podman)" ]; then
+  DOCKER=podman
+fi
+
+if [[ -f "${1:-.env.local}" ]]; then
+  echo "Using '${1:-.env.local}' as .env"
+  . ${1:-.env.local}
+else
+  echo "Missing ${1:-.env.local} file, provide valid one as argument"
+  exit 1
+fi
+
+${DOCKER} build -f docker/Dockerfile -t inuits-dams-frontend:dev --target=development-stage --build-arg NPM_AUTH_TOKEN=${NPM_AUTH_TOKEN} $__DIR $@
