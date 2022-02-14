51d53caad3a0259f81da11a0476e14d8184ddd85
Yens Van Hout <bulletje@inuits.eu>
2022-02-14 16:45:43 +0100

    Fixed some styling + added accordion

---
+++ src/components/base/FilterAccordion.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="bg-neutral-10">
+    <button
+      class="
+        flex
+        items-center
+        space-x-3
+        border-solid border-b-2 border-neutral-30
+        w-full
+        justify-between
+        px-3
+        py-5
+      "
+      @click="toggleAccordion"
+    >
+      <slot name="title" />
+      <p>
+        <span v-show="isOpen" class="text-2xl">-</span>
+        <span v-show="!isOpen" class="text-2xl">+</span>
+      </p>
+    </button>
+
+    <div v-show="isOpen" class="border-solid border-b-2 border-neutral-30 px-3 py-5">
+      <slot name="content" />
+    </div>
+  </div>
+</template>
+<script>
+  import { defineComponent, ref } from 'vue';
+  export default defineComponent({
+    name: 'FilterAccordion',
+    setup() {
+      const isOpen = ref(false);
+      const toggleAccordion = () => {
+        isOpen.value = !isOpen.value;
+      };
+      return { isOpen, toggleAccordion };
+    },
+  });
+</script>
