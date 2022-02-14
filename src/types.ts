8f5f3a7fe2ce552d46fac4c8c2bfb6f11702b534
joeydg <joeydg@inuits.eu>
2022-02-14 16:23:31 +0100

    functional togglebuttons for drawer component

--- src/types.ts
+++ src/types.ts
@@ -24,6 +24,7 @@ import {
   uniSortAmountDown,
   uniSortAmountUp,
   uniPlus,
+  uniFilter,
 } from 'vue-unicons/dist/icons';
 
 export enum Unicons {
@@ -52,4 +53,5 @@ export enum Unicons {
   Trash = uniTrashAlt,
   SortUp = uniSortAmountUp,
   SortDown = uniSortAmountDown,
+  Filter = uniFilter,
 }
