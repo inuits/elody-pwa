ab6ad840ca26124e87aca68572998596867add8f
sayansuresh <sayan.suresh@student.ap.be>
2022-04-25 12:23:55 +0200

    Refactor code in main.ts and tracer.ts

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,6 @@ const { GraphQLCodegenWebpackPlugin } = require('graphql-codegen-webpack-plugin'
 module.exports = {
   devServer: {
     allowedHosts: 'all',
-    // proxy: 'http://otel-collector.dams.localhost:8100/v1/traces', 
-    // proxy: 'http://localhost:4318/v1/traces', 
   },
   configureWebpack: {
     plugins: [new GraphQLCodegenWebpackPlugin({ configPath: './codegen.yml' })],
