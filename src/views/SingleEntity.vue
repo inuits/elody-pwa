3814c5f502115de9158da7643dcf793ff51365e1
Joey De Greef <joeydg@inuits.eu>
2022-05-03 13:39:04 +0000

    graphql error for not loading assetsfixed

--- src/views/SingleEntity.vue
+++ src/views/SingleEntity.vue
@@ -16,7 +16,7 @@
         v-if="
           !loading &&
           selectedMediafile !== null &&
-          selectedMediafile?.mimetype.includes('video')
+          selectedMediafile.mimetype.includes('video')
         "
         :source="selectedMediafile"
       />
@@ -24,7 +24,7 @@
         v-if="
           !loading &&
           selectedMediafile !== null &&
-          selectedMediafile?.mimetype.includes('audio')
+          selectedMediafile.mimetype.includes('audio')
         "
         :source="selectedMediafile"
       />
@@ -32,7 +32,7 @@
         v-if="
           !loading &&
           selectedMediafile !== null &&
-          selectedMediafile?.mimetype.includes('pdf')
+          selectedMediafile.mimetype.includes('pdf')
         "
         :source="selectedMediafile"
       />
@@ -40,13 +40,10 @@
         v-if="
           !loading &&
           selectedMediafile !== null &&
-          selectedMediafile?.mimetype.includes('image') &&
-          !selectedMediafile?.mimetype.includes('video') &&
-          !selectedMediafile?.mimetype.includes('audio') &&
-          !selectedMediafile?.mimetype.includes('application')
+          selectedMediafile.mimetype.includes('image')
         "
-        :image-url="selectedMediafile?.filename"
-        :image-meta-data="selectedMediafile?.metadata"
+        :image-url="selectedMediafile.filename"
+        :image-meta-data="selectedMediafile.metadata"
       />
     </div>
     <!-- meta is metadata form-->
