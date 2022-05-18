13987382fd2e4a06603bd70c3509f5d319c41196
sayansuresh <sayan.suresh@student.ap.be>
2022-05-18 22:10:45 +0200

    env OTEL_IS_DISABLED added

--- src/otel/tracer.ts
+++ src/otel/tracer.ts
@@ -3,20 +3,22 @@ import { ZoneContextManager } from '@opentelemetry/context-zone';
 import { ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { ParentBasedSampler, TraceIdRatioBasedSampler } from '@opentelemetry/core';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { XMLHttpRequestInstrumentation } from '@opentelemetry/instrumentation-xml-http-request';
+import { UserInteractionInstrumentation } from '@opentelemetry/instrumentation-user-interaction';
+import { FetchInstrumentation } from '@opentelemetry/instrumentation-fetch';
+
 // Plugins
 import { Resource } from '@opentelemetry/resources';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
-import { Sampler } from '@opentelemetry/api';
 
-import { environment as env} from '../environment';
-
-const init = function (serviceName: string, sampler: Sampler | undefined, spanProcessor: any) {
+const init = function (isTraceDisabled: string, serviceName: string, otlpHost: string, otlpPort: string) {
+    if (isTraceDisabled == "true") return;
 
     const otelExporter = new OTLPTraceExporter({
-        url: `http://${env.otlp.host}:${env.otlp.port}/v1/traces`, 
-        // headers: {
-        //     "content-type": "application/json"
-        // }
+        url: `http://${otlpHost}:${otlpPort}/v1/traces`, 
         headers: {},
     });
 
@@ -24,18 +26,29 @@ const init = function (serviceName: string, sampler: Sampler | undefined, spanPr
         resource: new Resource({
             [SemanticResourceAttributes.SERVICE_NAME]: serviceName
         }), 
-        sampler: sampler
+        sampler:new ParentBasedSampler({
+            root: new TraceIdRatioBasedSampler(1)
+        })
     });
 
     // provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter())); // Uncomment this, if you want print data on console for e.g. for  debugging purposes.
-    provider.addSpanProcessor(new spanProcessor(otelExporter));
+    provider.addSpanProcessor(new BatchSpanProcessor(otelExporter));
 
     provider.register({
         // Changing default contextManager to use ZoneContextManager - supports asynchronous operations - optional
         contextManager: new ZoneContextManager(),
     });
+    registerInstrumentations({
+        instrumentations: [
+            new UserInteractionInstrumentation(),
+            new XMLHttpRequestInstrumentation(),
+            new FetchInstrumentation()
+        ],
+    });
+  
+    const tracer = provider.getTracer(serviceName);
 
-    return { provider, otelExporter };
+    return { tracer, provider, otelExporter };
 };
 
 export default init;
