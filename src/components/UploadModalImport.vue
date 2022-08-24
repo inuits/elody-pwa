7dd26bfe011f4499302868d8cb41aaea21ba4d26
Ricardo Acosta Torres <ricardo@inuits.eu>
2022-08-24 15:12:47 +0200

    Ref: #79539, temporary back to original state.

--- src/components/UploadModalImport.vue
+++ src/components/UploadModalImport.vue
@@ -1,46 +1,28 @@
 <template>
-  <tabs>
-    <!-- TAB1 -->
-    <tab title="Upload files">
-      <div class="p-3 h-full">
-        <dropzone v-model:progress="progress" />
-      </div>
-    </tab>
-
-    <!-- TAB2 -->
-    <tab title="Import from external source">
-      <folder-tree :data="directories" />
-      <div class="w-full flex flex-col sticky bottom-0 p-5 bg-neutral-30 z-10">
-        <BaseButton
-          bg-color="blue-400"
-          txt-color="neutral-0"
-          label="Import"
-          bg-hover-color="blue-100"
-          @click="doImport()"
-        />
-      </div>
-    </tab>
-  </tabs>
+  <folder-tree :data="directories" />
+  <div class="w-full flex flex-col sticky bottom-0 p-5 bg-neutral-30 z-10">
+    <BaseButton
+      bg-color="blue-400"
+      txt-color="neutral-0"
+      label="Import"
+      bg-hover-color="blue-100"
+      @click="doImport()"
+    />
+  </div>
 </template>
 <script lang="ts">
-  import { defineComponent, inject, provide, ref, Ref } from 'vue';
+  import { defineComponent, provide, ref, Ref } from 'vue';
   import FolderTree from './FolderTree.vue';
   import BaseButton from './base/BaseButton.vue';
   import { useMutation } from '@vue/apollo-composable';
   import { Directory, PostStartImportDocument } from '@/queries';
-  import { UploadModalType, useUploadModal } from './UploadModal.vue';
-  import Tabs from './Tabs.vue';
-  import Tab from './Tab.vue';
-  import Dropzone from './Dropzone.vue';
+  import { useUploadModal } from './UploadModal.vue';
 
   export default defineComponent({
     name: 'UploadModalImport',
     components: {
       FolderTree,
       BaseButton,
-      Tabs,
-      Tab,
-      Dropzone,
     },
     props: {
       directories: {
@@ -74,16 +56,8 @@
         }
       };
 
-      const progress = ref<any>({
-        status: 'new',
-        progress: 0,
-        successFiles: 0,
-        errorFiles: 0,
-      });
-
       return {
         doImport,
-        progress,
       };
     },
   });
