f4cc046dc6a1bde761e285bd98899ffd1ccf6c57
Ricardo Acosta Torres <ricardo@inuits.eu>
2022-08-30 12:47:43 +0200

    Missing view

---
+++ src/views/Mediafiles.vue
@@ -0,0 +1,14 @@
+<template>
+  <AssetLibrary />
+  <router-view />
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import AssetLibrary from '@/components/AssetLibrary.vue';
+
+  export default defineComponent({
+    name: 'Mediafiles',
+    components: { AssetLibrary },
+  });
+</script>
