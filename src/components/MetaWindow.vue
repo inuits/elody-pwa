398a3104a4d8b52d34f56cd2545b9ac509614cf4
Jeroen Wouters <jeroenwouters@inuits.eu>
2022-10-21 12:11:52 +0000

    change metawindow to script setup

--- src/components/MetaWindow.vue
+++ src/components/MetaWindow.vue
@@ -14,53 +14,40 @@
     <meta-edit
       v-if="!loading && isEdit && form"
       v-model="metadataComputed"
-      :error="error"
       :loading="loading"
-      :discard="discard"
       :entity-title="entityTitle"
       :form="form"
     />
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref, watch } from "vue";
-import type { PropType } from "vue";
+<script lang="ts" setup>
+import { ref, watch } from "vue";
 import MetaEdit from "@/components/MetaEdit.vue";
 import MetaView from "@/components/MetaView.vue";
 import { useEditMode } from "@/composables/useEdit";
 import type { Form, MetadataAndRelation } from "@/queries";
 
-export default defineComponent({
-  name: "MetaWindow",
-  components: { MetaView, MetaEdit },
-  props: {
-    metadata: {
-      type: Array as PropType<MetadataAndRelation[]>,
-      required: true,
-    },
-    form: { type: Object as PropType<Form>, required: false },
-    entityTitle: { type: String, required: false, default: undefined },
-    loading: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  setup(props) {
-    const { isEdit } = useEditMode();
-    const metadataComputed = ref<MetadataAndRelation[]>(props.metadata);
+const props = withDefaults(
+  defineProps<{
+    metadata: MetadataAndRelation[];
+    form: Form;
+    entityTitle: string;
+    loading: boolean;
+  }>(),
+  {
+    entityTitle: "",
+    loading: false,
+  }
+);
 
-    watch(
-      () => props.metadata,
-      (value) => {
-        metadataComputed.value = value;
-      }
-    );
+const { isEdit } = useEditMode();
+const metadataComputed = ref<MetadataAndRelation[]>(props.metadata);
 
-    return {
-      isEdit,
-      metadataComputed,
-    };
-  },
-});
+watch(
+  () => props.metadata,
+  (value) => {
+    metadataComputed.value = value;
+  }
+);
 </script>
