6d1ade3edf1cece55e31d126e1cba7b3a869b7e4
Jeroen Wouters <jeroenwouters@inuits.eu>
2022-10-26 07:57:57 +0000

    add support for npx link vite

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  "*.ts": ["eslint --fix", "tsc-files --noEmit", "git add"],
+  "!(*config).ts": ["eslint --fix", "tsc-files --noEmit", "git add"],
   "*.vue": ["eslint --fix", "tsc-files --noEmit", "git add"],
 };
