67beb8adfbf58906baf5faa1849afc1c76e4b15a
Berend <berend@inuits.eu>
2023-01-31 16:54:30 +0100

    use primary mediafile for grid/list if transcode is not available

--- src/composables/useListItemHelper.ts
+++ src/composables/useListItemHelper.ts
@@ -2,11 +2,12 @@ import type { BaseEntity, Entity } from "../generated-types/queries";
 
 const useListItemHelper = () => {
   const getMediaFilenameFromEntity = (entity: BaseEntity) => {
-    return entity?.media?.primary_transcode
-      ? entity.media.primary_transcode
-      : entity.media?.mediafiles && entity.media.mediafiles[0]
-      ? entity.media?.mediafiles[0]?.transcode_filename
-      : null;
+    return (
+      entity?.media?.primary_transcode ||
+      entity?.media?.primaryMediafile ||
+      entity.media?.mediafiles[0]?.transcode_filename ||
+      null
+    );
   };
 
   return { getMediaFilenameFromEntity };
