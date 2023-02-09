220b1fc5c695ddb13eb8f6572a1037f71c039257
eray <eray@inuits.eu>
2023-02-09 17:08:59 +0100

    Refs #106648 Add header to apollo client

--- src/main.ts
+++ src/main.ts
@@ -79,7 +79,7 @@ const start = async () => {
 
   apolloClient = new ApolloClient({
     link: graphqlErrorInterceptor.concat(
-      createUploadLink({ uri: config.graphQlLink || "/api/graphql" })
+      createUploadLink({ uri: config.graphQlLink || "/api/graphql", headers: {'Apollo-Require-Preflight': 'true'} })
     ),
     cache: new InMemoryCache(),
   });
