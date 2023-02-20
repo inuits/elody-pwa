2115b4b7d95b4d6955476b05fd1fa8d1c78cfdc2
Bjarne <bjarne@inuits.eu>
2023-02-20 15:55:52 +0100

    Moved breadcrumbs to component

---
+++ src/components/BreadCrumbs.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1 class="text-lg font-semibold text-neutral-800 float-left">
+    <ol class="flex text-gray-700 bg-gray-300 rounded py-2 px-2">
+      <li class="px-2">
+        <a href="/" class="hover:underline">{{ pageInfo.routerTitle }}</a>
+      </li>
+      <li class="text-gray-500 select-none">&rsaquo;</li>
+      <li class="px-2">
+        <a href="#" class="hover:underline"
+          ><span
+            v-if="pageInfo.entityTitle !== '' && route.meta.showEntityTitle"
+            class="text-neutral-400"
+          >
+            {{ pageInfo.entityTitle }}</span
+          ></a
+        >
+      </li>
+    </ol>
+  </h1>
+  <EditToggle v-if="auth.isAuthenticated.value === true" />
+</template>
+
+<script lang="ts" setup>
+import { watch } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { usePageInfo } from "../composables/usePageInfo";
+import { useAuth } from "session-vue-3-oidc-library";
+
+const { pageInfo } = usePageInfo();
+const auth = useAuth();
+const route = useRoute();
+const router = useRouter();
+
+watch(
+  pageInfo,
+  () => {
+    if (pageInfo.value.entityTitle !== "") {
+      document.title = pageInfo.value.entityTitle;
+      return;
+    }
+    document.title = pageInfo.value.routerTitle;
+  },
+  { deep: true }
+);
+</script>
