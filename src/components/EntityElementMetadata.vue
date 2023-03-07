5c0f6489b6867d2d9e43f5b27aa9ffcf92552d5c
Berend Vandenbussche <berend@inuits.eu>
2023-03-07 08:19:26 +0100

    Refs #107742 Adjust metadata panel styles

--- src/components/EntityElementMetadata.vue
+++ src/components/EntityElementMetadata.vue
@@ -1,6 +1,8 @@
 <template>
-  <span class="w-full">{{ metadata.label }}</span>
-  <span class="w-full">{{ value }}</span>
+  <div class="text-sm pl-4">
+    <span class="text-text-light w-full">{{ metadata.label }}</span>
+    <span class="w-full">{{ value }}</span>
+  </div>
 </template>
 
 <script lang="ts" setup>
