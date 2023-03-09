e05fd59028888172b8c4c5a0a2728b3a69e03701
Berend Vandenbussche <berend@inuits.eu>
2023-03-09 11:44:48 +0100

    Show old single entity component based on feature flag

--- src/views/SingleEntity.vue
+++ src/views/SingleEntity.vue
@@ -1,14 +1,19 @@
 <template>
   <BaseSingleEntity
+    v-if="useOldComponent"
     :isSelectionDisplayed="true"
     :isMetaDisplayed="true"
     :isMediaFileSingle="false"
     :entityType="'Entity'"
   />
-  <!-- <entity-single></entity-single> -->
+  <entity-single v-else></entity-single>
 </template>
 
 <script lang="ts" setup>
 import BaseSingleEntity from "@/components/base/BaseSingleEntity.vue";
+import { inject } from "vue";
 import EntitySingle from "../components/EntitySingle.vue";
+
+const config: any = inject("config");
+const useOldComponent = config.features.useOldSingleEntityComponent;
 </script>
