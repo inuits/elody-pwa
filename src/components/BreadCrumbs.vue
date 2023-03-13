85b3edb7c048dbe892b026f32b6b2da9b30b7209
Bjarne <bjarne@inuits.eu>
2023-03-13 16:22:16 +0100

    finished composable

--- src/components/BreadCrumbs.vue
+++ src/components/BreadCrumbs.vue
@@ -1,24 +1,92 @@
 <template>
-  <div class="relative">
-    <h1 class="text-lg font-semibold text-neutral-800 float-left">
-      <a href="/" class="hover:underline">{{ routerTitle }}</a>
-      <span v-if="showEntityTitle" class="text-gray-500">&rsaquo;</span>
-      <span v-if="showEntityTitle" class="dropdown inline-block relative">
-        <a href="#" class="hover:underline">
-          {{ entityTitle }}
-        </a>
-        <ul class="dropdown-menu absolute hidden z-10">
-          <li v-for="(page, index) in visitedPages" :key="index">
-            <a :href="page.path" class="hover:underline">{{ page.title }}</a>
-          </li>
-        </ul>
-      </span>
-    </h1>
-    <EditToggle v-if="isAuthenticated" />
+  <div class="breadcrumb">
+    <span v-if="showVisitedPages"></span>
+    <select v-if="showVisitedPages" v-model="selectedVisitedPage" @change="onVisitedPageChange($event.target.value)">
+      <option v-for="page in visitedPagesOptions" :value="page.value">{{ page.label }}</option>
+    </select>
+    <span v-if="showEntityTitle" class="breadcrumb-divider"> > </span>
+    <span v-if="showEntityTitle" class="breadcrumb-item">{{ entityTitle }}</span>
   </div>
 </template>
 
-<script lang="ts" setup>
-import useBreadcrumb from '../composables/useBreadcrumb';
-const { routerTitle, entityTitle, showEntityTitle, isAuthenticated, visitedPages } = useBreadcrumb();
+<script>
+import { useBreadcrumb } from '@/composables/useBreadcrumb'
+
+export default {
+  name: 'Breadcrumb',
+  mounted() {
+    const lastBreadcrumbItem = document.querySelector('.breadcrumb-item:last-child')
+    if (lastBreadcrumbItem) {
+      lastBreadcrumbItem.classList.add('breadcrumb-item--active')
+    }
+  },
+  setup() {
+    const {
+      visitedPagesOptions,
+      selectedVisitedPage,
+      showVisitedPages,
+      entityTitle,
+      showEntityTitle,
+      addVisitedPage,
+      clearVisitedPages,
+      onVisitedPageChange,
+      pageInfo
+    } = useBreadcrumb()
+
+    return {
+      visitedPagesOptions,
+      selectedVisitedPage,
+      showVisitedPages,
+      entityTitle,
+      showEntityTitle,
+      addVisitedPage,
+      clearVisitedPages,
+      onVisitedPageChange,
+      pageInfo
+    }
+  },
+}
 </script>
+
+<style>
+.breadcrumb {
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+}
+
+.breadcrumb-divider {
+  color: #ccc;
+  margin: 0 5px;
+}
+
+.breadcrumb-item {
+  display: inline-block;
+  font-size: 0.875rem;
+  margin-right: 0.5rem;
+}
+
+.breadcrumb-item:not(:last-child)::after {
+  display: inline-block;
+  padding-left: 0.5rem;
+  padding-right: 0.5rem;
+  color: #6c757d;
+  content: "/";
+}
+
+.breadcrumb-item--active {
+  color: #6c757d;
+}
+
+.breadcrumb a {
+  color: #6c757d;
+}
+
+select {
+  padding: 0.5rem 1rem;
+  border-radius: 0.25rem;
+  border: 1px solid #ccc;
+  font-size: 1rem;
+  margin-left: 0.5rem;
+}
+</style>
--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -5,226 +5,108 @@
   >
     <router-link
       :to="{ name: 'Home' }"
-      class="logo router-link text-base text-neutral-700 font-semibold flex justify-center items-center mb-8 text-xl"
+      class="logo router-link text-base text-neutral-700 font-semibold flex justify-center items-center mb-8"
       @click="forceDisableEditModalHome"
     >
       {{ $t("navigation.title") }}
     </router-link>
-
-    <!-- Entities -->
-    <router-link
-      :to="{ name: 'Home' }"
-      class="flex flex-row items-center menu-item"
-      :class="{ IsActive: showDropdown }"
-      @click="toggleDropDown"
-    >
+    <div class="flex flex-row items-center menu-item">
       <BaseButton
         :icon="Unicons.BookOpen.name"
-        :icon-height="20"
+        bg-color="neutral-30"
         class="menu-btn"
         @click="forceDisableEditModalHome"
-        bg-color="var(--color-neutral)"
       />
       <span
-        class="nav-item-label w-0 h-0 overflow-hidden px-4 cursor-pointer font-bold"
+        class="nav-item-label w-0 h-0 overflow-hidden px-4 cursor-pointer"
         @click="forceDisableEditModalHome"
+        >{{ $t("navigation.assets") }}</span
       >
-        {{ $t("navigation.entities") }}
-      </span>
-      <div class="float-right ml-20">
-        <div v-if="showDropdown === true">
-          <BaseButton
-            :icon="Unicons.AngleDown.name"
-            class="rotate-180"
-            bg-color="var(--color-neutral-40)"
-          >
-          </BaseButton>
-        </div>
-        <div v-else-if="showDropdown === false">
-          <BaseButton
-            :icon="Unicons.AngleDown.name"
-            :icon-height="20"
-          ></BaseButton>
-        </div>
-      </div>
-    </router-link>
-
-    <!--Sub Menu-->
-    <div class="pl-13 bg-[var(--color-blue-500)]">
-      <div
-        class="flex flex-column items-center dropdownMenu-item"
-        v-if="showDropdown == true"
-      >
-        <span
-          class="nav-item-label w-0 h-0 overflow-hidden px-4 cursor-pointer font-bold"
-          @click="Asset"
-          :style="{
-            color: clickedonAsset ? '#02C6F2' : 'var(--color-neutral-900)',
-          }"
-          >{{ $t("navigation.asset") }}</span
-        >
-      </div>
-
-      <div
-        class="flex flex-column items-center dropdownMenu-item"
-        v-if="showDropdown == true"
-      >
-        <span
-          class="nav-item-label w-0 h-0 overflow-hidden px-4 cursor-pointer font-bold"
-          @click="Boeken"
-          :style="{
-            color: clickedonBoeken ? '#02C6F2' : 'var(--color-neutral-900)',
-          }"
-          >{{ $t("navigation.boeken") }}</span
-        >
-      </div>
-      <div
-        class="flex flex-column items-center dropdownMenu-item"
-        v-if="showDropdown == true"
-      >
-        <span
-          class="nav-item-label w-0 h-0 overflow-hidden px-4 cursor-pointer font-bold"
-          @click="Tijdschriften"
-          :style="{
-            color: clickedonTijdschriften
-              ? '#02C6F2'
-              : 'var(--color-neutral-900)',
-          }"
-          >{{ $t("navigation.tijdschriften") }}</span
-        >
-      </div>
     </div>
-
-    <!-- Mediafile -->
-    <router-link
-      :to="{ name: 'Mediafiles' }"
-      activeClass="IsActive"
+    <div
       v-show="auth.isAuthenticated.value === true"
       class="flex flex-row items-center menu-item"
-      @click="forceDisableEditMediafiles"
     >
       <BaseButton
-        :icon="Unicons.Image.name"
+        :icon="Unicons.FileAlt.name"
+        bg-color="neutral-30"
         class="menu-btn"
         @click="forceDisableEditMediafiles"
-        :icon-height="20"
-        bg-color="var(--color-neutral)"
       />
       <span
-        class="nav-item-label w-0 h-0 overflow-hidden px-4 cursor-pointer font-bold"
+        class="nav-item-label w-0 h-0 overflow-hidden px-4 cursor-pointer"
         @click="forceDisableEditMediafiles"
       >
-        {{ $t("navigation.mediafile") }}
+        {{ $t("navigation.mediafiles") }}
       </span>
-    </router-link>
-
-    <!-- Upload -->
-
+    </div>
+    <div
+      v-show="auth.isAuthenticated.value === true"
+      class="flex flex-row items-center menu-item"
+    >
+      <BaseButton
+        :icon="Unicons.History.name"
+        bg-color="neutral-30"
+        class="mt-1 menu-btn"
+        @click="forceDisableEditModalHistory"
+      />
+      <span
+        class="nav-item-label w-0 h-0 overflow-hidden px-4 cursor-pointer"
+        @click="forceDisableEditModalHistory"
+        >{{ $t("navigation.jobs") }}</span
+      >
+    </div>
     <div
       v-show="
         auth.isAuthenticated.value === true &&
         determinePermission('can-start-import')
       "
       class="flex flex-row items-center menu-item"
-      :class="{ IsActive: uploadModalState.state === 'show' }"
     >
       <BaseButton
         :icon="Unicons.Upload.name"
         class="mt-1 menu-btn"
+        bg-color="neutral-30"
         @click="openUploadModal(modalChoices.IMPORT)"
-        :icon-height="20"
-        bg-color="var(--color-neutral)"
       />
       <span
-        class="nav-item-label w-0 h-0 overflow-hidden px-4 cursor-pointer font-bold"
+        class="nav-item-label w-0 h-0 overflow-hidden px-4 cursor-pointer"
         @click="openUploadModal(modalChoices.IMPORT)"
+        >{{ $t("navigation.import") }}</span
       >
-        {{ $t("navigation.upload") }}
-      </span>
     </div>
-
-    <!-- Nieuw -->
     <div
       v-show="
         auth.isAuthenticated.value === true &&
         determinePermission('create-entity')
       "
       class="flex flex-row items-center menu-item"
-      :class="{ IsActive: createModalState.state === 'show' }"
     >
       <BaseButton
         :icon="Unicons.Create.name"
         class="mt-1 menu-btn"
+        bg-color="neutral-30"
         @click="openCreateModal"
-        :icon-height="20"
-        bg-color="var(--color-neutral)"
       />
       <span
-        class="nav-item-label w-0 h-0 overflow-hidden px-4 cursor-pointer font-bold"
+        class="nav-item-label w-0 h-0 overflow-hidden px-4 cursor-pointer"
         @click="openCreateModal"
+        >{{ $t("navigation.create") }}</span
       >
-        {{ $t("navigation.nieuw") }}
-      </span>
     </div>
-
-    <!-- Jobs -->
-    <router-link
-      :to="{ name: 'History' }"
-      @click="forceDisableEditModalHistory"
-      activeClass="IsActive"
-      v-show="auth.isAuthenticated.value === true"
-      class="flex flex-row items-center menu-item"
-    >
+    <!-- <div class="flex flex-row items-center menu-item">
       <BaseButton
-        :icon="Unicons.History.name"
-        class="mt-1 menu-btn"
-        @click="forceDisableEditModalHistory"
-        :icon-height="20"
-        bg-color="var(--color-neutral)"
-      />
-      <span
-        class="nav-item-label w-0 h-0 overflow-hidden px-4 cursor-pointer font-bold"
-        @click="forceDisableEditModalHistory"
-      >
-        {{ $t("navigation.jobs") }}
-      </span>
-    </router-link>
-
-    <div class="flex flex-row items-center menu-item login-out">
-      <BaseButton
-        v-if="auth.isAuthenticated.value === false"
-        :icon="Unicons.User.name"
-        class="mt-1 menu-btn"
-        @click="auth.redirectToLogin()"
-        :icon-height="20"
-        bg-color="var(--color-neutral)"
-      />
-      <span
-        v-if="auth.isAuthenticated.value === false"
-        class="nav-item-label w-0 h-0 overflow-hidden px-4 cursor-pointer font-bold"
-        @click="auth.redirectToLogin()"
-      >
-        {{ $t("navigation.log-in") }}
-      </span>
-    </div>
-
-    <div class="flex flex-row items-center menu-item login-out">
-      <BaseButton
-        v-if="auth.isAuthenticated.value === true"
         :icon="Unicons.SignOut.name"
-        :icon-height="20"
         class="mt-1 menu-btn"
-        bg-color="var(--color-neutral)"
-        @click="logout()"
+        bg-color="neutral-30"
+        @click="logout"
       />
       <span
-        v-if="auth.isAuthenticated.value === true"
-        class="nav-item-label w-0 h-0 overflow-hidden px-4 cursor-pointer font-bold"
-        @click="auth.logout()"
+        class="nav-item-label w-0 h-0 overflow-hidden px-4 cursor-pointer"
+        @click="logout"
+        >{{ $t("navigation.log-out") }}</span
       >
-        {{ $t("navigation.log-out") }}
-      </span>
-    </div>
+    </div> -->
   </nav>
 </template>
 
@@ -237,13 +119,28 @@ import { useEditMode } from "../composables/useEdit";
 import { useCreateModal } from "./CreateModal.vue";
 import { useAuth } from "session-vue-3-oidc-library";
 import { usePermissions } from "../composables/usePermissions";
+import { useQuery } from "@vue/apollo-composable";
+import {
+  GetMenuDocument,
+  type GetMenuQuery,
+  type GetMenuQueryVariables,
+} from "@/generated-types/queries";
+import useUploadModal, { modalChoices } from "@/composables/useUploadModal";
 
 const auth = useAuth();
 const { determinePermission, loading } = usePermissions();
-const { openUploadModal, uploadModalState } = useUploadModal();
-const { openCreateModal, createModalState } = useCreateModal();
+const { openUploadModal } = useUploadModal();
+const { openCreateModal } = useCreateModal();
 const router = useRouter();
 const { disableEditMode } = useEditMode();
+
+const queryVariables = reactive<GetMenuQueryVariables>({
+  name: "main-menu",
+});
+const { result: menuQueryResult } = useQuery<GetMenuQuery>(
+  GetMenuDocument,
+  queryVariables
+);
 
 const forceDisableEditModalHome = () => {
   router.push({ name: "Home" });
@@ -252,68 +149,12 @@ const forceDisableEditModalHome = () => {
 
 const forceDisableEditModalHistory = () => {
   router.push({ name: "History" });
-  showDropdown.value = false;
   disableEditMode();
 };
 
 const forceDisableEditMediafiles = () => {
   router.push({ name: "Mediafiles" });
-  showDropdown.value = false;
   disableEditMode();
-};
-
-const logout = async () => {
-  await auth.logout();
-  showDropdown.value = false;
-  router.push({ name: "Home" });
-};
-
-const Asset = () => {
-  clickedonAsset.value = !clickedonAsset.value;
-  if (clickedonAsset.value === true) {
-    if (clickedonBoeken.value === true) {
-      clickedonBoeken.value = false;
-    }
-    if (clickedonTijdschriften.value === true) {
-      clickedonTijdschriften.value = false;
-    }
-  }
-  console.log("Asset :" + clickedonAsset.value);
-};
-
-const Boeken = () => {
-  clickedonBoeken.value = !clickedonBoeken.value;
-  console.log("Boeken :" + clickedonBoeken.value);
-  if (clickedonBoeken.value === true) {
-    if (clickedonAsset.value === true) {
-      clickedonAsset.value = false;
-    }
-    if (clickedonTijdschriften.value === true) {
-      clickedonTijdschriften.value = false;
-    }
-  }
-};
-
-const Tijdschriften = () => {
-  clickedonTijdschriften.value = !clickedonTijdschriften.value;
-  console.log("Tijdschriften :" + clickedonTijdschriften.value);
-  if (clickedonTijdschriften.value === true) {
-    if (clickedonBoeken.value === true) {
-      clickedonBoeken.value = false;
-    }
-    if (clickedonAsset.value === true) {
-      clickedonAsset.value = false;
-    }
-  }
-};
-const toggleDropDown = () => {
-  showDropdown.value = !showDropdown.value;
-  console.log(showDropdown.value);
-  if (showDropdown.value === false) {
-    clickedonAsset.value = false;
-    clickedonBoeken.value = false;
-    clickedonTijdschriften.value = false;
-  }
 };
 </script>
 
@@ -322,16 +163,11 @@ const toggleDropDown = () => {
   transition-property: all;
   transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
   transition-duration: 300ms;
-  overflow-x: hidden;
-}
-
-.menu-item {
-  margin-top: 1rem;
 }
 
 .menu-item:hover .menu-btn {
   --tw-bg-opacity: 1;
-  fill: #02c6f2;
+  background-color: rgb(165 173 186 / var(--tw-bg-opacity));
 }
 
 .navbar:hover {
@@ -339,13 +175,10 @@ const toggleDropDown = () => {
 }
 
 .navbar:hover .router-link {
+  padding-left: 0.3rem;
   justify-content: flex-start;
 }
-.login-out {
-  position: fixed;
-  bottom: 3%;
-  left: 1%;
-}
+
 .navbar:hover .nav-item-label {
   animation: showText 0.1s ease-in 0.2s forwards;
   -moz-animation: showText 0.1s ease-in 0.2s forwards;
@@ -354,18 +187,6 @@ const toggleDropDown = () => {
   animation-fill-mode: forwards;
 }
 
-.dropdownMenu-item {
-  cursor: pointer;
-  margin-left: 3.6rem;
-  animation: dropdown 1s 1;
-}
-
-.IsActive {
-  fill: #02c6f2;
-  color: #02c6f2;
-  background-color: var(--color-neutral-40);
-  border-radius: 15px;
-}
 @keyframes showText {
   100% {
     width: auto;
@@ -373,15 +194,6 @@ const toggleDropDown = () => {
   }
 }
 
-@keyframes dropdown {
-  0% {
-    margin-top: -1.5rem;
-    transition: opacity 0 0.2s ease-in;
-  }
-  100% {
-    margin-top: 0.5rem;
-  }
-}
 @-webkit-keyframes showText {
   100% {
     width: auto;
