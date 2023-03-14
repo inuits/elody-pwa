c32c22b1aa316f5ae0b37bd9cea6128e9d06c44b
Berend Vandenbussche <berend@inuits.eu>
2023-03-14 11:36:17 +0100

    Refs #105030 Get mediainfo

--- src/components/EntityElementMetadata.vue
+++ src/components/EntityElementMetadata.vue
@@ -1,17 +1,13 @@
 <template>
   <div class="text-sm pl-4">
-    <p class="text-text-light w-full">{{ metadata.label }}</p>
+    <p class="text-text-light w-full">{{ label }}</p>
     <p class="w-full">{{ value || "-" }}</p>
   </div>
 </template>
 
 <script lang="ts" setup>
-import type { PanelMetaData, PanelRelation } from "@/generated-types/queries";
-import { useField } from "vee-validate";
-
 const props = defineProps<{
-  metadata: PanelMetaData;
+  label: String;
+  value: String;
 }>();
-
-const { value } = useField(props.metadata.key);
 </script>
--- src/components/EntityElementWindowPanel.vue
+++ src/components/EntityElementWindowPanel.vue
@@ -8,16 +8,7 @@
     </div>
     <transition>
       <div v-if="!isCollapsed">
-        <div v-if="metadataArray.length">
-          <div
-            v-for="(metadata, index) in metadataArray"
-            :key="index"
-            class="py-2"
-          >
-            <entity-element-metadata :metadata="metadata" />
-          </div>
-        </div>
-        <div v-if="relationArray.length">
+        <div v-if="panelType === PanelType.Relation">
           <div class="pl-2 rounded-sm bg-accent-light">
             <p class="text-sm text-text-body">Behoort tot</p>
             <div class="rounded-sm border-solid border-neutral-30 border-2">
@@ -31,6 +22,18 @@
             </div>
           </div>
         </div>
+        <div v-else>
+          <div
+            v-for="(metadata, index) in metadataArray"
+            :key="index"
+            class="py-2"
+          >
+            <entity-element-metadata
+              :label="metadata.key"
+              :value="metadata.value"
+            />
+          </div>
+        </div>
       </div>
     </transition>
   </div>
@@ -38,52 +41,85 @@
 
 <script lang="ts" setup>
 import type {
+  KeyValue,
+  MediaFile,
+  MetadataValuesInput,
   PanelMetaData,
   PanelRelation,
   WindowElementPanel,
 } from "@/generated-types/queries";
 import { PanelType } from "@/generated-types/queries";
-import { computed, ref } from "vue";
+import { ref } from "vue";
 import EntityElementMetadata from "./EntityElementMetadata.vue";
 import EntityElementRelation from "./EntityElementRelation.vue";
 import { Unicons } from "@/types";
+import { useField } from "vee-validate";
+import { useEntityMediafileSelector } from "./EntityImageSelection.vue";
 
 const props = defineProps<{
   panel: WindowElementPanel;
 }>();
 
+const panelType = ref<PanelType>(props.panel.panelType);
 const isCollapsed = ref<boolean>(false);
+const metadataArray = ref<MetadataValuesInput[]>([]);
+const relationArray = ref<PanelRelation[]>([]);
+const { mediafileSelectionState } = useEntityMediafileSelector();
 
 const toggleIsCollapsed = () => {
   isCollapsed.value = !isCollapsed.value;
 };
 
-const metadataArray = computed<PanelMetaData[]>(() => {
-  const returnArray: PanelMetaData[] = [];
-  if (props.panel.panelType !== PanelType.Relation) {
-    Object.values(props.panel).forEach((value, index) => {
-      if (typeof value !== "string") {
-        returnArray.push(value as PanelMetaData);
-      }
-    });
-  }
+const getMetadataForPanel = (): MetadataValuesInput[] => {
+  const returnArray: MetadataValuesInput[] = [];
+  Object.values(props.panel).forEach((value) => {
+    if (value && typeof value !== "string") {
+      const metadataObject = {
+        key: (value as PanelMetaData).label,
+        value: useField((value as PanelMetaData).key).value.value as string,
+      };
+      returnArray.push(metadataObject);
+    }
+  });
 
   return returnArray;
-});
+};
 
-const relationArray = computed<PanelRelation[]>(() => {
+const getRelationsForPanel = (): PanelRelation[] => {
   const returnArray: PanelRelation[] = [];
-  if (props.panel.panelType === PanelType.Relation) {
-    Object.values(props.panel).forEach((value, index) => {
-      if (typeof value !== "string") {
-        const relationList = value as [PanelRelation];
-        returnArray.push(...relationList);
-      }
-    });
-  }
+  Object.values(props.panel).forEach((value) => {
+    if (typeof value !== "string") {
+      const relationList = value as [PanelRelation];
+      returnArray.push(...relationList);
+    }
+  });
 
   return returnArray;
-});
+};
+
+const getMediaInfo = (): MetadataValuesInput[] => {
+  const returnArray: MetadataValuesInput[] = [];
+  const selectedMediafile: { [index: string]: any } | undefined =
+    mediafileSelectionState.selectedMediafile;
+  Object.values(props.panel).forEach((value) => {
+    if (typeof value !== "string" && selectedMediafile) {
+      const valueKey = (value as PanelMetaData).key;
+      returnArray.push({
+        key: (value as PanelMetaData).label,
+        value: selectedMediafile[valueKey],
+      });
+    }
+  });
+  return returnArray;
+};
+
+if (panelType.value === PanelType.Metadata) {
+  metadataArray.value = getMetadataForPanel();
+} else if (panelType.value === PanelType.Relation) {
+  relationArray.value = getRelationsForPanel();
+} else if (panelType.value === PanelType.Mediainfo) {
+  metadataArray.value = getMediaInfo();
+}
 </script>
 
 <style scoped>
