3fc91c44b2410d74d5f57a5a0c1e92a623e9fb1e
Berend Vandenbussche <berend@inuits.eu>
2023-03-20 09:23:03 +0100

    Add thumbnail fallback for edge cases with no mimetype

--- src/composables/useThumbnailHelper.ts
+++ src/composables/useThumbnailHelper.ts
@@ -4,26 +4,31 @@ const useThumbnailHelper = (): {
   getThumbnail: (entity: any) => string;
 } => {
   const getThumbnail = (data: any) => {
-    if (
-      data?.label === "audio" ||
-      (data?.media?.mediafiles?.length > 0 &&
-        data?.media?.mediafiles[0]?.mimetype.includes("audio"))
-    ) {
-      // @ts-ignore
-      return Unicons.Music.name;
-    }
+    try {
+      if (
+        data?.label === "audio" ||
+        (data?.media?.mediafiles?.length > 0 &&
+          data?.media?.mediafiles[0]?.mimetype.includes("audio"))
+      ) {
+        // @ts-ignore
+        return Unicons.Music.name;
+      }
 
-    if (
-      data?.label === "subtitle" ||
-      (data?.media?.mediafiles?.length > 0 &&
-        data?.media?.mediafiles[0]?.mimetype.includes("text"))
-    ) {
-      // @ts-ignore
-      return Unicons.Text.name;
-    }
+      if (
+        data?.label === "subtitle" ||
+        (data?.media?.mediafiles?.length > 0 &&
+          data?.media?.mediafiles[0]?.mimetype.includes("text"))
+      ) {
+        // @ts-ignore
+        return Unicons.Text.name;
+      }
 
-    // @ts-ignore
-    return Unicons.NoImage.name;
+      // @ts-ignore
+      return Unicons.NoImage.name;
+    } catch (e) {
+      console.log("Unable to get thumbnail, fallback to default");
+      return Unicons.NoImage.name;
+    }
   };
   return {
     getThumbnail,
