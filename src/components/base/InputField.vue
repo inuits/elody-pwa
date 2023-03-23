c1ed94e4bbf8d7e09d4424d6593b3b0e7488b865
Berend Vandenbussche <berend@inuits.eu>
2023-03-23 11:53:24 +0100

    Enforce inputfields of correct type

--- src/components/base/InputField.vue
+++ src/components/base/InputField.vue
@@ -24,8 +24,9 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from "vue";
+import { computed, defineComponent, type PropType } from "vue";
 import { debounce } from "ts-debounce";
+import { InputFieldTypes } from "@/generated-types/queries";
 export const lableStyle = 'ml-1 text-neutral-700 text-sm"';
 export const inputContainerStyle = "flex flex-row bg-neutral-20 rounded";
 export const inputStyle =
@@ -46,7 +47,11 @@ export default defineComponent({
     bgColor: { type: String, default: "neutral-0" },
     name: { type: String, default: "", required: false },
     isDisabled: { type: Boolean, default: false, required: false },
-    type: { type: String, required: false, default: "text" },
+    type: {
+      type: String as PropType<InputFieldTypes>,
+      required: false,
+      default: InputFieldTypes.Text,
+    },
     accept: { type: String, required: false, default: "" },
   },
   emits: ["update:modelValue"],
