1bb1ff335b4852b127de1a60e252116fd9fd6798
Berend Vandenbussche <berend@inuits.eu>
2023-03-28 12:05:23 +0200

    Refs #109438 Show Thumbnail for entities with link

--- src/composables/useListItemHelper.ts
+++ src/composables/useListItemHelper.ts
@@ -2,13 +2,22 @@ import type { BaseEntity, Entity } from "../generated-types/queries";
 
 const useListItemHelper = () => {
   const getMediaFilenameFromEntity = (entity: BaseEntity) => {
-    return (
-      entity?.media?.primary_transcode ||
-      entity?.media?.primaryMediafile ||
-      (entity?.media?.mediafiles &&
-        entity?.media?.mediafiles[0]?.transcode_filename) ||
-      null
-    );
+    let mediafile = undefined;
+    if (entity.media?.primary_transcode) {
+      mediafile = entity.media.primary_transcode;
+    }
+    if (entity?.media?.primaryMediafile) {
+      mediafile = entity.media.primaryMediafile;
+    }
+    if (entity?.media?.mediafiles && entity?.media?.mediafiles[0]) {
+      if (entity?.media?.mediafiles[0]?.transcode_filename) {
+        mediafile = entity?.media?.mediafiles[0]?.transcode_filename;
+      }
+      if (entity.media?.mediafiles[0]?.thumbnail_file_location) {
+        mediafile = entity.media?.mediafiles[0]?.thumbnail_file_location;
+      }
+    }
+    return mediafile;
   };
 
   return { getMediaFilenameFromEntity };
