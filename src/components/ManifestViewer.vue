e4a89e7bbaeaef95ba4550f78101950b9bbc1e83
Berend Vandenbussche <berend@inuits.eu>
2023-03-28 11:07:08 +0200

    Refs #109438 Show Mirador & Tify in tabs

--- src/components/ManifestViewer.vue
+++ src/components/ManifestViewer.vue
@@ -1,33 +1,39 @@
 <template>
-  <div v-if="isTify" class="w-full h-full" id="tify-viewer"></div>
-  <div v-else class="w-full h-full relative" id="mirador-viewer"></div>
+  <BaseTabs class="bg-neutral-0">
+    <BaseTab title="Mirador">
+      <div class="w-full h-full relative" id="mirador-viewer"></div>
+    </BaseTab>
+    <BaseTab title="Tify">
+      <div class="w-full h-full" id="tify-viewer"></div>
+    </BaseTab>
+  </BaseTabs>
 </template>
 
 <script lang="ts" setup>
 import { onMounted, ref } from "vue";
 // @ts-ignore
 import Mirador from "mirador/dist/mirador.min.js";
+import BaseTabs from "./BaseTabs.vue";
+import BaseTab from "./BaseTab.vue";
 
 const props = defineProps<{ manifestUrl: string }>();
 
 const isTify = ref<Boolean>(true);
 
 onMounted(() => {
-  if (isTify.value) {
-    // @ts-ignore
-    new Tify({
-      container: "#tify-viewer",
-      manifestUrl: props.manifestUrl,
-    });
-  } else {
-    Mirador.viewer({
-      id: "mirador-viewer",
-      windows: [
-        {
-          manifestId: props.manifestUrl,
-        },
-      ],
-    });
-  }
+  // @ts-ignore
+  new Tify({
+    container: "#tify-viewer",
+    manifestUrl: props.manifestUrl,
+  });
+
+  Mirador.viewer({
+    id: "mirador-viewer",
+    windows: [
+      {
+        manifestId: props.manifestUrl,
+      },
+    ],
+  });
 });
 </script>
