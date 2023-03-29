a878d4bac9be3671e4f602230d65a5611d5f89e6
eray <eray@inuits.eu>
2023-03-29 15:33:31 +0200

    Run linter

--- src/tests/useBreadcrumb.test.ts
+++ src/tests/useBreadcrumb.test.ts
@@ -2,22 +2,45 @@ import { describe, test, expect, beforeEach } from "vitest";
 import { useBreadcrumb } from "@/composables/useBreadcrumb";
 
 describe("useBreadcrumb", () => {
-  let pageInfo, visitedPagesOptions, selectedVisitedPage, showVisitedPages, entityTitle, showEntityTitle, addVisitedPage, clearVisitedPages, onVisitedPageChange;
+  let pageInfo,
+    visitedPagesOptions,
+    selectedVisitedPage,
+    showVisitedPages,
+    entityTitle,
+    showEntityTitle,
+    addVisitedPage,
+    clearVisitedPages,
+    onVisitedPageChange;
 
   beforeEach(() => {
     pageInfo = { value: {} };
-    ({ visitedPagesOptions, selectedVisitedPage, showVisitedPages, entityTitle, showEntityTitle, addVisitedPage, clearVisitedPages, onVisitedPageChange } = useBreadcrumb(pageInfo));
+    ({
+      visitedPagesOptions,
+      selectedVisitedPage,
+      showVisitedPages,
+      entityTitle,
+      showEntityTitle,
+      addVisitedPage,
+      clearVisitedPages,
+      onVisitedPageChange,
+    } = useBreadcrumb(pageInfo));
   });
 
   test("adds visited page", () => {
     addVisitedPage({ entityTitle: "Page 1", path: "/page1" });
-    expect(visitedPagesOptions.value).toEqual([{ label: "Home", value: 0 }, { label: "Page 1", value: 1 }]);
+    expect(visitedPagesOptions.value).toEqual([
+      { label: "Home", value: 0 },
+      { label: "Page 1", value: 1 },
+    ]);
   });
 
   test("does not add duplicate visited page", () => {
     addVisitedPage({ entityTitle: "Page 1", path: "/page1" });
     addVisitedPage({ entityTitle: "Page 1", path: "/page1" });
-    expect(visitedPagesOptions.value).toEqual([{ label: "Home", value: 0 }, { label: "Page 1", value: 1 }]);
+    expect(visitedPagesOptions.value).toEqual([
+      { label: "Home", value: 0 },
+      { label: "Page 1", value: 1 },
+    ]);
   });
 
   test("clears visited pages and adds home page", () => {
