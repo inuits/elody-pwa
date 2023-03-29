05863e68f720626dd067fb167489f02f25ecde7b
eray <eray@inuits.eu>
2023-03-29 15:25:39 +0200

    Fix breaking merge

---
+++ src/components/BreadCrumbs.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    class="flex flex-row items-center justify-center bg-neutral-light p-2 gap-2 w-250 h-120 rounded-lg"
+  >
+    <span v-if="showVisitedPages"></span>
+    <div class="relative flex-1">
+      <select
+        v-if="showVisitedPages"
+        v-model="selectedVisitedPage"
+        @change="onVisitedPageChange($event.target.value)"
+        class="w-full py-2 px-4 rounded border-0 text-gray-700 font-medium text-center appearance-none bg-transparent"
+      >
+        <option
+          v-for="page in visitedPagesOptions"
+          :key="page.value"
+          :value="page.value"
+          class="text-gray-700"
+        >
+          {{ page.label }}
+        </option>
+      </select>
+    </div>
+  </div>
+  <span v-if="showEntityTitle" class="mx-2 text-gray-400">&gt;</span>
+  <span v-if="showEntityTitle" class="text-gray-700">{{ entityTitle }}</span>
+</template>
+
+<script lang="ts" setup>
+import { useBreadcrumb } from "@/composables/useBreadcrumb";
+
+const {
+  visitedPagesOptions,
+  selectedVisitedPage,
+  showVisitedPages,
+  entityTitle,
+  showEntityTitle,
+  onVisitedPageChange,
+} = useBreadcrumb();
+</script>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -3,47 +3,17 @@
     class="w-full px-6 py-8 border-b border-neutral-50 z-10 flex items-center justify-between bg-neutral-0"
   >
     <div class="flex w-full items-center">
-      <h1 class="text-lg font-semibold text-neutral-800 float-left">
-        {{ pageInfo.routerTitle
-        }}<span
-          v-if="pageInfo.entityTitle !== '' && route.meta.showEntityTitle"
-          class="text-neutral-400"
-        >
-          / {{ pageInfo.entityTitle }}</span
-        >
-      </h1>
+      <BreadCrumbs />
       <EditToggle v-if="auth.isAuthenticated.value === true" />
     </div>
+    <SearchBar />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { watch } from "vue";
-import { useRoute, useRouter } from "vue-router";
-import { usePageInfo } from "../composables/usePageInfo";
-import EditToggle from "./EditButtons.vue";
+import BreadCrumbs from "@/components/BreadCrumbs.vue";
 import { useAuth } from "session-vue-3-oidc-library";
+import SearchBar from "./SearchBar.vue";
 
-const { pageInfo } = usePageInfo();
 const auth = useAuth();
-
-const route = useRoute();
-const router = useRouter();
-
-watch(
-  pageInfo,
-  () => {
-    if (pageInfo.value.entityTitle !== "") {
-      document.title = pageInfo.value.entityTitle;
-      return;
-    }
-    document.title = pageInfo.value.routerTitle;
-  },
-  { deep: true }
-);
-
-const logout = async () => {
-  await auth.logout();
-  router.push({ name: "Home" });
-};
 </script>
