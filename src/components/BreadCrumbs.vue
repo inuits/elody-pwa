a6f06d044a0e03408be66aae517d391f4e3eecd4
Bjarne <bjarne@inuits.eu>
2023-04-06 15:47:41 +0200

    Added some styling

--- src/components/BreadCrumbs.vue
+++ src/components/BreadCrumbs.vue
@@ -5,33 +5,40 @@
     <span class="text-gray-700">
       <i class="fas fa-home"></i>
     </span>
-    <unicon :name="Unicons.BookOpen.name" height="18" />
-    <span v-if="showVisitedPages"></span>
-    <div class="relative flex-1">
-      <select
-        v-if="showVisitedPages"
-        v-model="selectedVisitedPage"
-        @change="onVisitedPageChange($event.target.value)"
-        class="w-full py-2 px-4 rounded border-0 text-gray-700 font-medium text-center appearance-none bg-transparent"
-      >
-        <option
-          v-for="page in visitedPagesOptions"
-          :key="page.value"
-          :value="page.value"
-          class="text-gray-700"
+    <span v-if="showVisitedPages">
+      <div class="relative w-full">
+        <select
+          v-model="selectedVisitedPage"
+          @change="onVisitedPageChange($event.target.value)"
+          class="py-2 px-4 rounded border-0 text-gray-700 font-medium text-center appearance-none bg-transparent"
         >
-          {{ page.label }}
-        </option>
-      </select>
-    </div>
+          <option
+            v-for="page in visitedPagesOptions"
+            :key="page.value"
+            :value="page.value"
+            class="flex flex-row items-center justify-center bg-neutral-light p-2 gap-2 w-250 h-120 rounded-lg "
+          >    
+            <unicon :name="Unicons.BookOpen.name" height="18" />
+            {{ page.label }}
+          </option>
+        </select>
+      </div>
+    </span>
   </div>
   <span v-if="showEntityTitle" class="mx-2 text-gray-400">&gt;</span>
-  <span v-if="showEntityTitle" class="text-gray-700">{{ entityTitle }}</span>
+  <span v-if="showEntityTitle" class="text-gray-700 font-bold">
+    {{ entityTitle }}
+  </span>
 </template>
 
 <script lang="ts" setup>
+import { defineProps } from "vue";
 import { useBreadcrumb } from "@/composables/useBreadcrumb";
 import { Unicons, DamsIcons } from "@/types";
+
+const props = defineProps<{
+  icon: DamsIcons;
+}>();
 
 const {
   visitedPagesOptions,
