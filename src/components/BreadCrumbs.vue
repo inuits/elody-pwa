4c4dc2933451c8fd607162f80c376eb80a2e7bf4
Bjarne <bjarne@inuits.eu>
2023-04-13 09:24:13 +0200

    pnpm run lint

--- src/components/BreadCrumbs.vue
+++ src/components/BreadCrumbs.vue
@@ -1,19 +1,32 @@
 <template>
-  <div class="flex flex-row items-center justify-center bg-neutral-light p-2 gap-2 w-250 h-120 rounded-lg relative">
+  <div
+    class="flex flex-row items-center justify-center bg-neutral-light p-2 gap-2 w-250 h-120 rounded-lg relative"
+  >
     <span class="text-gray-700">
       <i class="fas fa-home"></i>
     </span>
     <div>
-      <button @click="isOpen = !isOpen"
-        class="pr-2.5 py-2 text-sm text-gray-800 text-right hover:bg-gray-100 cursor-pointer">
+      <button
+        @click="isOpen = !isOpen"
+        class="pr-2.5 py-2 text-sm text-gray-800 text-right hover:bg-gray-100 cursor-pointer"
+      >
         {{ entityTitle || "Home" }}
-        <svg v-if="isOpen" class="w-3 h-0">
-        </svg>
+        <svg v-if="isOpen" class="w-3 h-0"></svg>
       </button>
-      <ul v-show="isOpen" class="dropdown-menu pr-4 pb-4 bg-neutral-light rounded-lg w-full absolute top-10 left-0">
-        <li v-for="page in visitedPagesOptions" :key="page.value" @click="onVisitedPageChange(page.value)"
-          class="text-sm text-gray-800 text-right hover:bg-gray-100 cursor-pointer">
-          <unicon v-if="page.label != entityTitle" :name="Unicons.AngleUp.name" />
+      <ul
+        v-show="isOpen"
+        class="dropdown-menu pr-4 pb-4 bg-neutral-light rounded-lg w-full absolute top-10 left-0"
+      >
+        <li
+          v-for="page in visitedPagesOptions"
+          :key="page.value"
+          @click="onVisitedPageChange(page.value)"
+          class="text-sm text-gray-800 text-right hover:bg-gray-100 cursor-pointer"
+        >
+          <unicon
+            v-if="page.label != entityTitle"
+            :name="Unicons.AngleUp.name"
+          />
           <div v-if="page.label != entityTitle">
             <unicon v-if="icon" :name="Unicons[icon].name" />
             {{ page.label }}
@@ -25,7 +38,9 @@
   <span v-if="showEntityTitle" class="mx-2 mt-1 text-gray-400">
     <unicon :name="Unicons.AngleRight.name" />
   </span>
-  <span v-if="showEntityTitle" class="text-gray-700 font-bold">{{ entityTitle }}</span>
+  <span v-if="showEntityTitle" class="text-gray-700 font-bold">{{
+    entityTitle
+  }}</span>
 </template>
 
 <script lang="ts" setup>
@@ -39,7 +54,6 @@ const props = defineProps<{
 
 const {
   visitedPagesOptions,
-  showVisitedPages,
   entityTitle,
   showEntityTitle,
   onVisitedPageChange,
@@ -48,6 +62,8 @@ const {
 const isOpen = ref(false);
 </script>
 
-<style scoped lang="css">.dropdown-menu {
+<style scoped lang="css">
+.dropdown-menu {
   @apply absolute z-0;
-}</style>
+}
+</style>
