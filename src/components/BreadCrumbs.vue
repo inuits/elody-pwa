da02b1efdf87af8191814257e41d047bf12402d8
Bjarne <bjarne@inuits.eu>
2023-05-08 09:54:06 +0200

    made breadcrumb dynamic

--- src/components/BreadCrumbs.vue
+++ src/components/BreadCrumbs.vue
@@ -1,51 +1,26 @@
 <template>
-  <div
-    class="flex flex-row items-center justify-center bg-neutral-light p-2 gap-2 w-250 h-120 rounded-lg relative"
-  >
+  <div class="flex flex-row pt-3 items-center justify-center bg-neutral-light p-2 gap-2 w-250 h-120 rounded-lg relative">
     <span class="text-gray-700">
       <i class="fas fa-home"></i>
     </span>
-    <button
-      @click="isOpen = !isOpen"
-      class="py-2 text-sm text-gray-800 text-right hover:bg-gray-100 cursor-pointer"
-    >
+    <button @click="isOpen = !isOpen" class="py-2 text-sm text-gray-800 text-right hover:bg-gray-100 cursor-pointer">
       <div v-if="visitedPagesOptions.length > 0">
-        <div v-if="visitedPagesOptions.length > 0">
-          <div
-            v-if="visitedPagesOptions[0].label === 'Home'"
-            class="flex items-center"
-          >
-            <unicon :name="Unicons.BookOpen.name" class="w-6 h-6 mr-2" />
-          </div>
-          <div
-            v-else-if="visitedPagesOptions[0].label === 'Mediafiles'"
-            class="flex items-center"
-          >
-            <unicon :name="Unicons.Image.name" class="w-6 h-6 mr-2" />
-          </div>
-          <div
-            v-else-if="visitedPagesOptions[0].label === 'History'"
-            class="flex items-center"
-          >
-            <unicon :name="Unicons.History.name" class="w-6 h-6 mr-2" />
-          </div>
-          <div v-else>
-            {{ entityTitle }}
+        <div v-for="menuItem in menuItems">
+          <div v-if="visitedPagesOptions[0].label ===
+            menuItem.typeLink?.route?.destination
+            ">
+            <unicon :name="Unicons[menuItem.icon].name" class="w-6 h-6 mr-2" />
           </div>
         </div>
       </div>
+      <div v-else>
+        {{ entityTitle }}
+      </div>
       <svg v-if="isOpen" class="w-3 h-0"></svg>
     </button>
-    <ul
-      v-show="isOpen"
-      class="dropdown-menu pr-4 pb-4 bg-neutral-light rounded-lg w-full absolute top-10 left-0"
-    >
-      <li
-        v-for="page in visitedPagesOptions"
-        :key="page.value"
-        @click="onVisitedPageChange(page.value)"
-        class="text-sm text-gray-800 text-right hover:bg-gray-100 cursor-pointer"
-      >
+    <ul v-show="isOpen" class="dropdown-menu pr-4 pb-4 bg-neutral-light rounded-lg w-full absolute top-10 left-0">
+      <li v-for="page in visitedPagesOptions" :key="page.value" @click="onVisitedPageChange(page.value)"
+        class="text-sm text-gray-800 text-right hover:bg-gray-100 cursor-pointer">
         <unicon v-if="page.label != entityTitle" :name="Unicons.AngleUp.name" />
         <div v-if="page.label != entityTitle">
           {{ page.label }}
@@ -53,11 +28,20 @@
       </li>
     </ul>
   </div>
-  <span v-if="showEntityTitle" class="mx-2 mt-1 text-gray-400">
+  <span class="mx-2 mt-1 text-gray-400">
     <unicon :name="Unicons.AngleRight.name" />
   </span>
-  <span class="text-gray-700 font-bold">
-    <div>
+  <span class="font-serif text-xl leading-8 text-gray-800 font-bold">
+    <div v-if="entityTitle === ''">
+      <div v-for="menuItem in menuItems">
+        <div v-if="visitedPagesOptions[0].label ===
+          menuItem.typeLink?.route?.destination
+          ">
+          {{ menuItem.typeLink.route.destination }}
+        </div>
+      </div>
+    </div>
+    <div v-else>
       {{ entityTitle }}
     </div>
   </span>
@@ -67,6 +51,7 @@
 import { ref } from "vue";
 import { useBreadcrumb } from "@/composables/useBreadcrumb";
 import { Unicons } from "@/types";
+import useMenuHelper from "@/composables/useMenuHelper";
 
 const {
   visitedPagesOptions,
@@ -75,6 +60,8 @@ const {
   onVisitedPageChange,
 } = useBreadcrumb();
 
+const { getMenuEntities, menuItems } = useMenuHelper();
+getMenuEntities();
 const isOpen = ref(false);
 </script>
 
--- src/composables/useMenuHelper.ts
+++ src/composables/useMenuHelper.ts
@@ -2,16 +2,23 @@ import {
   type MenuItem,
   TypeModals,
   ModalChoices,
+  type GetMenuQueryVariables,
+  type GetMenuQuery,
+  GetMenuDocument,
 } from "@/generated-types/queries";
-import { useRouter } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import { useAvailableModals } from "@/composables/useAvailableModals";
-import { ref } from "vue";
+import { reactive, ref } from "vue";
+import { useQuery } from "@vue/apollo-composable";
+import { getEntityIdFromRoute } from "@/helpers";
 const { getModal } = useAvailableModals();
 const selectedMenuItem = ref<String | "no-item-selected">("no-item-selected");
+const menuItems = ref<Array<MenuItem>>([]);
 
 export const useMenuHelper = () => {
   const router = useRouter();
   const showdropdown = ref(false);
+
   const checkIfRouteOrModal = (_menuItem: MenuItem): void => {
     if (_menuItem.typeLink.modal) {
       getModal(_menuItem.typeLink.modal.typeModal as TypeModals).openModal(
@@ -39,6 +46,22 @@ export const useMenuHelper = () => {
     selectedMenuItem.value = "no-item-selected";
   };
 
+  const queryVariables = reactive<GetMenuQueryVariables>({ name: "main-menu" });
+  const { result: menuQueryResult, onResult } = useQuery<GetMenuQuery>(
+    GetMenuDocument,
+    queryVariables
+  );
+  const getMenuEntities = () => {
+    onResult((value) => {
+      menuItems.value = Object.values(value.data.Menu?.menu || {}).filter(
+        (menu) => menu.typeLink
+      );
+      getEntityIdFromRoute
+      console.log(menuItems.value)
+    });
+    console.log("This is the route",getEntityIdFromRoute())
+  }
+
   return {
     checkIfRouteOrModal,
     showdropdown,
@@ -46,6 +69,8 @@ export const useMenuHelper = () => {
     selectedMenuItem,
     isMenuItemActive,
     resetSelectedMenuItem,
+    getMenuEntities,
+    menuItems
   };
 };
 
