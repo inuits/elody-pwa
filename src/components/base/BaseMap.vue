d9920db9dae479a4f3b973d510247a4579a28383
Berend Vandenbussche <berend@inuits.eu>
2023-05-16 16:55:08 +0200

    Refs #111803 add name tooltip to marker

--- src/components/base/BaseMap.vue
+++ src/components/base/BaseMap.vue
@@ -5,14 +5,16 @@
         :use-global-leaflet="false"
         ref="map"
         v-model:zoom="zoom"
-        :center="convertedCoordinates"
+        :center="parsedMapData.coordinates"
       >
         <l-tile-layer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           layer-type="base"
           name="OpenStreetMap"
         ></l-tile-layer>
-        <l-marker :lat-lng="convertedCoordinates"></l-marker>
+        <l-marker :lat-lng="parsedMapData.coordinates"
+          ><l-tooltip>{{ parsedMapData.name }}</l-tooltip></l-marker
+        >
       </l-map>
     </div>
     <div class="info bg-neutral-0 w-full p-4 z-50">
@@ -28,7 +30,7 @@
 
 <script lang="ts" setup>
 import "leaflet/dist/leaflet.css";
-import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
+import { LMap, LTileLayer, LMarker, LTooltip } from "@vue-leaflet/vue-leaflet";
 import { computed, ref } from "vue";
 import EntityElementMetadata from "../EntityElementMetadata.vue";
 import type { MetadataAndRelation } from "@/generated-types/queries";
@@ -39,18 +41,21 @@ const props = defineProps<{
 
 const zoom = ref<number>(15);
 
-const convertedCoordinates = computed(() =>
-  props.metadata
-    .find((dataItem) => dataItem.key === "location")
-    ?.value.split(",")
-);
+const parsedMapData = computed(() => {
+  return {
+    coordinates: props.metadata
+      .find((dataItem) => dataItem.key === "location")
+      ?.value.split(","),
+    name: props.metadata.find((dataItem) => dataItem.key === "name")?.value,
+  };
+});
 </script>
 
 <style scoped>
 .map {
-  height: 85%;
+  height: 80%;
 }
 .info {
-  height: 15%;
+  height: 20%;
 }
 </style>
