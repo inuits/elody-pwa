04773ee1a37589048ede544635ed8e19bc90195b
eray <eray@inuits.eu>
2023-05-16 19:29:17 +0200

    Refs #104976 add plain BulkOperationModal

---
+++ src/components/bulk-operations/BulkOperationsModal.vue
@@ -0,0 +1,18 @@
+<template>
+  <BaseModal
+    :modal-state="modal.modalState.value.state"
+    modal-position="right"
+    modal-width-style="w-5/12"
+    @hide-modal="modal.closeModal()"
+  >
+  </BaseModal>
+</template>
+
+<script lang="ts" setup>
+import BaseModal from "@/components/base/BaseModal.vue";
+import { TypeModals } from "@/generated-types/queries";
+import { useAvailableModals } from "@/composables/useAvailableModals";
+
+const { getModal } = useAvailableModals();
+const modal = getModal(TypeModals.BulkOperations);
+</script>
