d44ea6a2f2e6451b09f38ab36fa8490c4618197d
Bjarne <bjarne@inuits.eu>
2023-05-22 16:34:42 +0200

    added last changes

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,37 +1,42 @@
 <template>
-  <div class="float-right">
-    <form>
-      <div class="flex">
-        <div class="relative w-full">
-          <input
-            type="search"
-            id="search"
-            class="rounded-lg"
-            placeholder=""
-            required
-          />
-          <button
-            type="submit"
-            class="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-#052737 rounded-lg border"
-            style="background-color: #052737"
-          >
-            <svg
-              aria-hidden="true"
-              class="w-5 h-5"
-              fill="none"
-              stroke="#ffffff"
-              viewBox="0 0 24 24"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              ></path>
-            </svg>
-          </button>
-        </div>
-      </div>
-    </form>
+  <div class="flex items-center bg-neutral-10 rounded-md p-2" @click="openModal">
+    <InputField placeholder="Search" v-model="searchTerm" class="h-full rounded-l-lg" />
+    <button type="submit" class="p-2.5 text-sm font-medium text-white bg-#6BC6B3 rounded-r-lg border z-0"
+      style="background-color: #6BC6B3; padding-top: 0.500rem; padding-bottom: 0.500rem;">
+      <svg aria-hidden="true" class="w-5 h-5 mx-auto" fill="none" stroke="#ffffff" viewBox="0 0 24 24">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+      </svg>
+    </button>
   </div>
 </template>
+
+<script lang="ts">
+import InputField from './base/InputField.vue';
+import SearchModal from './SearchModal.vue';
+
+export default {
+  name: "SearchBar",
+  components: {
+    InputField,
+    SearchModal,
+  },
+  data() {
+    return {
+      searchTerm: "",
+    };
+  },
+  methods: {
+    openModal() {
+      this.$emit("click");
+      this.$nextTick(() => {
+        const modal = this.$refs.modal;
+        modal.modalState.value.state = 'Show';
+      });
+    },
+    search() {
+      this.$emit("search", this.searchTerm);
+    },
+  },
+};
+</script>
---
+++ src/components/SearchModal.vue
@@ -0,0 +1,17 @@
+<template>
+  <BaseModal ref="modal" :modal-state="modal.modalState.value.state" modal-position="right" modal-width-style="w-11/12"
+    @hide-modal="modal.closeModal()">
+    <AssetMediaFileLibrary v-if="modal.modalState.value.state === 'Show'" />
+  </BaseModal>
+</template>
+
+<script lang="ts" setup>
+import BaseModal from "./base/BaseModal.vue";
+import AssetMediaFileLibrary from "./AssetMediaFileLibrary.vue";
+import { TypeModals } from "@/generated-types/queries";
+import { useAvailableModals } from "@/composables/useAvailableModals";
+
+const { getModal } = useAvailableModals();
+const modal = getModal(TypeModals.SimpleSearch);
+
+</script>
