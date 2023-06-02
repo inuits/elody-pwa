0a51869ddc5f879c8dddc3db4292dead5a1ca2ea
eray <eray@inuits.eu>
2023-06-02 11:09:25 +0200

    Refs #112605 get and handle filter-matcher mapping

--- src/components/filters-new/FiltersBase.vue
+++ src/components/filters-new/FiltersBase.vue
@@ -24,26 +24,41 @@
       </div>
     </div>
 
-    <FiltersListItem
-      v-for="filter in filters"
-      :key="filter.advancedFilter.key"
-      :filter="filter"
-    />
+    <div v-if="matchers.length > 0">
+      <FiltersListItem
+        v-for="filter in filters"
+        :key="filter.advancedFilter.key"
+        :filter="filter"
+        :matchers="
+          matchers.filter((option) =>
+            filterMatcherMapping[filter.advancedFilter.type].includes(
+              option.value
+            )
+          )
+        "
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import {
+  DamsIcons,
   GetAdvancedFiltersDocument,
+  GetFilterMatcherMappingDocument,
   type AdvancedFilter,
   type AdvancedFilters,
   type BaseEntity,
+  type DropdownOption,
+  type FilterMatcherMap,
   type GetAdvancedFiltersQuery,
+  type GetFilterMatcherMappingQuery,
   type Maybe,
 } from "@/generated-types/queries";
 import BaseButtonNew from "@/components/base/BaseButtonNew.vue";
 import FiltersListItem from "@/components/filters-new/FiltersListItem.vue";
 import { defineProps, ref } from "vue";
+import { useI18n } from "vue-i18n";
 import { useQuery } from "@vue/apollo-composable";
 
 export type FilterListItem = {
@@ -55,14 +70,43 @@ const props = defineProps<{
   entityType: string;
 }>();
 
+const { t } = useI18n();
+const filterMatcherMapping = ref<FilterMatcherMap>({
+  id: [],
+  text: [],
+  date: [],
+  number: [],
+  selection: [],
+  boolean: [],
+});
+const matchers = ref<DropdownOption[]>([]);
 const filters = ref<FilterListItem[]>([]);
 
-const { onResult } = useQuery<GetAdvancedFiltersQuery>(
+const { onResult: onFilterMatcherMappingResult } =
+  useQuery<GetFilterMatcherMappingQuery>(GetFilterMatcherMappingDocument);
+onFilterMatcherMappingResult((result) => {
+  filterMatcherMapping.value = result.data.FilterMatcherMapping;
+
+  const matcherSet = new Set<string>();
+  Object.values(filterMatcherMapping.value).forEach((matcherArray) => {
+    if (typeof matcherArray !== "string")
+      for (const matcher of matcherArray) matcherSet.add(matcher);
+  });
+
+  matchers.value = Array.from(matcherSet).map((matcher) => {
+    return {
+      icon: DamsIcons.NoIcon,
+      label: t(`filters.matchers.${matcher}`),
+      value: matcher,
+    };
+  });
+});
+
+const { onResult: onAdvancedFiltersResult } = useQuery<GetAdvancedFiltersQuery>(
   GetAdvancedFiltersDocument,
   { entityType: props.entityType }
 );
-
-onResult((result) => {
+onAdvancedFiltersResult((result) => {
   const advancedFilters: Maybe<AdvancedFilters> | undefined = (
     result.data.EntityTypeFilters as BaseEntity
   ).advancedFilters;
