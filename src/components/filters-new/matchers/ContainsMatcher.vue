289d37f1308fc6c69c3df4145ff3f4484b04c889
eray <eray@inuits.eu>
2023-06-05 12:56:16 +0200

    Refs #112605 add support for partial match text filtering

---
+++ src/components/filters-new/matchers/ContainsMatcher.vue
@@ -0,0 +1,36 @@
+<template>
+  <BaseInputTextNumber v-model="input" input-style="default" type="text" />
+</template>
+
+<script lang="ts" setup>
+import BaseInputTextNumber from "@/components/base/BaseInputTextNumber.vue";
+import type {
+  AdvancedFilter,
+  AdvancedFilterInput,
+} from "@/generated-types/queries";
+import { defineEmits, ref, watch } from "vue";
+
+const props = defineProps<{
+  filter: AdvancedFilter;
+}>();
+
+const emit = defineEmits<{
+  (
+    event: "newAdvancedFilterInput",
+    advancedFilterInput: AdvancedFilterInput
+  ): void;
+}>();
+
+const input = ref<string | number>("");
+
+watch(input, () =>
+  emit("newAdvancedFilterInput", {
+    type: props.filter.type,
+    key: props.filter.key,
+    value: input.value,
+    match_exact: false,
+  })
+);
+</script>
+
+<style></style>
