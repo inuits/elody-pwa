3c5c98f3fe80d3cfd2461cd0ed0614ba1251d074
eray <eray@inuits.eu>
2023-06-12 11:15:54 +0200

    Refs #112605 debug and fix bug causing baseLibrary to load twice in child routes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,3 @@
 <template>
-  <AssetLibrary />
   <router-view />
 </template>
-
-<script lang="ts">
-import { defineComponent } from "vue";
-import AssetLibrary from "@/components/AssetLibrary.vue";
-
-export default defineComponent({
-  name: "HomeView",
-  components: { AssetLibrary },
-});
-</script>
---
+++ src/views/HomeWrapper.vue
@@ -0,0 +1,9 @@
+<template>
+  <AssetLibrary />
+  <Home />
+</template>
+
+<script lang="ts" setup>
+import AssetLibrary from "@/components/AssetLibrary.vue";
+import Home from "@/views/Home.vue";
+</script>
