4dd98a5392efcaa573fddbb17370c56aa67daa3b
Berend Vandenbussche <berend@inuits.eu>
2023-06-20 14:25:22 +0200

    Refs #112043 Add posibility to update object type metadata

--- src/components/base/BaseMap.vue
+++ src/components/base/BaseMap.vue
@@ -5,7 +5,7 @@
         :use-global-leaflet="false"
         ref="map"
         v-model:zoom="zoom"
-        :center="parsedMapData.coordinates"
+        :center="(parsedMapData.coordinates as PointExpression)"
         @click="getMapCoordinates"
       >
         <l-tile-layer
@@ -13,7 +13,7 @@
           layer-type="base"
           name="OpenStreetMap"
         ></l-tile-layer>
-        <l-marker :lat-lng="parsedMapData.coordinates"
+        <l-marker :lat-lng="(parsedMapData.coordinates as LatLngExpression)"
           ><l-tooltip v-if="parsedMapData.name">{{
             parsedMapData.name
           }}</l-tooltip></l-marker
@@ -21,14 +21,21 @@
       </l-map>
     </div>
     <div class="info bg-neutral-0 w-full overflow-y-scroll">
-      <div v-for="data in parsedMetadata" class="px-2 py-1" :key="data.key">
+      <div v-for="data in mapData" class="px-2 py-1" :key="data.key">
         <EntityElementMetadata
           v-if="!isEdit || !data.field"
           :label="data.label"
           :value="data.value"
         ></EntityElementMetadata>
+        <entity-element-coordinate-edit
+          v-else-if="data.field && isEdit && data.unit === Unit.Coordinates"
+          :fieldKey="data.key"
+          :label="data.label"
+          v-model:value="data.value"
+          :field="data.field"
+        />
         <entity-element-metadata-edit
-          v-else-if="data.field"
+          v-else-if="data.field && isEdit"
           :fieldKey="data.key"
           :label="data.label"
           v-model:value="data.value"
@@ -42,38 +49,26 @@
 <script lang="ts" setup>
 import "leaflet/dist/leaflet.css";
 import { LMap, LTileLayer, LMarker, LTooltip } from "@vue-leaflet/vue-leaflet";
-import type { LeafletMouseEvent } from "leaflet";
+import type {
+  LatLngExpression,
+  LeafletMouseEvent,
+  PointExpression,
+} from "leaflet";
 import { computed, ref } from "vue";
 import EntityElementMetadata from "../EntityElementMetadata.vue";
 import EntityElementMetadataEdit from "../EntityElementMetadataEdit.vue";
-import type {
-  MediaFileElement,
-  MetadataAndRelation,
-  MetadataField,
-} from "@/generated-types/queries";
+import EntityElementCoordinateEdit from "../EntityElementCoordinateEdit.vue";
+import type { MediaFileElement } from "@/generated-types/queries";
 import { useEditMode } from "@/composables/useEdit";
+import { Unit } from "@/generated-types/queries";
 
 const props = defineProps<{
   element: MediaFileElement;
-  mapData: MetadataAndRelation[];
+  mapData: any[];
 }>();
 
 const zoom = ref<number>(15);
 const { isEdit } = useEditMode();
-
-const parsedMetadata = computed(() => {
-  const parsedData: any[] = [];
-  props.mapData.forEach((dataItem) => {
-    if (typeof dataItem.value === "string") {
-      parsedData.push(dataItem);
-    } else {
-      const newDataItem = { ...dataItem };
-      newDataItem.value = Object.values(dataItem.value).toString();
-      parsedData.push(newDataItem);
-    }
-  });
-  return parsedData;
-});
 
 const parsedMapData = computed(() => {
   const locationdata = props.mapData.find(
