c087e089df8264298733012a3d63ba4c8f8c95e8
Berend Vandenbussche <berend@inuits.eu>
2023-06-20 20:38:06 +0200

    Refs #113517 Add isAsc toggle to librarybar

--- src/components/toggles/SingleIconToggle.vue
+++ src/components/toggles/SingleIconToggle.vue
@@ -1,16 +1,16 @@
 <template>
   <div
     @click="toggleCheck"
-    class="rounded h-9 w-9 bg-neutral-0 cursor-pointer flex items-center justify-center mt-8"
+    class="rounded h-9 w-9 bg-neutral-0 cursor-pointer flex items-center justify-center"
   >
-    <div class="p-2" v-if="checked"><unicon :name="iconOn" /></div>
-    <div class="p-2" v-else><unicon :name="iconOff" /></div>
+    <div v-if="checked"><unicon :name="iconOn" /></div>
+    <div v-else><unicon :name="iconOff" /></div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { ref } from "vue";
-const props = defineProps({
+defineProps({
   label: { type: String, required: false },
   iconOn: { type: String, required: true },
   iconOff: { type: String, required: true },
