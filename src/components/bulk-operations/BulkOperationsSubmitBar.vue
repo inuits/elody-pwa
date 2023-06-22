3cc52c11da0130ccefacb0e18a8f7d614ba583a0
eray <eray@inuits.eu>
2023-06-22 15:36:37 +0200

    Refs #105046 use BulkOperationsSubmitBar in EditModal

--- src/components/bulk-operations/BulkOperationsSubmitBar.vue
+++ src/components/bulk-operations/BulkOperationsSubmitBar.vue
@@ -1,29 +1,44 @@
 <template>
-  <div class="flex items-center rounded-2xl p-3 h-full bg-neutral-white">
-    <div class="min-w-[10rem]">
+  <div
+    class="flex justify-between rounded-2xl p-3 w-full h-full bg-neutral-white"
+  >
+    <div class="flex items-center">
+      <div class="min-w-[10rem]">
+        <BaseButtonNew
+          class="font-bold"
+          :label="
+            buttonLabel
+              ? buttonLabel
+              : `${$t('bulk-operations.edit')} ${selectedItemsCount} ${$t(
+                  'bulk-operations.items'
+                )}`
+          "
+          :icon="buttonIcon"
+          :disabled="disabled"
+          button-style="accentAccent"
+          button-size="small"
+          @click="() => emit('submit')"
+        />
+      </div>
+      <div class="ml-5">
+        <span
+          class="text-text-body underline cursor-pointer select-none"
+          @click="() => emit('cancel')"
+        >
+          {{ $t("bulk-operations.cancel") }}
+        </span>
+      </div>
+    </div>
+
+    <div>
       <BaseButtonNew
         class="font-bold"
-        :label="
-          buttonLabel
-            ? buttonLabel
-            : `${$t('bulk-operations.edit')} ${selectedItemsCount} ${$t(
-                'bulk-operations.items'
-              )}`
-        "
-        :icon="buttonIcon"
-        :disabled="isDisabledButton"
-        button-style="accentAccent"
+        :label="$t('bulk-operations.delete')"
+        :icon="DamsIcons.Trash"
+        button-style="redDefault"
         button-size="small"
-        @click="() => emit('submit')"
+        @click="() => emit('delete')"
       />
-    </div>
-    <div class="ml-5">
-      <span
-        class="text-text-body underline cursor-pointer select-none"
-        @click="() => emit('cancel')"
-      >
-        {{ $t("bulk-operations.cancel") }}
-      </span>
     </div>
   </div>
 </template>
@@ -34,20 +49,22 @@ import BaseButtonNew from "@/components/base/BaseButtonNew.vue";
 
 withDefaults(
   defineProps<{
-    selectedItemsCount: number;
-    isDisabledButton: boolean;
-    buttonIcon: DamsIcons;
     buttonLabel?: string;
+    buttonIcon?: DamsIcons;
+    disabled?: boolean;
+    selectedItemsCount?: number;
   }>(),
   {
-    selectedItemsCount: 0,
-    buttonIcon: DamsIcons.Edit,
     buttonLabel: "",
+    buttonIcon: DamsIcons.Edit,
+    disabled: false,
+    selectedItemsCount: 0,
   }
 );
 
 const emit = defineEmits<{
   (event: "submit"): void;
   (event: "cancel"): void;
+  (event: "delete"): void;
 }>();
 </script>
