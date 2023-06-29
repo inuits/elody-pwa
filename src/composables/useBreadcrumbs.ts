0c1e9a7b2aae782fefba26221e7411e343f8c3d6
Berend Vandenbussche <berend@inuits.eu>
2023-06-29 16:30:04 +0200

    Fixes #112071 New breadcrumbs

---
+++ src/composables/useBreadcrumbs.ts
@@ -0,0 +1,40 @@
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+export type VisitedRoute = {
+  id: string;
+  routeName: string;
+};
+
+const currentRouteTitle = ref<string>("");
+const visitedRoutes = ref<VisitedRoute[]>([]);
+
+export const useBreadcrumbs = () => {
+  const setCurrentRouteTitle = (title: string): void => {
+    currentRouteTitle.value = title;
+  };
+
+  const addVisitedRoute = (route: VisitedRoute): void => {
+    if (!visitedRoutes.value.includes(route)) {
+      visitedRoutes.value.push(route);
+    }
+  };
+
+  const resetVisitedRoutes = (): void => {
+    visitedRoutes.value = [];
+  };
+
+  useRouter().afterEach((to) => {
+    currentRouteTitle.value = to.meta.title as string;
+    console.log(to);
+    if (to.name === "Home") resetVisitedRoutes();
+  });
+
+  return {
+    currentRouteTitle,
+    setCurrentRouteTitle,
+    resetVisitedRoutes,
+    addVisitedRoute,
+    visitedRoutes,
+  };
+};
--- src/composables/useMenuHelper.ts
+++ src/composables/useMenuHelper.ts
@@ -10,40 +10,34 @@ import { useRouter } from "vue-router";
 import { useAvailableModals } from "@/composables/useAvailableModals";
 import { reactive, ref } from "vue";
 import { useQuery } from "@vue/apollo-composable";
-import { getEntityIdFromRoute } from "@/helpers";
 const { getModal } = useAvailableModals();
-const selectedMenuItem = ref<String | "no-item-selected">("no-item-selected");
+const selectedMenuItem = ref<MenuItem | undefined>(undefined);
 const menuItems = ref<Array<MenuItem>>([]);
 
 export const useMenuHelper = () => {
   const router = useRouter();
   const showdropdown = ref(false);
 
+  const setSelectedMenuItem = (menuItem: MenuItem) => {
+    selectedMenuItem.value = menuItem;
+  };
+
   const checkIfRouteOrModal = (_menuItem: MenuItem): void => {
-    if (_menuItem.typeLink.modal) {
+    if (_menuItem?.typeLink?.modal) {
       getModal(_menuItem.typeLink.modal.typeModal as TypeModals).openModal(
         ModalChoices.Import
       );
-    } else if (_menuItem.typeLink.route) {
+    } else if (_menuItem?.typeLink?.route) {
       router.push(`/${_menuItem.typeLink.route.destination}`);
     }
   };
+
   const toggleDropDown = () => {
     showdropdown.value = !showdropdown.value;
   };
-  const isMenuItemActive = (menuItem: MenuItem): boolean => {
-    if (
-      selectedMenuItem.value === menuItem.typeLink.route?.destination ||
-      selectedMenuItem.value === menuItem.typeLink.modal?.typeModal
-    ) {
-      return true;
-    } else {
-      showdropdown.value = false;
-      return false;
-    }
-  };
+
   const resetSelectedMenuItem = () => {
-    selectedMenuItem.value = "no-item-selected";
+    selectedMenuItem.value = undefined;
   };
 
   const queryVariables = reactive<GetMenuQueryVariables>({ name: "main-menu" });
@@ -51,18 +45,18 @@ export const useMenuHelper = () => {
   const getMenuEntities = () => {
     onResult((value) => {
       menuItems.value = Object.values(value.data?.Menu?.menu || {}).filter(
-        (menu) => menu.typeLink
+        (menu) => menu?.typeLink
       );
-      getEntityIdFromRoute;
+      setSelectedMenuItem(menuItems.value[0]);
     });
   };
 
   return {
+    setSelectedMenuItem,
     checkIfRouteOrModal,
     showdropdown,
     toggleDropDown,
     selectedMenuItem,
-    isMenuItemActive,
     resetSelectedMenuItem,
     getMenuEntities,
     menuItems,
