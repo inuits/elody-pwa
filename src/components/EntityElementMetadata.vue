57abf2660e4c2fcb066e366e9e723d8fee709b9b
Berend Vandenbussche <berend@inuits.eu>
2023-07-18 16:31:10 +0200

    Show array items underneath each other and create links

--- src/components/EntityElementMetadata.vue
+++ src/components/EntityElementMetadata.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="text-sm pl-4">
     <p class="text-text-light w-full">{{ label }}</p>
-    <p class="w-full">{{ readableValue || "-" }}</p>
+    <div v-if="Array.isArray(readableValue)">
+      <div v-for="item in readableValue" :key="item">
+        <p v-if="!stringIsUrl(item)">{{ item }}</p>
+        <a v-else class="underline" target="_blank" :href="item">{{ item }}</a>
+      </div>
+    </div>
+    <div v-else>
+      <a
+        v-if="stringIsUrl(readableValue)"
+        class="underline"
+        target="_blank"
+        :href="readableValue"
+        >{{ readableValue }}</a
+      >
+      <p v-else>{{ readableValue || "-" }}</p>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import type { Unit } from "@/generated-types/queries";
-import { convertUnitToReadbleFormat } from "@/helpers";
+import { convertUnitToReadbleFormat, stringIsUrl } from "@/helpers";
 import { computed } from "vue";
 
 const props = defineProps<{
