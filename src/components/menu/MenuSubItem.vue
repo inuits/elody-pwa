bbd951274afd26a05a347fe171d3f45fab619874
Berend Vandenbussche <berend@inuits.eu>
2023-07-20 15:41:30 +0200

    Refs #114771 Cleanub submenu code & fix ts errors

--- src/components/menu/MenuSubItem.vue
+++ src/components/menu/MenuSubItem.vue
@@ -2,20 +2,17 @@
   <router-link
     v-if="show === true"
     :to="`${destination}`"
-    class="flex flex-column items-center cursor-pointer ml-9 mt-1 origin-top-center"
+    class="flex flex-column items-center cursor-pointer ml-9 mt-1 origin-top-center hover:text-accent-accent"
   >
-    <router-link
-      :to="`${destination}`"
-      class="nav-item-label w-0 h-0 overflow-hidden px-4 cursor-pointer"
-    >
-      {{ labelName }}</router-link
-    >
+    <p class="nav-item-label w-0 h-0 overflow-hidden px-4 cursor-pointer">
+      {{ labelName }}
+    </p>
   </router-link>
 </template>
 
 <script lang="ts" setup>
 import { defineProps } from "vue";
-const props = defineProps({
+defineProps({
   show: {
     type: Boolean,
     default: false,
