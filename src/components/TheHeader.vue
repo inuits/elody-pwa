45dd50104bb1f9f97bd2f3e1ba5a4eefddac883f
siltn depuydt <siltn@inuits.eu>
2023-08-10 15:10:01 +0200

    added page LanguageSelect which works as language switcher fixes #110373

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,12 +1,11 @@
 <template>
-  <div
-    class="flex items-center z-10 mx-6 my-8 px-6 py-4 rounded-3xl bg-neutral-white"
-  >
+  <div class="flex items-center z-10 mx-6 my-8 px-6 py-4 rounded-3xl bg-neutral-white">
     <div class="flex w-full items-center">
       <BreadCrumbs />
       <EditToggle v-if="auth.isAuthenticated.value === true" />
     </div>
     <SearchBar v-if="showSearch" />
+    <LanguageSelect />
   </div>
 </template>
 
@@ -16,6 +15,7 @@ import { useAuth } from "session-vue-3-oidc-library";
 import SearchBar from "./SearchBar.vue";
 import EditToggle from "./EditButtons.vue";
 import { inject } from "vue";
+import LanguageSelect from "./LanguageSelect.vue";
 
 const auth = useAuth();
 const config: any = inject("config");
