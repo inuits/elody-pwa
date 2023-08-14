dfb77d55b18e846e4579cd70c6f35841df37d0f6
Jeroen <jeroenwouters@inuits.eu>
2023-08-14 17:32:00 +0200

    entities type hidden filters refresh bugfix

--- src/components/library/AssetLibrary.vue
+++ src/components/library/AssetLibrary.vue
@@ -12,14 +12,20 @@
         ? RouteNames.SingleMediafile
         : RouteNames.SingleEntity
     "
+    :filter-type="entityType"
   />
 </template>
 
 <script lang="ts" setup>
+import { computed } from "vue";
 import type { Context } from "@/composables/useBulkOperations";
 import BaseLibrary from "@/components/library/BaseLibrary.vue";
 import { RouteNames, SearchInputType } from "@/generated-types/queries";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
+
+const entityType = computed<string | "not-set">(() =>
+  route.meta.entityType ? (route.meta.entityType as string) : "not-set"
+);
 </script>
