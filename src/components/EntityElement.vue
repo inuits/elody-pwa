68792c0cc5d11e3adc1dbb9725941ee9f956f559
Jeroen <jeroenwouters@inuits.eu>
2023-08-17 09:20:43 +0200

    add promGraphElement for iotDevice

--- src/components/EntityElement.vue
+++ src/components/EntityElement.vue
@@ -28,6 +28,10 @@
         v-if="element.__typename === 'WindowElement'"
         :element="element"
       />
+      <entity-element-prom-graph
+        v-if="element.__typename === 'PromGraphElement'"
+        :element="element"
+      />
     </div>
   </div>
 </template>
@@ -36,16 +40,22 @@ import { computed } from "vue";
 import EntityElementList from "./EntityElementList.vue";
 import EntityElementMedia from "./EntityElementMedia.vue";
 import EntityElementWindow from "./EntityElementWindow.vue";
+import EntityElementPromGraph from "./EntityElementPromGraph.vue";
 
 import type {
   EntityViewElements,
   WindowElement,
   MediaFileElement,
   EntityListElement,
+  GraphElement,
   Entity,
 } from "@/generated-types/queries";
 
-export type Elements = WindowElement | MediaFileElement | EntityListElement;
+export type Elements =
+  | WindowElement
+  | MediaFileElement
+  | EntityListElement
+  | GraphElement;
 
 const props = defineProps<{
   elements: EntityViewElements;
---
+++ src/components/EntityElementPromGraph.vue
@@ -0,0 +1,72 @@
+<template>
+  <entity-element-wrapper
+    :isCollapsed="element.isCollapsed"
+    :label="element.label"
+    class="flex flex-col h-full"
+  >
+    <template v-slot:actions />
+    <template v-slot:content>
+      <canvas class="bg-neutral-0" ref="canvasRef" id="chart"></canvas>
+    </template>
+  </entity-element-wrapper>
+</template>
+<script lang="ts" setup>
+import { ref, onMounted } from "vue";
+import Chart from "chart.js/auto";
+import "chartjs-adapter-date-fns";
+import ChartDatasourcePrometheusPlugin from "chartjs-plugin-datasource-prometheus";
+import { type PromGraphElement, PanelType } from "@/generated-types/queries";
+import EntityElementWrapper from "@/components/base/EntityElementWrapper.vue";
+import { getValueForPanelMetadata } from "@/helpers";
+import { useRoute } from "vue-router";
+
+const props = defineProps<{
+  element: PromGraphElement;
+}>();
+
+const canvasRef = ref<HTMLCanvasElement | null>(null);
+
+const getQuery = () => {
+  const pattern = /\$initialValues\.(\w+)/;
+  const matches: RegExpExecArray | null = pattern.exec(props.element.query);
+  if (matches) {
+    const desiredWordBehind: string = matches[1];
+    const valueFromMetadata = getValueForPanelMetadata(
+      PanelType.Metadata,
+      desiredWordBehind,
+      useRoute().params.id
+    );
+    return props.element.query.replace(pattern, valueFromMetadata);
+  } else {
+    return props.element.query;
+  }
+};
+
+onMounted(() => {
+  Chart.registry.plugins.register(ChartDatasourcePrometheusPlugin);
+  if (canvasRef.value !== null) {
+    new Chart(canvasRef.value, {
+      type: "line",
+      plugins: [ChartDatasourcePrometheusPlugin],
+      options: {
+        plugins: {
+          "datasource-prometheus": {
+            prometheus: {
+              endpoint: "/api/prom",
+              baseURL: "/", // default value
+            },
+            query: getQuery(),
+            timeRange: {
+              type: "relative",
+
+              // from 12 hours ago to now
+              start: -12 * 60 * 60 * 1000,
+              end: 0,
+            },
+          },
+        },
+      },
+    });
+  }
+});
+</script>
