0a88a4fece6920ad1086423cb4229069e60dd2be
Eray Özcan <eray@inuits.eu>
2023-09-08 22:09:06 +0200

    Use /tenants endpoint

--- src/composables/useTenant.ts
+++ src/composables/useTenant.ts
@@ -1,16 +1,14 @@
-import { ref, computed, watch } from "vue";
 import type { ApolloClient } from "@apollo/client/core";
 import {
-  GetEntitiesDocument,
-  SearchInputType,
   DamsIcons,
+  GetTenantsDocument,
   type DropdownOption,
-  type GetEntitiesQuery,
+  type GetTenantsQuery,
 } from "@/generated-types/queries";
 import { isObject } from "chart.js/helpers";
+import { ref, computed, watch } from "vue";
 
-const TENANT_ENTITY_TYPE = "tenant";
-const tenants = ref<Array<{ name: string; id: string }> | "no-tenants">(
+const tenants = ref<Array<{ id: string; name: string; }> | "no-tenants">(
   "no-tenants"
 );
 const selectedTenant = ref<DropdownOption | string | undefined>(undefined);
@@ -19,46 +17,26 @@ const useTenant = (apolloClient: ApolloClient<any>) => {
   const tenantsLoaded = ref<boolean>(false);
   const getTenants = () => {
     apolloClient
-      .query<GetEntitiesQuery>({
-        query: GetEntitiesDocument,
-        variables: {
-          limit: 9999,
-          skip: 1,
-          searchValue: {
-            value: "",
-            isAsc: false,
-            key: "title",
-            order_by: "",
-          },
-          advancedSearchValue: [],
-          advancedFilterInputs: [
-            {
-              type: "type",
-              key: "type",
-              value: TENANT_ENTITY_TYPE,
-              match_exact: true,
-            },
-          ],
-          searchInputType: SearchInputType.AdvancedInputType,
-        },
+      .query<GetTenantsQuery>({
+        query: GetTenantsDocument,
         fetchPolicy: "no-cache",
         notifyOnNetworkStatusChange: true,
       })
       .then((result) => {
-        const entities = result.data.Entities;
+        const entities = result.data.Tenants;
         entities &&
           entities.results &&
           entities.results.forEach((entity) => {
             if (entity && entity.__typename === "Tenant") {
-              const name = entity.intialValues.name as string;
               const id = entity.intialValues.id as string;
+              const name = entity.intialValues.name as string;
               if (tenants.value === "no-tenants") {
                 tenants.value = [];
               }
 
               tenants.value.push({
-                name,
                 id,
+                name,
               });
             }
           });
@@ -109,7 +87,6 @@ const useTenant = (apolloClient: ApolloClient<any>) => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          // 'Content-Type': 'application/x-www-form-urlencoded',
         },
         body: JSON.stringify({ tenant: selectedTenantValue.value }),
       });
