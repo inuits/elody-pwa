c21f5e7d57bfd049ab7eb011b7fd97b7ea1db5de
Berend Vandenbussche <berend@inuits.eu>
2023-09-21 11:01:00 +0200

    Fixes #117152 Show manifestviewers based on manifest version

--- src/components/entityElements/EntityElementManifestViewer.vue
+++ src/components/entityElements/EntityElementManifestViewer.vue
@@ -1,51 +1,80 @@
 <template>
   <entity-element-wrapper
-      :isCollapsed="element.isCollapsed"
-      :label="element.label"
-      class="h-[80vh]"
+    :isCollapsed="element.isCollapsed"
+    :label="element.label"
+    class="h-[80vh]"
   >
     <template v-slot:actions>
-      <div v-if="!isInManifestCollection" class="flex items-center text-text-subtitle ml-4 cursor-pointer" @click="addToManifestCollection">
+      <div
+        v-if="!isInManifestCollection"
+        class="flex items-center text-text-subtitle ml-4 cursor-pointer"
+        @click="addToManifestCollection"
+      >
         <unicon :name="Unicons.PlusCircle.name" :height="20"></unicon>
-        <p class="text-text-subtitle">{{t('manifest-viewer.add-to-manifest-collection')}}</p>
+        <p class="text-text-subtitle">
+          {{ t("manifest-viewer.add-to-manifest-collection") }}
+        </p>
       </div>
-      <div v-else class="flex items-center text-text-subtitle ml-4 cursor-pointer" @click="removeFromManifestCollection">
+      <div
+        v-else
+        class="flex items-center text-text-subtitle ml-4 cursor-pointer"
+        @click="removeFromManifestCollection"
+      >
         <unicon :name="Unicons.Minus.name" :height="20"></unicon>
-        <p class="text-text-subtitle">{{t('manifest-viewer.remove-from-manifest-collection')}}</p>
+        <p class="text-text-subtitle">
+          {{ t("manifest-viewer.remove-from-manifest-collection") }}
+        </p>
       </div>
     </template>
     <template v-slot:content>
       <ManifestViewer
-          v-if="element.manifestUrl"
-          :manifest-url="element.manifestUrl"
+        v-if="element.manifestUrl"
+        :manifest-url="element.manifestUrl"
+        :viewers="availableViewers"
       />
-      <div v-else><p>{{t('manifest-viewer.no-manifest')}}</p></div>
+      <div v-else>
+        <p>{{ t("manifest-viewer.no-manifest") }}</p>
+      </div>
     </template>
   </entity-element-wrapper>
 </template>
 <script lang="ts" setup>
 import ManifestViewer from "@/components/ManifestViewer.vue";
-import {useI18n} from "vue-i18n";
-import type {ManifestViewerElement} from "@/generated-types/queries";
+import { useI18n } from "vue-i18n";
+import type { ManifestViewerElement } from "@/generated-types/queries";
 import EntityElementWrapper from "@/components/base/EntityElementWrapper.vue";
-import {Unicons} from "@/types";
-import {BulkOperationsContextEnum, useBulkOperations} from "@/composables/useBulkOperations";
-import {computed} from "vue";
+import { Unicons } from "@/types";
+import {
+  BulkOperationsContextEnum,
+  useBulkOperations,
+} from "@/composables/useBulkOperations";
+import { computed } from "vue";
 
 const props = defineProps<{
-  element: ManifestViewerElement
-}>()
+  element: ManifestViewerElement;
+}>();
 
-const {t} = useI18n()
-const {enqueueItemForBulkProcessing, dequeueItemForBulkProcessing, isEnqueued} = useBulkOperations()
-const context = BulkOperationsContextEnum.ManifestCollection
-const isInManifestCollection = computed(() => isEnqueued(context, props.element.manifestUrl))
+const { t } = useI18n();
+const {
+  enqueueItemForBulkProcessing,
+  dequeueItemForBulkProcessing,
+  isEnqueued,
+} = useBulkOperations();
+const context = BulkOperationsContextEnum.ManifestCollection;
+const isInManifestCollection = computed(() =>
+  isEnqueued(context, props.element.manifestUrl)
+);
+const availableViewers = computed(() => {
+  const viewers: string[] = ["tify", "mirador"];
+  if (props.element.manifestVersion === 2) viewers.splice(0, 1);
+  return viewers;
+});
 
 const removeFromManifestCollection = () => {
-  dequeueItemForBulkProcessing(context, props.element.manifestUrl)
-}
+  dequeueItemForBulkProcessing(context, props.element.manifestUrl);
+};
 
 const addToManifestCollection = () => {
-  enqueueItemForBulkProcessing(context, {id: props.element.manifestUrl})
-}
+  enqueueItemForBulkProcessing(context, { id: props.element.manifestUrl });
+};
 </script>
