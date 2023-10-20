4ead9879e3688efc529da2ca7de5ca6052e8cc90
Berend Vandenbussche <berend@inuits.eu>
2023-10-20 12:15:32 +0200

    Fixes #114499 Add simpleSearch to pwa

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,37 +1,98 @@
 <template>
-  <div class="float-right">
-    <form>
-      <div class="flex">
-        <div class="relative w-full">
-          <input
-            type="search"
-            id="search"
-            class="rounded-lg"
-            placeholder=""
-            required
-          />
-          <button
-            type="submit"
-            class="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-#052737 rounded-lg border"
-            style="background-color: #052737"
-          >
-            <svg
-              aria-hidden="true"
-              class="w-5 h-5"
-              fill="none"
-              stroke="#ffffff"
-              viewBox="0 0 24 24"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              ></path>
-            </svg>
-          </button>
-        </div>
-      </div>
-    </form>
+  <div :class="['flex', { 'p-4 bg-neutral-lightest': inputEnabled }]">
+    <div class="flex w-full bg-neutral-0 rounded-lg">
+      <input
+        v-if="inputEnabled"
+        type="text"
+        class="w-full rounded-lg border-0"
+        v-model="inputValue"
+        @keydown.enter="applyFilterToLibrary"
+      />
+      <button
+        type="button"
+        class="
+          flex
+          justify-center
+          items-center
+          h-10
+          w-10
+          p-2.5
+          text-sm
+          font-medium
+          text-neutral-white
+          bg-accent-normal
+          rounded-lg
+        "
+        @click="
+          inputEnabled === false ? openSearchModal() : applyFilterToLibrary()
+        "
+      >
+        <unicon :name="Unicons.SearchGlass.name"></unicon>
+      </button>
+    </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { Unicons } from "@/types";
+import { useBaseModal } from "@/composables/useBaseModal";
+import {
+  AdvancedFilterInput,
+  AdvancedFilterTypes,
+  ModalState,
+  TypeModals,
+} from "@/generated-types/queries";
+import { computed, inject, ref, watch } from "vue";
+
+const props = withDefaults(
+  defineProps<{
+    inputEnabled?: boolean;
+  }>(),
+  {
+    inputEnabled: false,
+  }
+);
+
+const emit = defineEmits<{
+  (event: "updateFilters", filters: AdvancedFilterInput[]): void;
+}>();
+
+const config = inject("config") as any;
+const { openModal, getModalInfo } = useBaseModal();
+const inputValue = ref<string>("");
+const entityTypeFilters = computed(() =>
+  config.features.simpleSearch.simpleSearchEntityTypes.map((type: string) => {
+    return {
+      match_exact: true,
+      type: AdvancedFilterTypes.Type,
+      value: type,
+    };
+  })
+);
+
+const applyFilterToLibrary = () => {
+  const filters = [...entityTypeFilters.value];
+  const metadataKey = config.features.simpleSearch.simpleSearchMetadataKey;
+  filters.push({
+    key: metadataKey,
+    value: inputValue.value,
+    type: AdvancedFilterTypes.Text,
+    match_exact: false,
+    parent_key: "metadata",
+  });
+  emit("updateFilters", filters);
+};
+
+const openSearchModal = () => {
+  openModal(TypeModals.Search, undefined, "right");
+};
+
+watch(
+  () => getModalInfo(TypeModals.Search).state,
+  (modalState: ModalState) => {
+    if (modalState !== ModalState.Show) {
+      inputValue.value = "";
+    }
+  }
+);
+</script>
---
+++ src/components/SearchModal.vue
@@ -0,0 +1,47 @@
+<template>
+  <BaseModal
+    :modal-state="getModalInfo(TypeModals.Search).state"
+    :modal-position="getModalInfo(TypeModals.Search).modalPosition"
+    modal-width-style="w-7/12"
+    @hide-modal="closeModal(TypeModals.Search)"
+  >
+    <div class="bg-neutral-lightest h-full">
+      <search-bar
+        :input-enabled="true"
+        @updateFilters="updateBaseLibraryFilters"
+      />
+      <base-library
+        v-if="getModalInfo(TypeModals.Search).state === ModalState.Show"
+        :bulk-operations-context="BulkOperationsContextEnum.SearchModal"
+        list-item-route-name="SingleEntity"
+        :enable-advanced-filters="false"
+        :search-input-type-on-drawer="SearchInputType.AdvancedInputType"
+        :enable-bulk-operations="false"
+        :filters="filters"
+        :is-search-library="true"
+      ></base-library>
+    </div>
+  </BaseModal>
+</template>
+
+<script setup lang="ts">
+import BaseModal from "@/components/base/BaseModal.vue";
+import { useBaseModal } from "@/composables/useBaseModal";
+import {
+  AdvancedFilterInput,
+  ModalState,
+  SearchInputType,
+  TypeModals,
+} from "@/generated-types/queries";
+import BaseLibrary from "@/components/library/BaseLibrary.vue";
+import { BulkOperationsContextEnum } from "@/composables/useBulkOperations";
+import SearchBar from "@/components/SearchBar.vue";
+import { ref } from "vue";
+
+const { getModalInfo, closeModal } = useBaseModal();
+const filters = ref<AdvancedFilterInput[]>([]);
+
+const updateBaseLibraryFilters = (updatedFilters: AdvancedFilterInput[]) => {
+  filters.value = updatedFilters;
+};
+</script>
