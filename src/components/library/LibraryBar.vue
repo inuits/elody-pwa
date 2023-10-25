90828b97572b221d86814e5d8414163c8190420a
Eray Özcan <eray@inuits.eu>
2023-10-25 11:39:39 +0200

    Fixes #118516 go to first page after applying filter

--- src/components/library/LibraryBar.vue
+++ src/components/library/LibraryBar.vue
@@ -102,6 +102,9 @@ watch(isAsc, () => {
 watch(skip, () => {
   if (queryVariables.value) queryVariables.value.skip = skip.value;
 });
+watch(() => queryVariables.value?.skip, () => {
+  if (queryVariables.value.skip) skip.value = queryVariables.value.skip;
+});
 watch(
   () => getModalInfo(TypeModals.BulkOperations).state,
   (bulkOperationsModalState: ModalState) => {
