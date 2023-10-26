39425082eb7fecb7844f4f9c58aee3234088c7e8
Berend Vandenbussche <berend@inuits.eu>
2023-10-26 17:21:31 +0200

    Refs #118780 Add H tag styles back to markdown component

--- src/components/entityElements/EntityElementMarkdownViewer.vue
+++ src/components/entityElements/EntityElementMarkdownViewer.vue
@@ -4,12 +4,8 @@
     :label="element.label"
   >
     <template v-slot:content>
-      <div
-        id="app"
-        v-if="element.markdownContent"
-        class="h-full bg-neutral-light p-4"
-      >
-        <vue-markdown :source="element.markdownContent" />
+      <div v-if="element.markdownContent" class="h-full bg-neutral-light p-4">
+        <vue-markdown class="md" :source="element.markdownContent" />
       </div>
     </template>
   </entity-element-wrapper>
@@ -21,3 +17,30 @@ import VueMarkdown from "vue-markdown-render";
 
 const props = defineProps<{ element: MarkdownViewerElement }>();
 </script>
+
+<style>
+.md h1 {
+  font-size: 2em !important;
+  font-weight: bold !important;
+}
+.md h2 {
+  font-size: 1.5em !important;
+  font-weight: bold !important;
+}
+.md h3 {
+  font-size: 1.3em !important;
+  font-weight: bold !important;
+}
+.md h4 {
+  font-size: 1em !important;
+  font-weight: bold !important;
+}
+.md h5 {
+  font-size: 0.8em !important;
+  font-weight: bold !important;
+}
+.md h6 {
+  font-size: 0.7em !important;
+  font-weight: bold !important;
+}
+</style>
