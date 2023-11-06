d86fee2f3afac5dd134d826597f0c1db7d6c0594
Jonas Verschueren <jonasv@inuits.eu>
2023-11-06 16:26:40 +0100

    Refs #117724 Add mappings for permissions with can function to check

--- src/composables/usePermissions.ts
+++ src/composables/usePermissions.ts
@@ -1,33 +1,74 @@
 import {
-  Permission,
   GetUserPermissionsDocument,
+  GetPermissionMappingPerEntityDocument,
+  GetPermissionMappingEntitiesDocument,
+  type GetPermissionMappingPerEntityQuery,
+  type GetPermissionMappingPerEntityQueryVariables,
+  type GetPermissionMappingEntitiesQuery,
+  Permission,
+  Entitytyping,
 } from "@/generated-types/queries";
 import { useQuery } from "@vue/apollo-composable";
-import { ref } from "vue";
+import { reactive, ref } from "vue";
+import { apolloClient } from "@/main";
 
 const ignorePermissions = ref<boolean>(false);
 const setIgnorePermissions = (value: boolean) => {
   ignorePermissions.value = value;
 };
 
+const permissionsMappings = ref<Map<string, Map<Permission, boolean>>>(
+  new Map<string, Map<Permission, boolean>>()
+);
+const setPermissionsMappings = () => {
+  if (ignorePermissions.value) return;
+  permissionsMappings.value = new Map<string, Map<Permission, boolean>>();
+  for (const entity in Entitytyping) {
+    const permissions = new Map<Permission, boolean>();
+    try {
+      apolloClient
+        .query<GetPermissionMappingPerEntityQuery>({
+          query: GetPermissionMappingPerEntityDocument,
+          variables: reactive<GetPermissionMappingPerEntityQueryVariables>({
+            type: entity,
+          }),
+        })
+        .then((result) => {
+          permissions.set(
+            Permission.Canget,
+            result.data?.PermissionMappingPerEntity
+          );
+        });
+      permissionsMappings.value.set(entity, permissions);
+    } catch (e) {
+      console.log(
+        `Error in usePermissions set function for post entities/filter: ${e}`
+      );
+    }
+  }
+  try {
+    apolloClient
+      .query<GetPermissionMappingEntitiesQuery>({
+        query: GetPermissionMappingEntitiesDocument,
+      })
+      .then((result) => {
+        const permissions = new Map<Permission, boolean>();
+        for (let i = 0; i < result.data?.PermissionMappingEntities.length; i++)
+          permissions.set(
+            result.data?.PermissionMappingEntities[i].permission,
+            result.data?.PermissionMappingEntities[i].hasPermission
+          );
+        permissionsMappings.value.set("all_entities", permissions);
+      });
+  } catch (e) {
+    console.log(
+      `Error in usePermissions set function for put, patch & delete entities/id: ${e}`
+    );
+  }
+};
+
 const usePermissions = () => {
   const { result, loading } = useQuery(GetUserPermissionsDocument);
-
-  const canGet = (permissions: Permission[] | undefined) => {
-    if (!permissions) return false;
-    return permissions.includes(Permission.Canget);
-  };
-  const canEdit = (permissions: Permission[] | undefined) => {
-    if (!permissions) return false;
-    return (
-      permissions.includes(Permission.Canput) &&
-      permissions.includes(Permission.Canpatch)
-    );
-  };
-
-  const canDelete = (permissions: Permission[]) =>
-    permissions.includes(Permission.Candelete);
-
   const determinePermission = (perm: string) => {
     if (ignorePermissions.value) {
       return true;
@@ -38,14 +79,26 @@ const usePermissions = () => {
     return false;
   };
 
+  const can = (permission: Permission, entity: Entitytyping | undefined) => {
+    if (ignorePermissions.value) return true;
+    try {
+      if (entity === undefined && permission === Permission.Canget)
+        throw Error("For the canGet permission you have to specify an entity");
+      if (entity === undefined)
+        return permissionsMappings.value!.get("all_entities")!.get(permission);
+      if (permission !== Permission.Canget) entity = "all_entities";
+      return permissionsMappings.value!.get(entity)?.get(permission);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return {
-    canGet,
-    canDelete,
-    canEdit,
     determinePermission,
     loading,
     setIgnorePermissions,
+    can,
   };
 };
 
-export { usePermissions, setIgnorePermissions };
+export { usePermissions, setIgnorePermissions, setPermissionsMappings };
