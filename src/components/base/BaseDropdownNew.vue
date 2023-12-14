e844534bb8727a37f1d3336305eac4de1c366378
Berend Vandenbussche <berend@inuits.eu>
2023-12-14 15:50:46 +0100

    Refs #120315 Fix disabled style for dropdown when no options are available

--- src/components/base/BaseDropdownNew.vue
+++ src/components/base/BaseDropdownNew.vue
@@ -1,7 +1,7 @@
 <template>
-  <div v-if="options.length" :class="[labelPosition === 'inline' ? 'flex items-center' : undefined]">
+  <div :class="[labelPosition === 'inline' ? 'flex items-center' : undefined]">
   <p :class="['pr-2']" v-if="label">{{t(label)}}</p>
-  <select :class="['cursor-pointer', dropdownStyles[dropdownStyle].style, 'max-w-full']" v-model="selectedItemLabel">
+  <select :disabled="disable" :class="['cursor-pointer', dropdownStyles[dropdownStyle].style, 'max-w-full', {'opacity-50': disable}]" v-model="selectedItemLabel">
     <option v-for="option in [defaultOption, ...options]" :key="option.value" :value="option.label" @click="selectItem(option)" :class="[dropdownStyles[dropdownStyle].hoverStyle]">{{t(option.label)}}</option>
   </select>
   </div>
@@ -15,33 +15,27 @@ import { useI18n } from "vue-i18n";
 type Dropdown = {
   style: string
   hoverStyle: string;
-  disabledStyle: string;
 };
 
 const defaultDropdown: Dropdown = {
   style: "text-text-body bg-neutral-white border-none rounded-lg",
   hoverStyle: "hover:text-accent-accent hover:bg-neutral-lightest hover:border-none",
-  disabledStyle: "text-text-disabled bg-neutral-lightest border-none"
 };
 const defaultFullWidthDropdown: Dropdown = {
   style: "text-text-body bg-neutral-white border-none rounded-lg w-full",
   hoverStyle: "hover:text-accent-accent hover:bg-neutral-lightest hover:border-none",
-  disabledStyle: "text-text-disabled bg-neutral-lightest border-none"
 };
 const defaultWithBorderDropdown: Dropdown = {
   style: "text-text-body bg-neutral-white border-[rgba(0,58,82,0.6)] rounded-lg w-full",
   hoverStyle: "hover:text-accent-accent hover:bg-neutral-lightest hover:border-[rgba(0,58,82,0.6)]",
-  disabledStyle: "text-text-disabled bg-neutral-lightest border-text-disabled",
 };
 const accentAccentDropdown: Dropdown = {
   style: "text-neutral-white bg-accent-accent border-none rounded-lg",
   hoverStyle: defaultDropdown.hoverStyle,
-  disabledStyle: defaultDropdown.disabledStyle,
 };
 const neutralLightDropdown: Dropdown = {
   style: "text-text-body bg-neutral-light border-none rounded-lg",
   hoverStyle: "hover:text-[rgba(0,58,82,0.8)] hover:bg-neutral-lightest hover:border-none",
-  disabledStyle: defaultDropdown.disabledStyle,
 };
 
 type DropdownStyle =
