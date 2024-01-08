3f5b0b8cefeca8ff64b631dab4da066ffcf42b8a
Eray Özcan <eray@inuits.eu>
2024-01-08 16:03:44 +0100

    Refs #120636 check create permissions per type

--- src/composables/usePermissions.ts
+++ src/composables/usePermissions.ts
@@ -9,24 +9,29 @@ import {
   type GetPermissionMappingEntityDetailQueryVariables,
   Permission,
   Entitytyping,
+  type GetPermissionMappingCreateQueryVariables,
 } from "@/generated-types/queries";
-import { reactive, ref, watch } from "vue";
-import { apolloClient } from "@/main";
 import EventBus from "../EventBus";
+import { apolloClient } from "@/main";
+import { reactive, ref } from "vue";
 
 const ignorePermissions = ref<boolean>(false);
 const setIgnorePermissions = (value: boolean) => {
   ignorePermissions.value = value;
 };
+const permittedEntitiesToCreate = ref<Entitytyping[]>([]);
 
 const permissionsMappings = ref<Map<string, Map<Permission, boolean>>>(
   new Map<string, Map<Permission, boolean>>()
 );
+
 const setPermissionsMappings = () => {
   if (ignorePermissions.value) return;
   const { numberOfEntities } = usePermissions();
+
   Object.values(Entitytyping).forEach((entity, index) => {
     const permissions = new Map<Permission, boolean>();
+
     try {
       apolloClient
         .query<GetPermissionMappingPerEntityTypeQuery>({
@@ -34,6 +39,8 @@ const setPermissionsMappings = () => {
           variables: reactive<GetPermissionMappingPerEntityTypeQueryVariables>({
             type: entity,
           }),
+          fetchPolicy: "no-cache",
+          notifyOnNetworkStatusChange: true,
         })
         .then((result) => {
           permissions.set(
@@ -50,41 +57,41 @@ const setPermissionsMappings = () => {
         `Error in usePermissions set function for post entities/filter: ${e}`
       );
     }
+
+    try {
+      apolloClient
+        .query<GetPermissionMappingCreateQuery>({
+          query: GetPermissionMappingCreateDocument,
+          variables: reactive<GetPermissionMappingCreateQueryVariables>({
+            entityType: entity,
+          }),
+          fetchPolicy: "no-cache",
+          notifyOnNetworkStatusChange: true,
+        })
+        .then((result) => {
+          permissionsMappings.value.get(entity)?.set(Permission.Cancreate, result.data?.PermissionMappingCreate)
+          EventBus.emit("permissions_updated", permissionsMappings.value);
+        });
+    } catch (e) {
+      console.log(
+        `Error in usePermissions set function for create entities: ${e}`
+      );
+    }
   });
-  try {
-    apolloClient
-      .query<GetPermissionMappingCreateQuery>({
-        query: GetPermissionMappingCreateDocument,
-      })
-      .then((result) => {
-        const permissions = new Map<Permission, boolean>();
-        permissions.set(
-          Permission.Cancreate,
-          result.data?.PermissionMappingCreate
-        );
-        permissionsMappings.value.set("all_entities", permissions);
-        EventBus.emit("permissions_updated", permissionsMappings.value);
-      });
-  } catch (e) {
-    console.log(
-      `Error in usePermissions set function for create entities: ${e}`
-    );
-  }
 };
 
 const usePermissions = () => {
   const numberOfEntities = Object.keys(Entitytyping).length;
+
   const can = (permission: Permission, entity: Entitytyping | undefined) => {
     if (ignorePermissions.value) return true;
     try {
       if (permissionsMappings.value.size < 1)
         throw Error("The mappings are not fetched yet. Wait a bit.");
-      if (permission === Permission.Canread && entity != undefined) {
+      if ((permission === Permission.Canread || permission === Permission.Cancreate) && entity != undefined) {
         const entityMapping = permissionsMappings.value!.get(entity);
         return entityMapping?.get(permission);
       }
-      if (permission === Permission.Cancreate)
-        return permissionsMappings.value!.get("all_entities")!.get(permission);
       throw Error("There is something wrong with how this function is used");
     } catch (e) {
       // console.log(e);
@@ -100,6 +107,8 @@ const usePermissions = () => {
           variables: reactive<GetPermissionMappingEntityDetailQueryVariables>({
             id: id,
           }),
+          fetchPolicy: "no-cache",
+          notifyOnNetworkStatusChange: true,
         })
         .then((result) => {
           for (
@@ -129,8 +138,9 @@ const usePermissions = () => {
 };
 
 export {
-  usePermissions,
-  setPermissionsMappings,
-  setIgnorePermissions,
   ignorePermissions,
+  permittedEntitiesToCreate,
+  setIgnorePermissions,
+  setPermissionsMappings,
+  usePermissions,
 };
