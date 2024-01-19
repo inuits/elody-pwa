e39dbc9fc4a8819a27877c65f30908efe5ca52b0
Eray Özcan <eray@inuits.eu>
2024-01-19 15:55:43 +0100

    Fixes #121617 give more specific error message in all supported languages when entity creation fails

--- src/composables/useGraphqlErrors.ts
+++ src/composables/useGraphqlErrors.ts
@@ -1,11 +1,12 @@
 import type { ErrorResponse } from "@apollo/client/link/error";
+import type { GraphQLError } from "graphql/error";
 import {
   useNotification,
   NotificationType,
 } from "../components/base/BaseNotification.vue";
-import { useRouter } from "vue-router";
 import { auth } from "@/main";
-import type { GraphQLError } from "graphql/error";
+import { getApplicationDetails, i18n } from "@/helpers";
+import { useRouter } from "vue-router";
 
 const baseGraphQLError = {
   displayTime: 10,
@@ -22,7 +23,18 @@ const createErrorNotification = (title: string, description: string) => {
 };
 
 const useGraphqlErrors = (_errorResponse: ErrorResponse) => {
-  const handleErrorByCode = (errorMessage: number) => {
+  const handleErrorByCode = async (errorMessage: number) => {
+    const { config, translations } = await getApplicationDetails();
+    let language = config.customization.applicationLocale;
+
+    const displayPreferences = localStorage.getItem("_displayPreferences");
+    if (displayPreferences) {
+      const preferences = JSON.parse(displayPreferences);
+      if (preferences.lang) language = preferences.lang;
+    }
+
+    const { t } = i18n(translations, language).global;
+
     switch (errorMessage) {
       case 401:
         auth.logout();
@@ -32,15 +44,30 @@ const useGraphqlErrors = (_errorResponse: ErrorResponse) => {
         break;
       case 403:
         createErrorNotification(
-          "Forbidden",
-          "You don't have access to this page/action"
+          t("notifications.graphql-errors.forbidden.title"),
+          t("notifications.graphql-errors.forbidden.description")
         );
         useRouter().go(-1);
         break;
       case 409:
+        let errorMessage: string =
+          "notifications.graphql-errors.duplicate-upload";
+        let duplicateKey = "";
+
+        if (_errorResponse.operation.operationName == "createEntity") {
+          errorMessage = "notifications.graphql-errors.duplicate-entity";
+          const responseBody: string = (
+            _errorResponse.graphQLErrors?.[0].extensions.response as any
+          )?.body;
+          if (responseBody) {
+            const key = responseBody.match(/"([^"]*)"/)?.[0];
+            if (key) duplicateKey = `: ${key}`;
+          }
+        }
+
         createErrorNotification(
-          "Duplicate",
-          "Duplicate file detected, upload reverted"
+          t(`${errorMessage}.title`),
+          `${t(`${errorMessage}.description`)}${duplicateKey}`
         );
         break;
       default:
