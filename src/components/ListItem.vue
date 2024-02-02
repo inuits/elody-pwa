7317cd153f6c509f536cf11db9c42b85a5f31ddf
Jonas Verschueren <jonasv@inuits.eu>
2024-02-02 14:53:25 +0100

    Refs #122109 Hide the drag handle for ordering

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -17,12 +17,12 @@
       v-show="isEdit"
       v-if="onlyEditableTeaserMetadata.length > 0"
     >
-      <div class="pl-2">
-        <unicon
-          :name="Unicons.Bars.name"
-          class="h-5 w-5 text-neutral-700 rounded-sm outline-none shadow-sm self-center"
-        />
-      </div>
+<!--      <div class="pl-2">-->
+<!--        <unicon-->
+<!--          :name="Unicons.Bars.name"-->
+<!--          class="h-5 w-5 text-neutral-700 rounded-sm outline-none shadow-sm self-center"-->
+<!--        />-->
+<!--      </div>-->
       <div v-if="!loading" class="w-16 pr-2">
         <div
           v-for="metadataItem in onlyEditableTeaserMetadata"
