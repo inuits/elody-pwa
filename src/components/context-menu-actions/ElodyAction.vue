6b78a4406a448b6048dc2329bbb4972801266c7c
Jonas Verschueren <jonasv@inuits.eu>
2024-02-05 11:47:31 +0100

    Refs #121765 Make context menu more generic with icon

--- src/components/context-menu-actions/ElodyAction.vue
+++ src/components/context-menu-actions/ElodyAction.vue
@@ -2,7 +2,7 @@
   <base-context-menu-item
     @clicked="doAction()"
     :label="$t(label)"
-    :icon="Unicons.Trash.name"
+    :icon="Unicons[icon].name"
   />
 </template>
 
@@ -18,24 +18,25 @@ import { useI18n } from "vue-i18n";
 import useEditMode from "@/composables/useEdit";
 const { t } = useI18n();
 const { update } = useFieldArray("relationValues.relations");
-const { setEditMode, save } = useEditMode();
+const { save, disableEditMode } = useEditMode();
 
 const props = defineProps<{
   label: String;
+  icon: String
   action: ContextMenuElodyActionEnum;
   entityId: String;
   relation: object;
 }>();
 
-const deleteRelation = () => {
-  setEditMode();
+const deleteRelation = async () => {
   if (props.relation !== "no-relation-found") {
     update(props.relation.idx, {
       ...props.relation.relation,
       editStatus: EditStatus.Deleted,
     });
   }
-  save();
+  await save();
+  disableEditMode();
 };
 
 const doAction = () => {
