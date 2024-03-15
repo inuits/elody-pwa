1a4276401327e22f9f4fd3e11084d29ca3a54241
boriss <boriss@inuits.eu>
2024-03-15 16:23:19 +0100

    wip

--- src/components/base/VideoPlayer.vue
+++ src/components/base/VideoPlayer.vue
@@ -13,8 +13,7 @@
   </div>
 </template>
 <script lang="ts" setup>
-import { onMounted } from "vue";
-import { useAuth } from "session-vue-3-oidc-library";
+// import { useAuth } from "session-vue-3-oidc-library";
 import { useEntityMediafileSelector } from "@/composables/useEntityMediafileSelector";
 
 const props = defineProps<{
@@ -22,39 +21,9 @@ const props = defineProps<{
 }>();
 
 const { getValueOfMediafile } = useEntityMediafileSelector();
-const auth = useAuth();
+// const auth = useAuth();
 const fileName =
   getValueOfMediafile("filename") || getValueOfMediafile("transcode_filename");
-
-onMounted(() => {
-  fetchVideoFile();
-});
-
-const fetchVideoFile = async () => {
-  if (!auth.isAuthenticated.value) return;
-
-  fetch("/api/mediafile/" + fileName, {
-    method: "GET",
-    headers: {
-      "Content-Type": "video/mp4",
-      Authorization: `Bearer token`,
-    },
-  })
-    .then((response) => {
-      console.log("response: ", response);
-
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.blob();
-    })
-    .then((blob) => {
-      console.log(blob);
-    })
-    .catch((error) => {
-      console.error("Error fetching data:", error);
-    });
-};
 
 // fetch("/api/mediafile/" + fileName, {
 //   method: "GET",
