e244176672f9934ece94d9d8c6c819d66f1256ed
boriss <boriss@inuits.eu>
2024-03-15 16:15:02 +0100

    wip: fetch video

--- src/components/base/VideoPlayer.vue
+++ src/components/base/VideoPlayer.vue
@@ -2,38 +2,77 @@
   <div class="w-full relative">
     <video class="w-full h-full" controls>
       <source
-        :src="
-          source && (getValueOfMediafile('transcode_filename') || getValueOfMediafile('filename'))
-            ? '/api/mediafile/' + getValueOfMediafile('transcode_filename') || getValueOfMediafile('filename')
-            : 'no-src'
+        :src="source && fileName ? '/api/mediafile/' + fileName : 'no-src'"
+        :type="
+          source && getValueOfMediafile('mimetype')
+            ? getValueOfMediafile('mimetype')
+            : 'no-type'
         "
-        :type="source && getValueOfMediafile('mimetype') ? getValueOfMediafile('mimetype') : 'no-type'"
       />
     </video>
   </div>
 </template>
-<script lang="ts">
-import { defineComponent } from "vue";
-import type { PropType } from "vue";
+<script lang="ts" setup>
+import { onMounted } from "vue";
 import { useAuth } from "session-vue-3-oidc-library";
 import { useEntityMediafileSelector } from "@/composables/useEntityMediafileSelector";
 
-export default defineComponent({
-  name: "VideoPlayer",
-  components: {},
-  props: {
-    source: {
-      type: Object as PropType<any>,
-      required: true,
-    },
-  },
-  setup(props) {
-    const { getValueOfMediafile } = useEntityMediafileSelector();
-    const auth = useAuth();
-    const fileName = props.source?.initialValues?.['transcode_filename'] || props.source?.initialValues?.['transcode_filename']
-    return {
-      getValueOfMediafile
-    };
-  },
+const props = defineProps<{
+  source?: any;
+}>();
+
+const { getValueOfMediafile } = useEntityMediafileSelector();
+const auth = useAuth();
+const fileName =
+  getValueOfMediafile("filename") || getValueOfMediafile("transcode_filename");
+
+onMounted(() => {
+  fetchVideoFile();
 });
+
+const fetchVideoFile = async () => {
+  if (!auth.isAuthenticated.value) return;
+
+  fetch("/api/mediafile/" + fileName, {
+    method: "GET",
+    headers: {
+      "Content-Type": "video/mp4",
+      Authorization: `Bearer token`,
+    },
+  })
+    .then((response) => {
+      console.log("response: ", response);
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.blob();
+    })
+    .then((blob) => {
+      console.log(blob);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+};
+
+// fetch("/api/mediafile/" + fileName, {
+//   method: "GET",
+//   headers: {
+//     "Content-Type": "video/mp4",
+//     Token: auth.to,
+//   },
+// })
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error("Network response was not ok");
+//     }
+//     return response.blob();
+//   })
+//   .then((blob) => {
+//     // Handle the blob data here
+//   })
+//   .catch((error) => {
+//     console.error("Error fetching data:", error);
+//   });
 </script>
