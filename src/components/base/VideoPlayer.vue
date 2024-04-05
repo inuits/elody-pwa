de8e3c6f5146d7974863ff8cc22faaaad9a6ac87
boris <boriss@inuits.eu>
2024-04-05 15:41:21 +0200

    wip

--- src/components/base/VideoPlayer.vue
+++ src/components/base/VideoPlayer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full relative">
-    <video class="w-full h-full" controls>
+    <video class="w-full h-full" controls @error="handleError1">
       <source
         :src="source && fileName ? '/api/mediafile/' + fileName : 'no-src'"
         :type="
@@ -8,12 +8,14 @@
             ? getValueOfMediafile('mimetype')
             : 'no-type'
         "
+        @error="handleError"
       />
     </video>
   </div>
 </template>
 <script lang="ts" setup>
 import { useEntityMediafileSelector } from "@/composables/useEntityMediafileSelector";
+import useHttpErrors from "@/composables/useHttpErrors";
 
 defineProps<{
   source?: any;
@@ -22,4 +24,15 @@ defineProps<{
 const { getValueOfMediafile } = useEntityMediafileSelector();
 const fileName =
   getValueOfMediafile("filename") || getValueOfMediafile("transcode_filename");
+const { logFormattedErrors } = useHttpErrors();
+
+const handleError = (event: any) => {
+  console.log(event.target.parentNode.error);
+  console.log(event.target.parentNode.networkState);
+}
+
+const handleError1 = (event: any) => {
+  console.log(event.target.parentNode.error);
+  console.log(event.target.parentNode.networkState);
+}
 </script>
