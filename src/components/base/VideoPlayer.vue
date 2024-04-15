9b0d919daff88ab7b30d21df9b64bc1bdada7097
boris <boriss@inuits.eu>
2024-04-15 10:55:32 +0200

    remove unused code from video player

--- src/components/base/VideoPlayer.vue
+++ src/components/base/VideoPlayer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full relative">
-    <video class="w-full h-full" controls @error="handleError1">
+    <video class="w-full h-full" controls>
       <source
         :src="source && fileName ? '/api/mediafile/' + fileName : 'no-src'"
         :type="
@@ -8,14 +8,12 @@
             ? getValueOfMediafile('mimetype')
             : 'no-type'
         "
-        @error="handleError"
       />
     </video>
   </div>
 </template>
 <script lang="ts" setup>
 import { useEntityMediafileSelector } from "@/composables/useEntityMediafileSelector";
-import useHttpErrors from "@/composables/useHttpErrors";
 
 defineProps<{
   source?: any;
@@ -24,15 +22,4 @@ defineProps<{
 const { getValueOfMediafile } = useEntityMediafileSelector();
 const fileName =
   getValueOfMediafile("filename") || getValueOfMediafile("transcode_filename");
-const { logFormattedErrors } = useHttpErrors();
-
-const handleError = (event: any) => {
-  console.log(event.target.parentNode.error);
-  console.log(event.target.parentNode.networkState);
-}
-
-const handleError1 = (event: any) => {
-  console.log(event.target.parentNode.error);
-  console.log(event.target.parentNode.networkState);
-}
 </script>
