9ec2f95fa5b07739f35df15efe02a06ccd031bd1
boris <boriss@inuits.eu>
2024-05-15 15:27:49 +0200

    fix(login): verify auth on server when refresh or open in a new tab

--- src/main.ts
+++ src/main.ts
@@ -60,6 +60,8 @@ const start = async () => {
     await auth.processAuthCode(authCode);
     if (!config.allowAnonymousUsers && !auth.isAuthenticated.value)
       await auth.redirectToLogin();
+  } else if (config.allowAnonymousUsers) {
+    await auth.verifyServerAuth();
   }
 
   bulkSelectAllSizeLimit = config.bulkSelectAllSizeLimit;
