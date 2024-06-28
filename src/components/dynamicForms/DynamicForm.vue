db0d12d69c28db1aba5a41423a4307fd1380ed2d
boris <boriss@inuits.eu>
2024-06-28 16:42:57 +0200

    bad commit: another part of the work

--- src/components/dynamicForms/DynamicForm.vue
+++ src/components/dynamicForms/DynamicForm.vue
@@ -157,7 +157,7 @@ import {
 } from "@/generated-types/queries";
 import { useImport } from "@/composables/useImport";
 import { useDynamicForm } from "@/components/dynamicForms/useDynamicForm";
-import { computed, inject, ref, watch } from "vue";
+import { computed, inject, ref, watch, toRaw } from "vue";
 import SpinnerLoader from "@/components/SpinnerLoader.vue";
 import MetadataWrapper from "@/components/metadata/MetadataWrapper.vue";
 import UploadInterfaceDropzone from "@/components/UploadInterfaceDropzone.vue";
@@ -194,6 +194,8 @@ const props = withDefaults(
     modalFormFields: undefined,
   }
 );
+
+const emit = defineEmits(["entityCreated"]);
 
 type FormFieldTypes = UploadContainer | PanelMetaData | FormAction;
 
@@ -346,6 +348,21 @@ const submitActionFunction = async (field: FormAction) => {
   goToEntityPage(entity, "SingleEntity", props.router);
 };
 
+const submitWithExtraMetadata = async (field: FormAction) => {
+  await form.value.validate();
+  if (formContainsErrors.value) return;
+  const document = await getQuery(field.actionQuery as string);
+  const entityInput = createEntityFromFormInput(field.creationType);
+  entityInput.metadata?.push(...toRaw(props.savedContext));
+  const entity = (await performSubmitAction(document, entityInput)).data
+    .CreateEntity;
+  emit("entityCreated", { ...entity, metadata: entityInput.metadata });
+  await getTenants();
+  closeModal(TypeModals.DynamicForm);
+  changeExpandedState(false);
+  deleteForm(props.dynamicFormQuery);
+};
+
 const downloadActionFunction = async (field: FormAction) => {
   await form.value.validate();
   if (formContainsErrors.value) return;
@@ -433,6 +450,7 @@ const performActionButtonClickEvent = (field: FormAction): void => {
     download: () => downloadActionFunction(field),
     ocr: () => startOcrActionFunction(field),
     endpoint: () => callEndpointInGraphql(field),
+    submitWithExtraMetadata: () => submitWithExtraMetadata(field),
   };
   if (!field.actionType) return;
   showErrors.value = true;
