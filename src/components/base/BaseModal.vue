45aa958972c77630743394031479e1b7a6765f96
Berend Vandenbussche <berend@inuits.eu>
2024-08-07 12:56:18 +0200

    fix: fix modalStyling and entityPickerModal not loading
    Refs #128366

--- src/components/base/BaseModal.vue
+++ src/components/base/BaseModal.vue
@@ -29,7 +29,7 @@ const props = withDefaults(
     cancelButtonAvailabe?: boolean;
   }>(),
   {
-    modalHeightStyle: "h-[75vh] my-[12.5vh]",
+    modalHeightStyle: "max-h-[75vh] my-[12.5vh]",
     iconHeight: 18,
     modalColor: "bg-neutral-white",
     cancelButtonAvailabe: false,
@@ -39,44 +39,18 @@ const props = withDefaults(
 const emit = defineEmits(["update:modalState", "hideModal"]);
 
 const cancelButtonAvailabe = ref<boolean>(props.cancelButtonAvailabe);
-const { getModalInfo, setModalElement } = useBaseModal();
+const { getModalInfo, closeModal } = useBaseModal();
 const dialog = ref<HTMLDialogElement>();
 const modalStyle = computed(
   () => modalStyles[getModalInfo(props.modalType).modalPosition]
 );
 
-onMounted(() => {
-  setModalElement(dialog.value!!, props.modalType);
-  dialog.value?.addEventListener("click", (event) => {
-    closeDialogOnBackdropClick(event);
-  });
-});
-
-onUnmounted(() => {
-  dialog.value?.removeEventListener("click", (event) =>
-    closeDialogOnBackdropClick(event)
-  );
-});
-
 const modalStyles: { [key: string]: string } = {
-  right: `${props.modalWidthStyle || "w-2/5"} ${
-    props.modalHeightStyle || "h-screen"
-  } absolute mr-0 my-0`,
-  center: `t-[40vh] ${props.modalWidthStyle || "w-1/4"}`,
+  right: `${
+    props.modalWidthStyle || "w-2/5"
+  } h-screen max-h-screen absolute mr-0 my-0`,
+  center: `${props.modalWidthStyle || "w-1/4"} ${props.modalHeightStyle}`,
   left: "",
-};
-
-const closeDialogOnBackdropClick = (event: MouseEvent) => {
-  if (!dialog.value) return;
-  const rect = dialog.value.getBoundingClientRect();
-  const isInDialog =
-    rect.top <= event.clientY &&
-    event.clientY <= rect.top + rect.height &&
-    rect.left <= event.clientX &&
-    event.clientX <= rect.left + rect.width;
-  if (isInDialog) return;
-
-  dialog.value.close();
 };
 
 watch(
@@ -99,12 +73,16 @@ const hideModal = () => {
 <style scoped>
 dialog {
   z-index: 100;
-  max-height: 100vh;
   max-width: 100vw;
+  border: 0;
 }
 
 dialog::backdrop {
   background-color: theme("colors.accent.normal");
   opacity: 0.3;
+}
+
+dialog:focus {
+  outline: none;
 }
 </style>
