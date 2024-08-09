7b8b60aeb9b9c92856cdddeebca46b5a6e0f4bd8
boris <boriss@inuits.eu>
2024-08-09 10:59:47 +0200

    cleanup, added direction for action context menu, fix styles

--- src/components/ActionMenuGroup.vue
+++ src/components/ActionMenuGroup.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-row justify-center items-center">
     <BaseButtonNew
       v-if="primaryOption"
-      class="-mr-4"
+      class="-mr-4 pr-6"
       button-style="accentNormal"
       :label="t(primaryOption.label)"
       @click="handleEmit(primaryOption)"
@@ -10,25 +10,20 @@
     <BaseButtonNew
       v-if="secondaryOptions.length > 0"
       :icon="DamsIcons.EllipsisV"
-      class="!w-1/5 !border-l-2 border-white"
-      @click.stop="(event: MouseEvent) => {
-        console.log(event)
-        contextMenuHandler.openContextMenu({x: event.clientX, y: event.clientY})
-      }"
+      class="!w-max !border-l-2 border-white"
+      @click.stop="(event: MouseEvent) => contextMenuHandler.openContextMenu({x: event.clientX, y: event.clientY})"
     />
 
     <BaseContextMenu
       :context-menu="contextMenuHandler.getContextMenu()"
-      direction="left"
+      :direction="ContextMenuDirection.Left"
     >
-      <div role="none">
-        <BaseContextMenuItem
-          v-for="(option, idx) in secondaryOptions"
-          :key="idx"
-          @clicked="handleEmit(option)"
-          :label="t(option.label)"
-        />
-      </div>
+      <BaseContextMenuItem
+        v-for="(option, idx) in secondaryOptions"
+        :key="idx"
+        @clicked="handleEmit(option)"
+        :label="t(option.label)"
+      />
     </BaseContextMenu>
   </div>
 </template>
@@ -36,7 +31,11 @@
 <script lang="ts" setup>
 import { computed, ref } from "vue";
 import { ContextMenuHandler } from "@/components/context-menu-actions/ContextMenuHandler";
-import { DropdownOption, DamsIcons } from "@/generated-types/queries";
+import {
+  DropdownOption,
+  DamsIcons,
+  ContextMenuDirection,
+} from "@/generated-types/queries";
 import BaseButtonNew from "./base/BaseButtonNew.vue";
 import { useI18n } from "vue-i18n";
 import BaseContextMenu from "@/components/base/BaseContextMenu.vue";
