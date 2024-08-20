8e920fb3e5333dcdeb37c476b92018a2f8954bd1
Jonas Verschueren <jonasv@inuits.eu>
2024-08-20 15:12:20 +0200

    feat: Place contextMenu direction left when there is not much space on the right
    Refs: #129832

--- src/components/base/BaseContextMenu.vue
+++ src/components/base/BaseContextMenu.vue
@@ -23,7 +23,7 @@ const getStyles = () => {
     top: `${props.contextMenu.position.y}px`,
   };
 
-  if (props.direction === ContextMenuDirection.Left) {
+  if (document.body.clientWidth - props.contextMenu.position.x < 250 || props.direction === ContextMenuDirection.Left) {
     styles["right"] = `${
       document.body.clientWidth - props.contextMenu.position.x
     }px`;
