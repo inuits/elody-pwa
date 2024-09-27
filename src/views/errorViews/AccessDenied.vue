2088e78605e3b6a735ecf7046bdebab2c3b28ba0
Berend Vandenbussche <berend@inuits.eu>
2024-09-27 16:14:42 +0200

    fix: Add max-w to AccessDenied, NotFound and Unauthorized pages

--- src/views/errorViews/AccessDenied.vue
+++ src/views/errorViews/AccessDenied.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full h-full flex flex-col justify-center items-center">
     <div class="flex p-4 w-1/2">
-      <img src="/logo.svg" alt="Elody logo" class="h-64" />
+      <img src="/logo.svg" alt="Elody logo" class="h-64 max-w-xs" />
       <div class="px-4 pl-12">
         <h2 class="text-4xl font-bold py-4">
           {{ t("error-pages.access-denied.title") }}
