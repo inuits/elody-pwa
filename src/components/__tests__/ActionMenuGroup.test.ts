76af24b1e20fbb23cc6bfea7d406cc75dff8b43c
Boris Sobolev <boriss@inuits.eu>
2024-10-08 10:53:13 +0000

    feat(permissions): check permissions in bulk operations + tests
    refs: #132180

---
+++ src/components/__tests__/ActionMenuGroup.test.ts
@@ -0,0 +1,147 @@
+import { mount } from "@vue/test-utils";
+import ActionMenuGroup from "../ActionMenuGroup.vue";
+import { describe, it, expect, vi, afterEach } from "vitest";
+import {
+  Entitytyping,
+  ActionContextEntitiesSelectionType,
+  TypeModals,
+  Permission,
+  ActionContextViewModeTypes,
+  DropdownOption,
+} from "@/generated-types/queries";
+import { flushPromises } from "@vue/test-utils";
+
+vi.mock("vue-i18n", () => ({
+  useI18n: () => ({
+    t: (key: string) => key,
+  }),
+}));
+
+vi.mock("@vue/apollo-composable", () => ({
+  useMutation: () => ({
+    mutate: vi.fn(),
+  }),
+}));
+
+vi.mock("@/types", () => ({
+  Unicons: {
+    EllipsisV: { name: "EllipsisV" },
+  },
+}));
+
+vi.mock("session-vue-3-oidc-library", () => ({
+  useAuth: () => ({
+    isAuthenticated: { value: true },
+  }),
+}));
+
+const mocks = vi.hoisted(() => {
+  return {
+    advancedPermissions: {
+      can_do_whatever_you_want: true,
+    },
+  };
+});
+
+vi.mock("@/composables/usePermissions", () => ({
+  usePermissions: () => ({
+    can: vi.fn(),
+    fetchAdvancedPermission: vi.fn(),
+    fetchPermissionsForDropdownOptions: vi.fn(),
+  }),
+  ignorePermissions: { value: false },
+  advancedPermissions: mocks.advancedPermissions,
+}));
+
+describe("ActionMenuGroup", () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+    vi.resetAllMocks();
+  });
+
+  const bulkOption: DropdownOption = {
+    label: "bulk-operations.add-relation",
+    value: "addRelation",
+    actionContext: {
+      entitiesSelectionType: ActionContextEntitiesSelectionType.SomeSelected,
+      activeViewMode: [
+        ActionContextViewModeTypes.ReadMode,
+        ActionContextViewModeTypes.EditMode,
+      ],
+      labelForTooltip: null,
+    },
+    bulkOperationModal: {
+      typeModal: TypeModals.DynamicForm,
+      formQuery: "GetForm",
+      formRelationType: null,
+      askForCloseConfirmation: true,
+      neededPermission: Permission.Canupdate,
+    },
+    can: ["can_do_whatever_you_want"],
+  };
+
+  it("contains the option if permission is granted", async () => {
+    mocks.advancedPermissions["can_do_whatever_you_want"] = true;
+
+    const wrapper = mount(ActionMenuGroup, {
+      props: {
+        options: [bulkOption],
+        isMainActionDisabled: false,
+        entityType: "Entitytyping" as Entitytyping,
+      },
+    });
+
+    await flushPromises();
+
+    expect(wrapper.vm.availableOptions.length).toBe(1);
+  });
+
+  it("has no option if permission is not granted", async () => {
+    mocks.advancedPermissions["can_do_whatever_you_want"] = false;
+
+    const wrapper = mount(ActionMenuGroup, {
+      props: {
+        options: [bulkOption],
+        isMainActionDisabled: false,
+        entityType: "Entitytyping" as Entitytyping,
+      },
+    });
+
+    await flushPromises();
+
+    expect(wrapper.vm.availableOptions.length).toBe(0);
+  });
+
+  it("contains the option to render if permission is not provided", async () => {
+    const wrapper = mount(ActionMenuGroup, {
+      props: {
+        options: [
+          { ...bulkOption, can: undefined },
+          { ...bulkOption, can: undefined },
+        ],
+        isMainActionDisabled: false,
+        entityType: "Entitytyping" as Entitytyping,
+      },
+    });
+
+    await flushPromises();
+
+    expect(wrapper.vm.availableOptions.length).toBe(2);
+  });
+
+  it("contains only permitted options", async () => {
+    mocks.advancedPermissions["can_do_whatever_you_want"] = true;
+
+    const wrapper = mount(ActionMenuGroup, {
+      props: {
+        options: [bulkOption, { ...bulkOption, can: ["take_whats_mine"] }],
+        isMainActionDisabled: false,
+        entityType: "Entitytyping" as Entitytyping,
+      },
+    });
+
+    await flushPromises();
+
+    expect(wrapper.vm.availableOptions.length).toBe(1);
+  });
+});
