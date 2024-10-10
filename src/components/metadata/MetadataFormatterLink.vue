48a91df16d815a165228281d364278eb5b4011ce
boris <boriss@inuits.eu>
2024-10-10 10:10:40 +0200

    fix(review): add separate components for formatters

---
+++ src/components/metadata/MetadataFormatterLink.vue
@@ -0,0 +1,12 @@
+<template>
+  <a class="text-sm break-words underline" target="_blank" :href="link">{{
+    label
+  }}</a>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  label: string;
+  link: string;
+}>();
+</script>
