3e2d8d95fcb1209265478b13278b6ef04fb2efae
Boris Sobolev <boriss@inuits.eu>
2024-10-11 14:20:19 +0000

    feat(customFormatters): add link and pill formatters
    refs: #129620

---
+++ src/components/metadata/MetadataFormatter.vue
@@ -0,0 +1,41 @@
+<template>
+  <MetadataFormatterLink
+    v-if="formatterType === CustomFormatterTypes.Link"
+    :formatter="formatter"
+    :label="readableLabel"
+    :link="link"
+  />
+  <MetadataFormatterPill
+    v-if="formatterType === CustomFormatterTypes.Pill"
+    :formatter="formatter"
+    :label="readableLabel"
+  />
+</template>
+
+<script lang="ts" setup>
+import { CustomFormatterTypes } from "@/generated-types/queries";
+import MetadataFormatterLink from "./MetadataFormatterLink.vue";
+import MetadataFormatterPill from "./MetadataFormatterPill.vue";
+
+import { computed } from "vue";
+
+const props = withDefaults(
+  defineProps<{
+    formatter: string;
+    label: string | string[];
+    link: string;
+  }>(),
+  {
+    link: "",
+  }
+);
+
+const formatterType = computed(() => {
+  const [type] = props.formatter.split("|");
+  return type;
+});
+
+const readableLabel = computed(() => {
+  return Array.isArray(props.label) ? props.label.join(", ") : props.label;
+});
+</script>
