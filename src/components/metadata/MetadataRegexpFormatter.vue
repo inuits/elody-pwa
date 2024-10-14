2544d507fdfe891a16a68b3d15ad10345147352c
boris <boriss@inuits.eu>
2024-10-14 12:54:42 +0200

    wip

---
+++ src/components/metadata/MetadataRegexpFormatter.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- <div
+    class="rounded-md bg-slate-800 py-0.25 px-1 border border-transparent text-sm"
+    :style="{
+      background: pillSettings.background,
+      color: pillSettings.text,
+    }"
+  >
+    {{ label }}
+  </div> -->
+  <div>
+    !!!regexp formatter!!!
+    {{ label }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { formattersSettings } from "@/main";
+
+import { computed } from "vue";
+const props = defineProps<{
+  formatter: string;
+  label: string;
+}>();
+
+const pillSettings = computed(() => {
+  const [formatterType, pillType] = props.formatter.split("|");
+  return formattersSettings[formatterType][pillType];
+});
+</script>
