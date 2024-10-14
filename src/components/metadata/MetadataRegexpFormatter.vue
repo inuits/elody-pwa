70c3db6b645265d6a8e1105bcc8afdf6b188ae8f
boris <boriss@inuits.eu>
2024-10-14 16:01:45 +0200

    add stupid component for metadata regexp formatter, some formatting for labels

--- src/components/metadata/MetadataRegexpFormatter.vue
+++ src/components/metadata/MetadataRegexpFormatter.vue
@@ -1,30 +1,11 @@
 <template>
-  <!-- <div
-    class="rounded-md bg-slate-800 py-0.25 px-1 border border-transparent text-sm"
-    :style="{
-      background: pillSettings.background,
-      color: pillSettings.text,
-    }"
-  >
-    {{ label }}
-  </div> -->
-  <div>
-    !!!regexp formatter!!!
+  <div class="text-sm break-words">
     {{ label }}
   </div>
 </template>
 
 <script lang="ts" setup>
-import { formattersSettings } from "@/main";
-
-import { computed } from "vue";
 const props = defineProps<{
-  formatter: string;
   label: string;
 }>();
-
-const pillSettings = computed(() => {
-  const [formatterType, pillType] = props.formatter.split("|");
-  return formattersSettings[formatterType][pillType];
-});
 </script>
