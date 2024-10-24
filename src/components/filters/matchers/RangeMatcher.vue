fb4f3d96a51effff2f8872f7fe811366c8466bf6
boris <boriss@inuits.eu>
2024-10-24 11:29:55 +0200

    add base slider, basic range slider, range filter

--- src/components/filters/matchers/RangeMatcher.vue
+++ src/components/filters/matchers/RangeMatcher.vue
@@ -1,254 +1,78 @@
 <template>
-  <div id="app">
-    <div class="slider-container">
-      <!-- Dual Range Slider -->
-      <div class="range-slider">
-        <input
-          type="range"
-          :value="fromValue"
-          :min="min"
-          :max="max"
-          step="1"
-          @input="handleFromChange"
-          class="thumb thumb--left"
-        />
-        <input
-          type="range"
-          :value="tillValue"
-          :min="min"
-          :max="max"
-          step="1"
-          @input="handleTillChange"
-          class="thumb thumb--right"
-        />
-
-        <!-- Track bar -->
-        <div class="slider-track"></div>
-        <div class="highlight-track" :style="highlightStyle"></div>
-      </div>
-
-      <!-- Dynamic labels and ticks underneath the slider -->
-      <div class="range-labels">
-        <span v-for="n in rangeLabels" :key="n" class="range-label">
-          {{ n }}
-          <div class="tick"></div>
-        </span>
-      </div>
-
-      <!-- Display current range values -->
-      <div class="value-display">
-        <label
-          >From: <span>{{ from }}</span></label
-        >
-        <label
-          >Till: <span>{{ to }}</span></label
-        >
-      </div>
-    </div>
-  </div>
+  <BaseRangeSlider
+    :from="from"
+    :to="to"
+    :min="min"
+    :max="max"
+    :unit="unit"
+    @updateRange="updateValues"
+  />
 </template>
 
 <script lang="ts" setup>
-import { ref, computed, watch } from "vue";
+import type { FilterListItem } from "@/composables/useStateManagement";
+import { type AdvancedFilterInput } from "@/generated-types/queries";
+import { onMounted, ref, watch } from "vue";
+import BaseRangeSlider from "@/components/base/BaseRangeSlider.vue";
 
-// Props to pass dynamic range values and labels
 const props = defineProps<{
-  initialFrom: number;
-  initialTill: number;
-  min: number;
-  max: number;
+  filter: FilterListItem;
 }>();
 
-// Reactive values for internal state
-const fromValue = ref<number>(props.initialFrom);
-const tillValue = ref<number>(props.initialTill);
+const emit = defineEmits<{
+  (
+    event: "newAdvancedFilterInput",
+    advancedFilterInput: AdvancedFilterInput,
+    force: boolean
+  ): void;
+}>();
 
-const from = ref<number>(props.initialFrom);
-const to = ref<number>(props.initialTill);
+const min = ref<number>(0);
+const max = ref<number>(0);
 
-// Generate range labels only for even numbers
-const rangeLabels = computed(() => {
-  const labels: number[] = [];
-  for (let i = Math.max(props.min, 0); i <= props.max; i += 2) {
-    labels.push(i);
+const from = ref<number>(0);
+const to = ref<number>(0);
+const force = ref<boolean>(false);
+const unit = ref<string | undefined>(props.filter.advancedFilter?.unit);
+
+const emitNewAdvancedFilterInput = () => {
+  if (from.value && to.value) {
+    const newAdvancedFilterInput: AdvancedFilterInput = {
+      type: props.filter.advancedFilter.type,
+      parent_key: props.filter.advancedFilter.parentKey,
+      key: props.filter.advancedFilter.key,
+      value: {
+        min: from.value,
+        max: to.value,
+      },
+      aggregation: props.filter.advancedFilter.aggregation,
+    };
+    if (props.filter.advancedFilter.lookup)
+      newAdvancedFilterInput.lookup = {
+        from: props.filter.advancedFilter.lookup.from,
+        local_field: props.filter.advancedFilter.lookup.local_field,
+        foreign_field: props.filter.advancedFilter.lookup.foreign_field,
+        as: props.filter.advancedFilter.lookup.as,
+      };
+    emit("newAdvancedFilterInput", newAdvancedFilterInput, force.value);
+    force.value = false;
   }
-  return labels;
+};
+
+const updateValues = (values: number[]) => {
+  const [newFrom, newTo] = values;
+  from.value = newFrom;
+  to.value = newTo;
+};
+
+onMounted(() => {
+  min.value = props.filter.advancedFilter?.min || 0;
+  max.value = props.filter.advancedFilter?.max || 0;
+  from.value = props.filter.inputFromState?.value?.min ?? min.value;
+  to.value = props.filter.inputFromState?.value?.max ?? max.value;
+  force.value = Boolean(props.filter.inputFromState);
 });
 
-watch(
-  () => [props.initialFrom, props.initialTill],
-  ([newFrom, newTill]) => {
-    fromValue.value = newFrom;
-    tillValue.value = newTill;
-  }
-);
-
-// Handlers for input changes
-const handleFrom = (event: Event) => {
-  const newValue = parseInt((event.target as HTMLInputElement).value);
-  console.log("from(reactive): ", newValue);
-
-  from.value = newValue;
-};
-
-const handleTill = (event: Event) => {
-  const newValue = parseInt((event.target as HTMLInputElement).value);
-  console.log("to(reactive): ", newValue);
-  to.value = newValue;
-};
-
-const handleFromChange = (event: Event) => {
-  const newValue = parseInt((event.target as HTMLInputElement).value);
-  console.log("from: ", newValue);
-  if (newValue > tillValue.value) {
-    fromValue.value = tillValue.value;
-    tillValue.value = newValue;
-    return;
-  }
-
-  fromValue.value = newValue;
-  // fromValue.value = Math.min(newValue, tillValue.value - 1);
-};
-
-const handleTillChange = (event: Event) => {
-  const newValue = parseInt((event.target as HTMLInputElement).value);
-  console.log("till: ", newValue);
-  if (newValue < fromValue.value) {
-    tillValue.value = fromValue.value;
-    from.value = newValue;
-    return;
-  }
-
-  tillValue.value = newValue;
-  // tillValue.value = Math.max(newValue, fromValue.value + 1);
-};
-
-// Highlight track style based on selected range
-
-const highlightStyle = computed(() => ({
-  left: `${((fromValue.value - props.min) / (props.max - props.min)) * 100}%`,
-  right: `${
-    (1 - (tillValue.value - props.min) / (props.max - props.min)) * 100
-  }%`,
-}));
-
-// const highlightStyle = computed(() => {
-//   const left = `${((from.value - props.min) / (props.max - props.min)) * 100}%`;
-//   const right = `${
-//     (1 - (to.value - props.min) / (props.max - props.min)) * 100
-//   }%`;
-
-//   const isFromBigger = from.value > to.value;
-
-//   return {
-//     left: isFromBigger ? right : left,
-//     right: isFromBigger ? left : right,
-//   };
-// });
+watch(from, () => emitNewAdvancedFilterInput());
+watch(to, () => emitNewAdvancedFilterInput());
 </script>
-
-<style scoped>
-/* Container to hold the slider and labels */
-.slider-container {
-  position: relative;
-  width: 100%;
-  margin: 0 auto;
-  padding: 10px;
-}
-
-/* Range slider container */
-.range-slider {
-  position: relative;
-  width: 100%;
-  height: 5px;
-  background: #e0e0e0;
-  margin-bottom: 20px; /* Add space for ticks */
-}
-
-/* Track and thumb customization */
-input[type="range"] {
-  -webkit-appearance: none;
-  appearance: none;
-  pointer-events: none;
-  position: absolute;
-  width: 100%;
-  height: 5px;
-  background: transparent;
-}
-
-input[type="range"]::-webkit-slider-thumb {
-  -webkit-appearance: none;
-  appearance: none;
-  pointer-events: all;
-  width: 12px;
-  height: 12px;
-  background: #009688;
-  border-radius: 50%;
-  cursor: pointer;
-}
-
-.slider-track {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  height: 5px;
-  background: #ddd;
-}
-
-/* Highlighted range track between the two sliders */
-.highlight-track {
-  position: absolute;
-  top: 0;
-  height: 5px;
-  background-color: #00bcd4;
-}
-
-/* Thumbs styling for left and right */
-.thumb--left {
-  z-index: 3;
-}
-
-.thumb--right {
-  z-index: 4;
-}
-
-/* Range labels and ticks container */
-.range-labels {
-  display: flex;
-  justify-content: space-between;
-  position: relative;
-  padding-bottom: 10px;
-}
-
-.range-label {
-  position: relative;
-  text-align: center;
-  font-size: 12px;
-  color: #999;
-  width: 7%; /* Adjust width for proper spacing */
-}
-
-/* Small ticks below the numbers */
-.tick {
-  width: 1px;
-  height: 10px;
-  background-color: #999;
-  position: absolute;
-  bottom: -8px;
-  left: 50%;
-  transform: translateX(-50%);
-}
-
-/* Display the selected range values */
-.value-display {
-  display: flex;
-  justify-content: space-between;
-  padding-top: 10px;
-}
-
-.value-display label {
-  font-size: 14px;
-  color: #333;
-}
-</style>
