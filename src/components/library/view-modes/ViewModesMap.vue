2d5062192c1f5e60014287bbac30bdefa42a5947
Jonas Verschueren <jonasv@inuits.eu>
2024-10-31 15:59:59 +0100

    feat: Add config to viewModes
    Refs: #133636

--- src/components/library/view-modes/ViewModesMap.vue
+++ src/components/library/view-modes/ViewModesMap.vue
@@ -1,21 +1,23 @@
 <template>
   <HeatMap
-    v-if="getMapType(entities) === MapTypes.HeatMap"
+    v-if="getBasicMapProperties(config).mapType === MapTypes.HeatMap"
+    :config="config"
     :entities="entities"
   />
 </template>
 
 
 <script setup lang="ts">
-import { Entity, MapTypes } from "@/generated-types/queries";
+import { ConfigItem, Entity, MapTypes } from "@/generated-types/queries";
 import HeatMap from "@/components/maps/HeatMap.vue";
 import { useMaps } from "@/composables/useMaps";
 
 defineProps<{
+  config: ConfigItem[]
   entities: Entity[];
 }>();
 
-const { getMapType } = useMaps();
+const { getBasicMapProperties } = useMaps();
 </script>
 
 <style scoped>
