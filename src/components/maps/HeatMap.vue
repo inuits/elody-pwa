562a50ec66ccfa75d7ba61acd03a20721cb62a2f
Jonas Verschueren <jonasv@inuits.eu>
2024-10-31 12:49:01 +0100

    feat: Add HeatMap component so ViewModesMap can use multiple Map components
    Refs: #132586

---
+++ src/components/maps/HeatMap.vue
@@ -0,0 +1,91 @@
+<template>
+  <ol-map
+    ref="map"
+    :loadTilesWhileAnimating="true"
+    :loadTilesWhileInteracting="true"
+    style="height: 65vh"
+  >
+    <ol-view
+      ref="view"
+      :zoom="getBasicMapProperties(entities).zoom"
+      :center="getBasicMapProperties(entities).center"
+    />
+
+    <ol-tile-layer>
+      <ol-source-osm />
+    </ol-tile-layer>
+
+    <ol-heatmap-layer
+      title="heatmap"
+      :blur="getBasicMapProperties(entities).blur"
+      :radius="getBasicMapProperties(entities).radius"
+      :weight="heatmapWeight"
+      :zIndex="1"
+    >
+      <ol-source-vector
+        :features="heatmapPoints"
+      />
+    </ol-heatmap-layer>
+
+    <ol-context-menu-control
+      :items="contextMenuItems"
+    />
+
+    <ol-fullscreen-control />
+  </ol-map>
+</template>
+
+
+<script setup lang="ts">
+
+import { useI18n } from "vue-i18n";
+import { ref, computed, toRefs } from "vue";
+import { type View, type Item } from "vue3-openlayers";
+import { Feature } from 'ol';
+import { Point } from 'ol/geom';
+import { useMaps } from "@/composables/useMaps";
+import type { Entity } from "@/generated-types/queries";
+
+const props = defineProps<{
+  entities: Entity[];
+}>();
+
+const { t } = useI18n();
+const {
+  getBasicMapProperties,
+  geoToMercator
+} = useMaps();
+
+const { entities } = toRefs(props);
+const view = ref<View | null>(null);
+const contextMenuItems = ref<Item[]>([]);
+
+contextMenuItems.value = [
+  {
+    text: t('map.center-map'),
+    callback: (val) => {
+      view.value?.setCenter(val.coordinate);
+    },
+  },
+  "-",
+];
+
+const heatmapPoints = computed(() => entities.value?.map((entity) => {
+  const mapData = entity.mapComponent;
+  if (!mapData) return new Feature();
+  return new Feature({
+    geometry: new Point(geoToMercator(mapData.coordinateX.value, mapData.coordinateY.value)),
+    weight: mapData.weight.value
+  });
+}));
+
+const heatmapWeight = function (feature: Feature) {
+  const weight = feature.get("weight");
+  return weight / 100;
+};
+
+</script>
+
+
+<style scoped>
+</style>
