a4d226ed806ceab7b3ea6e2cc7bf876be5b1207d
Berend Vandenbussche <berend@inuits.eu>
2024-11-06 13:29:38 +0100

    fix: Serve frontend when none of the special cases have been met
    Refs #105106

--- src/main.ts
+++ src/main.ts
@@ -19,11 +19,12 @@ import { createRouter, createWebHistory } from "vue-router";
 import { createUploadLink } from "apollo-upload-client";
 import { DefaultApolloClient } from "@vue/apollo-composable";
 import { getApplicationDetails, getFormattersSettings, i18n } from "@/helpers";
-import { onError } from "@apollo/client/link/error";
+import { type ErrorResponse, onError } from "@apollo/client/link/error";
 import { OpenIdConnectClient } from "session-vue-3-oidc-library";
 import { setIgnorePermissions } from "./composables/usePermissions";
 import { Unicons } from "./types";
 import { useFormHelper } from "@/composables/useFormHelper";
+import { useErrorCodes } from "@/composables/useErrorCodes";
 
 import OpenLayersMap from "vue3-openlayers";
 
@@ -74,9 +75,9 @@ const start = async () => {
 
   bulkSelectAllSizeLimit = config.bulkSelectAllSizeLimit;
 
-  const graphqlErrorInterceptor = onError((error: any) => {
-    const errorHandler = useGraphqlErrors(error);
-    errorHandler.logFormattedErrors(router);
+  const graphqlErrorInterceptor = onError((error: ErrorResponse) => {
+    const { handleGraphqlError } = useErrorCodes();
+    handleGraphqlError(error);
   });
 
   apolloClient = new ApolloClient({
