d407732aaeb4231d35ebbdcd38374399846aea72
Jonas Verschueren <jonasv@inuits.eu>
2024-11-08 16:13:38 +0100

    fix: Fixes for multiple values in dropdown
    Refs: #133990

--- src/components/metadata/MetadataWrapper.vue
+++ src/components/metadata/MetadataWrapper.vue
@@ -166,6 +166,7 @@ const props = withDefaults(
 // );
 
 const setNewValue = (newValue: string | BaseRelationValuesInput[] | string[]) => {
+  if (Array.isArray(newValue) && newValue.length === 1) newValue = newValue[0];
   value.value = newValue;
   const form = getForm(props.formId);
   if (form) {
