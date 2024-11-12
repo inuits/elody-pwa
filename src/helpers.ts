d73dc2be5cbe7f2460d640ae5b80037059f23993
Eray Özcan <eray@inuits.eu>
2024-11-12 15:46:46 +0100

    TEMP: __typename fix for pza deploy

--- src/helpers.ts
+++ src/helpers.ts
@@ -39,7 +39,7 @@ export const goToEntityPage = (
     name: listItemRouteName,
     params: {
       id: entityId,
-      type: entity.type,
+      type: entity.__typename,
     },
   });
 };
@@ -102,7 +102,7 @@ export const getEntityPageRoute = (
     name: listItemRouteName,
     params: {
       id: entityId,
-      type: entity.type,
+      type: entity.__typename,
     },
   };
 };
