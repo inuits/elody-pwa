466b5e76372c3e9d9a6dfce1b5b10f1d71cd4a77
Boris Sobolev <boriss@inuits.eu>
2024-12-02 08:33:28 +0000

    chores: downgrade eslint, fix lint staged file, add error code to fallback to handle error codes 401 from base graphql

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  "!(*config).ts": ["eslint --fix", "tsc-files --noEmit", "git add"],
-  "*.vue": ["eslint --fix", "tsc-files --noEmit", "git add"],
+  "!(*config).ts": ["eslint --fix", "tsc-files --noEmit"],
+  "*.vue": ["eslint --fix", "tsc-files --noEmit"],
 };
