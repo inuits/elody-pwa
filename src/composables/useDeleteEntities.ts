d7dc3283eb34a2cfdd1bfe337aadbf209df98243
boriss <boriss@inuits.eu>
2024-12-11 15:53:24 +0100

    bulk delete entities with option to fetch which linked entities needs to be removed

--- src/composables/useDeleteEntities.ts
+++ src/composables/useDeleteEntities.ts
@@ -8,20 +8,46 @@ import {
 } from "@/generated-types/queries";
 import { type InBulkProcessableItem } from "@/composables/useBulkOperations";
 import { getChildrenOfHomeRoutes } from "@/helpers";
+import { apolloClient } from "@/main";
+import { ref } from "vue";
+import { useImport } from "./useImport";
 
 export const useDeleteEntities = () => {
   const config: any = inject("config");
   const { mutate } = useMutation<DeleteDataMutation>(DeleteDataDocument);
+  const { loadDocument } = useImport();
+
+  const form = ref<any | undefined>(undefined);
+
+  const getQuery = async (queryName: string) => {
+    return await loadDocument(queryName);
+  };
+
+  const getDeletionForm = async (queryDocument: any): Promise<void> => {
+    const query = await getQuery(queryDocument);
+    const response = await apolloClient.query({
+      query,
+    });
+
+    form.value = response.data.GetDynamicForm.formTab.formFields.field;
+  };
 
   const deleteEntities = async (
     items: InBulkProcessableItem[] | { id: string; type: string }[],
     deleteMediafiles: boolean = false,
+    linkedEntitiesToRemove: { [key: string]: boolean } | undefined,
   ) => {
     if (items.length === 0) return;
 
     const childRoutes = getChildrenOfHomeRoutes(config).map(
       (route: any) => route.meta,
     );
+
+    const deleteEntities = {
+      ...(linkedEntitiesToRemove ? linkedEntitiesToRemove : {}),
+      deleteMediafiles:
+        linkedEntitiesToRemove?.["deleteMediafiles"] || deleteMediafiles,
+    };
 
     const deletePromises = items.map(
       (item: InBulkProcessableItem | { id: string; type: string }) => {
@@ -38,7 +64,7 @@ export const useDeleteEntities = () => {
         return mutate({
           id: item.id,
           path: collection,
-          deleteMediafiles,
+          ...deleteEntities,
         });
       },
     );
@@ -48,6 +74,8 @@ export const useDeleteEntities = () => {
   };
 
   return {
+    form,
     deleteEntities,
+    getDeletionForm,
   };
 };
