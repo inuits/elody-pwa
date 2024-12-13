ab673e6720480001642367cec7d1c45a04bc3010
Boris Sobolev <boriss@inuits.eu>
2024-12-13 09:32:06 +0000

    fix: show tooltip text in modals
    refs: #135884

--- src/components/metadata/MetadataWrapper.vue
+++ src/components/metadata/MetadataWrapper.vue
@@ -30,6 +30,7 @@
         v-if="metadata?.tooltip"
         position="top-right"
         :tooltip-offset="8"
+        :is-used-in-modal="isUsedInModal"
       >
         <template #activator="{ on }">
           <div v-on="on" class="pl-1">
@@ -65,6 +66,7 @@
           metadata.inputField?.validation?.fastValidationMessage)
       "
       :field-is-valid="fieldIsValid"
+      :is-used-in-modal="isUsedInModal"
       @click.stop.prevent
       @update:value="setNewValue"
       @register-enter-pressed:value="registerEnterKeyPressed"
@@ -201,11 +203,13 @@ const props = withDefaults(
     baseLibraryMode?: BaseLibraryModes;
     formFlow?: "edit" | "create";
     showErrors?: boolean;
+    isUsedInModal: boolean;
   }>(),
   {
     baseLibraryMode: BaseLibraryModes.NormalBaseLibrary,
     formFlow: "edit",
     showErrors: false,
+    isUsedInModal: false,
   },
 );
 
