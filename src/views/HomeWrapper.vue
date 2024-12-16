89c530f35816517aca1198bc8a860130fca82534
Jonas Verschueren <jonasv@inuits.eu>
2024-12-16 17:11:04 +0100

    fix: Do not show overviewpage view when singleEntity view is active and reversed
    Refs: #136070

--- src/views/HomeWrapper.vue
+++ src/views/HomeWrapper.vue
@@ -1,9 +1,18 @@
 <template>
-  <AssetLibrary />
-  <Home />
+  <AssetLibrary v-if="!isSingleEntityPage" />
+  <Home v-if="isSingleEntityPage" />
 </template>
 
 <script lang="ts" setup>
 import AssetLibrary from "@/components/library/AssetLibrary.vue";
 import Home from "@/views/Home.vue";
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute()
+
+const isSingleEntityPage = computed(() => {
+  return route.name === "SingleEntity" || route.name === "SingleMediafile"
+})
+
 </script>
