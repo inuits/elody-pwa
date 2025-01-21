2d96a13ebd6f385d1c0390cf1f2aaece0d8aec0a
boriss <boriss@inuits.eu>
2025-01-21 10:56:50 +0100

    revert wkt poc

--- src/composables/useMaps.ts
+++ src/composables/useMaps.ts
@@ -1,5 +1,4 @@
 import { type ConfigItem } from "@/generated-types/queries";
-import WKT from "ol/format/WKT.js";
 
 export const useMaps = () => {
   const getBasicMapProperties = (config: ConfigItem[]) => {
@@ -24,18 +23,8 @@ export const useMaps = () => {
     return [x, y];
   };
 
-  const getWktFeature = (wkt: string) => {
-    const format = new WKT();
-    return format.readFeature(wkt, {
-      dataProjection: "EPSG:3857",
-      // dataProjection: "EPSG:4326" for the normal latitude/longitude,
-      featureProjection: "EPSG:3857",
-    });
-  };
-
   return {
     getBasicMapProperties,
     geoToMercator,
-    getWktFeature,
   };
 };
