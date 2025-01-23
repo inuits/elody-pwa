6022121d21f7eb1625e51c000fb84a11d6882945
Berend Vandenbussche <berend@inuits.eu>
2025-01-23 15:11:59 +0100

    Feat: Show buttons based on available extensions
    Refs #137648

--- src/components/entityElements/EntityElementWYSIWYG.vue
+++ src/components/entityElements/EntityElementWYSIWYG.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     :class="[
-      'border-solid border-neutral-30 border-2 bg-neutral-0 rounded-t-md min-h-[300px]',
+      'border-solid border-neutral-30 border-2 bg-neutral-0 rounded-t-md min-h-[300px] relative',
       { 'animate-pulse': !editor },
     ]"
   >
@@ -12,84 +12,9 @@
         {{ t(element.label) }}
       </h1>
     </div>
-    <div v-if="editor" class="p-2">
-      <div class="control-group">
-        <div class="button-group">
-          <button
-            @click="editor.chain().focus().setParagraph().run()"
-            :class="{ 'is-active': editor.isActive('paragraph') }"
-          >
-            Paragraph
-          </button>
-          <button
-            @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
-            :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"
-          >
-            H1
-          </button>
-          <button
-            @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
-            :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }"
-          >
-            H2
-          </button>
-          <button
-            @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
-            :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }"
-          >
-            H3
-          </button>
-          <button
-            @click="editor.chain().focus().toggleHeading({ level: 4 }).run()"
-            :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }"
-          >
-            H4
-          </button>
-          <button
-            @click="editor.chain().focus().toggleHeading({ level: 5 }).run()"
-            :class="{ 'is-active': editor.isActive('heading', { level: 5 }) }"
-          >
-            H5
-          </button>
-          <button
-            @click="editor.chain().focus().toggleHeading({ level: 6 }).run()"
-            :class="{ 'is-active': editor.isActive('heading', { level: 6 }) }"
-          >
-            H6
-          </button>
-          <button
-            @click="editor.chain().focus().toggleBulletList().run()"
-            :class="{ 'is-active': editor.isActive('bulletList') }"
-          >
-            Bullet list
-          </button>
-          <button
-            @click="editor.chain().focus().toggleOrderedList().run()"
-            :class="{ 'is-active': editor.isActive('orderedList') }"
-          >
-            Ordered list
-          </button>
-          <button
-            @click="editor.chain().focus().toggleCodeBlock().run()"
-            :class="{ 'is-active': editor.isActive('codeBlock') }"
-          >
-            Code block
-          </button>
-          <button
-            @click="editor.chain().focus().toggleBlockquote().run()"
-            :class="{ 'is-active': editor.isActive('blockquote') }"
-          >
-            Blockquote
-          </button>
-          <button @click="editor.chain().focus().setHorizontalRule().run()">
-            Horizontal rule
-          </button>
-          <button @click="editor.chain().focus().setHardBreak().run()">
-            Hard break
-          </button>
-        </div>
-      </div>
-      <editor-content :editor="editor" class="h-full" />
+    <div v-if="editor" class="p-2 h-full">
+      <WYSIWYGButtons :editor="editor" :extensions="element.extensions" />
+      <editor-content :editor="editor" />
     </div>
   </div>
 </template>
@@ -98,6 +23,7 @@
 import { Editor, EditorContent } from "@tiptap/vue-3";
 import { ref, onMounted, onUnmounted } from "vue";
 import { useWYSIWYGEditor } from "@/composables/useWYSIWYGEditor";
+import WYSIWYGButtons from "@/components/WYSIWYGButtons.vue";
 import { WysiwygElement } from "@/generated-types/queries";
 import { useI18n } from "vue-i18n";
 
@@ -133,10 +59,19 @@ onUnmounted(() => {
 <style lang="scss">
 /* Basic editor styles */
 .tiptap {
+  position: relative;
   height: 100%;
 
-  :first-child {
+  &:first-child {
     margin-top: 0;
+    border: solid 1px rgba(0, 58, 82, 0.6);
+    border-radius: 0.5rem;
+    height: 100%;
+
+    &:focus {
+      border: solid 1px rgba(0, 58, 82, 0.6);
+      border-radius: 0.5rem;
+    }
   }
 
   /* List styles */
@@ -149,6 +84,14 @@ onUnmounted(() => {
       margin-top: 0.25em;
       margin-bottom: 0.25em;
     }
+  }
+
+  ul {
+    list-style: circle;
+  }
+
+  ol {
+    list-style: decimal;
   }
 
   /* Heading styles */
