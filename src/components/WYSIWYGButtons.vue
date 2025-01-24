3971fb5a75b5fe9972a8c77a5f7c9c6da41794a7
Berend Vandenbussche <berend@inuits.eu>
2025-01-24 17:36:53 +0100

    Feat: Save WYSIWYG element contents to veevalidate state
    Refs #137648

--- src/components/WYSIWYGButtons.vue
+++ src/components/WYSIWYGButtons.vue
@@ -1,101 +1,136 @@
 <template>
-  <div class="sticky top-0 bg-neutral-0 z-[100] p-2 flex">
-    <div v-if="extensions.includes(WysiwygExtensions.StarterKit)">
-      <button
-        @click="editor.chain().focus().setParagraph().run()"
-        :class="{ 'is-active': editor.isActive('paragraph') }"
-      >
-        Paragraph
-      </button>
-      <button
-        @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
-        :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"
-      >
-        H1
-      </button>
-      <button
-        @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
-        :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }"
-      >
-        H2
-      </button>
-      <button
-        @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
-        :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }"
-      >
-        H3
-      </button>
-      <button
-        @click="editor.chain().focus().toggleHeading({ level: 4 }).run()"
-        :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }"
-      >
-        H4
-      </button>
-      <button
-        @click="editor.chain().focus().toggleHeading({ level: 5 }).run()"
-        :class="{ 'is-active': editor.isActive('heading', { level: 5 }) }"
-      >
-        H5
-      </button>
-      <button
-        @click="editor.chain().focus().toggleHeading({ level: 6 }).run()"
-        :class="{ 'is-active': editor.isActive('heading', { level: 6 }) }"
-      >
-        H6
-      </button>
-      <button
-        @click="editor.chain().focus().toggleBulletList().run()"
-        :class="{ 'is-active': editor.isActive('bulletList') }"
-      >
-        Bullet list
-      </button>
-      <button
-        @click="editor.chain().focus().toggleOrderedList().run()"
-        :class="{ 'is-active': editor.isActive('orderedList') }"
-      >
-        Ordered list
-      </button>
-      <button
-        @click="editor.chain().focus().toggleCodeBlock().run()"
-        :class="{ 'is-active': editor.isActive('codeBlock') }"
-      >
-        Code block
-      </button>
-      <button
-        @click="editor.chain().focus().toggleBlockquote().run()"
-        :class="{ 'is-active': editor.isActive('blockquote') }"
-      >
-        Blockquote
-      </button>
-      <button @click="editor.chain().focus().setHorizontalRule().run()">
-        Horizontal rule
-      </button>
-      <button @click="editor.chain().focus().setHardBreak().run()">
-        Hard break
-      </button>
-    </div>
-    <div v-if="extensions.includes(WysiwygExtensions.Bold)">
-      <button
-        @click="editor.chain().focus().toggleBold().run()"
-        :class="{ 'is-active': editor.isActive('bold') }"
-      >
-        Bold
-      </button>
-    </div>
-    <div v-if="extensions.includes(WysiwygExtensions.Italic)">
-      <button
-        @click="editor.chain().focus().toggleItalic().run()"
-        :class="{ 'is-active': editor.isActive('italic') }"
-      >
-        Italic
-      </button>
-    </div>
+  <div class="sticky top-0 transparent-bg z-[100] p-2 flex flex-wrap">
+    <button
+      v-if="extensions.includes(WysiwygExtensions.StarterKit)"
+      @click="editor.chain().focus().setParagraph().run()"
+      :class="{ 'is-active': editor.isActive('paragraph') }"
+      title="Paragraph"
+    >
+      Paragraph
+    </button>
+    <button
+      v-if="extensions.includes(WysiwygExtensions.StarterKit)"
+      @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"
+      title="H1"
+    >
+      H1
+    </button>
+    <button
+      v-if="extensions.includes(WysiwygExtensions.StarterKit)"
+      @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }"
+      title="H2"
+    >
+      H2
+    </button>
+    <button
+      v-if="extensions.includes(WysiwygExtensions.StarterKit)"
+      @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }"
+      title="H3"
+    >
+      H3
+    </button>
+    <button
+      v-if="extensions.includes(WysiwygExtensions.StarterKit)"
+      @click="editor.chain().focus().toggleHeading({ level: 4 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }"
+      title="H4"
+    >
+      H4
+    </button>
+    <button
+      v-if="extensions.includes(WysiwygExtensions.StarterKit)"
+      @click="editor.chain().focus().toggleHeading({ level: 5 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 5 }) }"
+      title="H5"
+    >
+      H5
+    </button>
+    <button
+      v-if="extensions.includes(WysiwygExtensions.StarterKit)"
+      @click="editor.chain().focus().toggleHeading({ level: 6 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 6 }) }"
+      title="H6"
+    >
+      H6
+    </button>
+    <button
+      v-if="extensions.includes(WysiwygExtensions.StarterKit)"
+      @click="editor.chain().focus().toggleBulletList().run()"
+      :class="{ 'is-active': editor.isActive('bulletList') }"
+      title="Bullet list"
+    >
+      <unicon :name="Unicons.ListUl.name" />
+    </button>
+    <button
+      v-if="extensions.includes(WysiwygExtensions.StarterKit)"
+      @click="editor.chain().focus().toggleOrderedList().run()"
+      :class="{ 'is-active': editor.isActive('orderedList') }"
+      title="Ordered list"
+    >
+      <unicon :name="Unicons.ListOl.name" />
+    </button>
+    <button
+      v-if="extensions.includes(WysiwygExtensions.StarterKit)"
+      @click="editor.chain().focus().toggleCodeBlock().run()"
+      :class="{ 'is-active': editor.isActive('codeBlock') }"
+      title="Code block"
+    >
+      <unicon :name="Unicons.Arrow.name" />
+    </button>
+    <button
+      v-if="extensions.includes(WysiwygExtensions.StarterKit)"
+      @click="editor.chain().focus().toggleBlockquote().run()"
+      :class="{ 'is-active': editor.isActive('blockquote') }"
+      title="Blockquote"
+    >
+      Blockquote
+    </button>
+    <button
+      v-if="extensions.includes(WysiwygExtensions.StarterKit)"
+      @click="editor.chain().focus().setHorizontalRule().run()"
+      title="Horizontal rule"
+    >
+      <unicon :name="Unicons.Minus.name" />
+    </button>
+    <button
+      v-if="extensions.includes(WysiwygExtensions.StarterKit)"
+      @click="editor.chain().focus().setHardBreak().run()"
+      title="Hard break"
+    >
+      Hard break
+    </button>
+    <button
+      v-if="
+        extensions.includes(WysiwygExtensions.Bold) ||
+        extensions.includes(WysiwygExtensions.StarterKit)
+      "
+      @click="editor.chain().focus().toggleBold().run()"
+      :class="['font-mono', { 'is-active': editor.isActive('bold') }]"
+      title="Bold"
+    >
+      <b>B</b>
+    </button>
+    <button
+      v-if="
+        extensions.includes(WysiwygExtensions.Italic) ||
+        extensions.includes(WysiwygExtensions.StarterKit)
+      "
+      @click="editor.chain().focus().toggleItalic().run()"
+      :class="['font-mono', { 'is-active': editor.isActive('italic') }]"
+      title="Italic"
+    >
+      <i>I</i>
+    </button>
   </div>
 </template>
 
 <script setup lang="ts">
 import { Editor } from "@tiptap/vue-3";
 import { WysiwygExtensions } from "@/generated-types/queries";
+import { Unicons } from "@/types";
 
 const props = defineProps<{
   editor: Editor;
@@ -109,10 +144,15 @@ button {
   margin: 0.25rem;
   background-color: theme("colors.neutral.40");
   border-radius: 0.25rem;
+  min-width: 2rem;
 }
 
 .is-active {
   background-color: theme("colors.accent.normal");
   color: theme("colors.white");
+}
+
+.transparent-bg {
+  background-color: rgba(255, 255, 255, 0.95);
 }
 </style>
