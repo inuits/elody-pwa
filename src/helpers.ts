aa19735c306f5b1555b6307b9c8d7e5e26fb78d8
Berend Vandenbussche <berend@inuits.eu>
2025-01-27 16:36:30 +0100

    Fix: Add WYSIWYG metadatakey to EditableKeys so it can be saved

--- src/helpers.ts
+++ src/helpers.ts
@@ -18,6 +18,7 @@ import {
   type Column,
   DamsIcons,
   type DropdownOption
+  type Column,
 } from "@/generated-types/queries";
 import { createI18n } from "vue-i18n";
 import { useEntityMediafileSelector } from "@/composables/useEntityMediafileSelector";
@@ -354,7 +355,8 @@ export const convertUnitToReadbleFormat = (unit: Unit, value: string) => {
     SECONDS: (value: string) => `${value} s`,
     COORDINATES: (value: string) =>
       `${(value as any).longitude}, ${(value as any).latitude}`,
-    PERCENT: (value: string) => Number(value) !== -1 ? `${Number(value) * 100}%` : "charging",
+    PERCENT: (value: string) =>
+      Number(value) !== -1 ? `${Number(value) * 100}%` : "charging",
     VOLT: (value: string) => `${value} V`,
   };
 
@@ -369,7 +371,7 @@ export const convertUnitToReadbleFormat = (unit: Unit, value: string) => {
 export const convertDateToReadbleFormat = (
   dateString: string,
   format: string,
-  showTime: boolean
+  showTime: boolean,
 ): string => {
   const date = new Date(dateString);
   const options: Intl.DateTimeFormatOptions = {
@@ -416,7 +418,7 @@ export const createPlaceholderEntities = (amount: number): any[] => {
 };
 
 export const findPanelMetadata = (
-  obj: any,
+  obj: Record<string, Column>,
   parentIsEditable?: boolean,
 ): PanelMetaData[] => {
   const results: PanelMetaData[] = [];
