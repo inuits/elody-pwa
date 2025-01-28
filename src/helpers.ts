4b548050beceaa9cd2205a7942e3c4c70d4c44ac
Jonas Verschueren <jonasv@inuits.eu>
2025-01-28 09:50:55 +0100

    style: Change download template csv to info label on top of form + add option for link to documentation
    Refs: #137876

--- src/helpers.ts
+++ src/helpers.ts
@@ -355,8 +355,7 @@ export const convertUnitToReadbleFormat = (unit: Unit, value: string) => {
     SECONDS: (value: string) => `${value} s`,
     COORDINATES: (value: string) =>
       `${(value as any).longitude}, ${(value as any).latitude}`,
-    PERCENT: (value: string) =>
-      Number(value) !== -1 ? `${Number(value) * 100}%` : "charging",
+    PERCENT: (value: string) => Number(value) !== -1 ? `${Number(value) * 100}%` : "charging",
     VOLT: (value: string) => `${value} V`,
   };
 
@@ -371,7 +370,7 @@ export const convertUnitToReadbleFormat = (unit: Unit, value: string) => {
 export const convertDateToReadbleFormat = (
   dateString: string,
   format: string,
-  showTime: boolean,
+  showTime: boolean
 ): string => {
   const date = new Date(dateString);
   const options: Intl.DateTimeFormatOptions = {
@@ -418,7 +417,7 @@ export const createPlaceholderEntities = (amount: number): any[] => {
 };
 
 export const findPanelMetadata = (
-  obj: Record<string, Column>,
+  obj: any,
   parentIsEditable?: boolean,
 ): PanelMetaData[] => {
   const results: PanelMetaData[] = [];
