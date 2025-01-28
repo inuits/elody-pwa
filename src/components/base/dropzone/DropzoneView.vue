81065a5cf204ff0339d24d883102415236f7bbc7
Jonas Verschueren <jonasv@inuits.eu>
2025-01-28 09:50:55 +0100

    style: Change download template csv to info label on top of form + add option for link to documentation
    Refs: #137876

--- src/components/base/dropzone/DropzoneView.vue
+++ src/components/base/dropzone/DropzoneView.vue
@@ -12,29 +12,6 @@
         <div class="text-body">
           {{ $t(dropzoneLabel) }}
         </div>
-        <div v-if="isValidation" @click.stop>
-          <p
-            class="underline text-accent-accent"
-            @click="
-              downloadCsvTemplate(`/${selectedItem}`)
-            "
-          >
-            {{ $t("upload-fields.csv-template-link") }}
-          </p>
-          <select
-            v-if="templateCsvs"
-            v-model="selectedItem"
-            class="inline-block min-w-0 max-w-full pl-2 pr-8 border-none border-b underline text-accent-accent"
-          >
-            <option
-              v-for="csv in templateCsvs"
-              :key="csv"
-              :value="csv"
-            >
-              {{ csv }}
-            </option>
-          </select>
-        </div>
       </div>
     </div>
   </div>
@@ -42,18 +19,9 @@
 
 <script lang="ts" setup>
 import { onMounted, ref } from "vue";
-import useUpload from "@/composables/useUpload";
-import Papa from "papaparse";
-import { downloadCsv } from "@/helpers";
 
-const { files } = useUpload();
 const dropzoneView = ref<HTMLDivElement>();
 
-enum csvHeaders {
-  file_identifier = "file_identifier",
-  filename = "filename",
-  file_source ="file_source",
-}
 
 const props = withDefaults(
   defineProps<{
@@ -62,49 +30,16 @@ const props = withDefaults(
     isValidation: boolean;
     fileCount: number;
     style: string;
-    templateCsvs?: string | undefined;
   }>(),
   {
     style: "",
     isValidation: false,
-    templateCsvs: undefined,
   },
-);
-
-const selectedItem = ref<string | undefined>(
-  props.templateCsvs ? props.templateCsvs[0] : undefined,
 );
 
 const emit = defineEmits<{
   (event: "update:modelValue", modelValue: HTMLDivElement | undefined): void;
 }>();
-
-const downloadCsvTemplate = async (filePath: any) => {
-  const response = await fetch(filePath);
-  const text = await response.text();
-  const results = Papa.parse(text, {
-    header: true,
-    dynamicTyping: true,
-  });
-
-  if (files.value.length > 0) {
-    const header = results.meta.fields as string[];
-    const filenameIndex = header.indexOf(csvHeaders.filename);
-    let columnToAddFilename =
-      filenameIndex === -1 ? csvHeaders.file_identifier : csvHeaders.filename;
-    const type = results.data[0]["type"];
-
-    files.value.forEach((file: any, index: number) => {
-      results.data.push({});
-      results.data[index]["type"] = type;
-      results.data[index][columnToAddFilename] = file.name;
-      if (columnToAddFilename === csvHeaders.file_identifier)
-        results.data[index][csvHeaders.file_source] = "File";
-    });
-  }
-  const csv = Papa.unparse(results.data);
-  downloadCsv(filePath, csv);
-};
 
 onMounted(() => {
   emit("update:modelValue", dropzoneView.value);
