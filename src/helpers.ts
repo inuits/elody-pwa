7f4ed05f38b3db6955da88e5ab7719c59c14e65f
Berend Vandenbussche <berend@inuits.eu>
2025-01-28 13:20:20 +0100

    Chore: Update vite config

--- src/helpers.ts
+++ src/helpers.ts
@@ -15,7 +15,6 @@ import {
   RouteNames,
   GetCustomFormattersSettingsDocument,
   GetCustomTypeUrlMappingDocument,
-  type Column,
   DamsIcons,
   type DropdownOption,
   type Column,
@@ -355,7 +354,8 @@ export const convertUnitToReadbleFormat = (unit: Unit, value: string) => {
     SECONDS: (value: string) => `${value} s`,
     COORDINATES: (value: string) =>
       `${(value as any).longitude}, ${(value as any).latitude}`,
-    PERCENT: (value: string) => Number(value) !== -1 ? `${Number(value) * 100}%` : "charging",
+    PERCENT: (value: string) =>
+      Number(value) !== -1 ? `${Number(value) * 100}%` : "charging",
     VOLT: (value: string) => `${value} V`,
   };
 
@@ -370,7 +370,7 @@ export const convertUnitToReadbleFormat = (unit: Unit, value: string) => {
 export const convertDateToReadbleFormat = (
   dateString: string,
   format: string,
-  showTime: boolean
+  showTime: boolean,
 ): string => {
   const date = new Date(dateString);
   const options: Intl.DateTimeFormatOptions = {
@@ -664,7 +664,9 @@ export const determineDefaultIntialValues = (
   return newInitialData;
 };
 
-export const mapModelValueToDropdownOptions = (values: any[]): DropdownOption[] => {
+export const mapModelValueToDropdownOptions = (
+  values: any[],
+): DropdownOption[] => {
   if (!values) return [];
 
   if (Array.isArray(values)) {
