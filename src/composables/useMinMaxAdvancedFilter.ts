3d40bf07b925dcf106229ad9e1082c04a554eea1
Boris Sobolev <boriss@inuits.eu>
2025-02-03 08:33:23 +0000

    feat: add composable for min max advanced filter, add tests for it
    refs: #134026

---
+++ src/composables/useMinMaxAdvancedFilter.ts
@@ -0,0 +1,122 @@
+import { computed, ref, watch, onMounted } from "vue";
+import { useI18n } from "vue-i18n";
+import {
+  AdvancedFilterTypes,
+  type AdvancedFilterInput,
+} from "@/generated-types/queries";
+import {
+  addCurrentTimeZoneToDateTimeString,
+  isDateTime,
+  extractDate,
+  extractTime,
+} from "@/helpers";
+import type { FilterListItem } from "@/composables/useStateManagement";
+
+export function useMinMaxAdvancedFilter(
+  filter: FilterListItem,
+  emitEvent: (event: string, ...args: any[]) => void,
+) {
+  const { t } = useI18n();
+
+  const inputMin = ref<number | string>();
+  const inputMax = ref<number | string>();
+  const inputTimeMin = ref<number | string | undefined>(undefined);
+  const inputTimeMax = ref<number | string | undefined>(undefined);
+  const force = ref<boolean>(false);
+
+  const determineInputType = computed<"number" | ["date", "time"]>(() => {
+    return filter.advancedFilter.type === AdvancedFilterTypes.Date
+      ? ["date", "time"]
+      : "number";
+  });
+
+  const determinePlaceholder = computed(() => {
+    return filter.advancedFilter.type === AdvancedFilterTypes.Number
+      ? t("filters.matcher-placeholders.number")
+      : t("filters.matcher-placeholders.date");
+  });
+
+  const totalInputMin = computed(() => {
+    if (isNumberType.value) return inputMin.value;
+    return inputMin.value
+      ? `${inputMin.value}T${inputTimeMin.value || "00:00:00"}`
+      : undefined;
+  });
+
+  const totalInputMax = computed(() => {
+    if (isNumberType.value) return inputMax.value;
+    return inputMax.value
+      ? `${inputMax.value}T${inputTimeMax.value || "00:00:00"}`
+      : undefined;
+  });
+
+  const isNumberType = computed(
+    () => filter.advancedFilter.type === AdvancedFilterTypes.Number,
+  );
+
+  const emitNewAdvancedFilterInput = () => {
+    const value =
+      totalInputMin.value || totalInputMax.value
+        ? {
+            min: totalInputMin.value
+              ? isDateTime(totalInputMin.value)
+                ? addCurrentTimeZoneToDateTimeString(totalInputMin.value)
+                : totalInputMin.value
+              : undefined,
+            max: totalInputMax.value
+              ? isDateTime(totalInputMax.value)
+                ? addCurrentTimeZoneToDateTimeString(totalInputMax.value)
+                : totalInputMax.value
+              : undefined,
+            included: true,
+          }
+        : undefined;
+
+    const newAdvancedFilterInput: AdvancedFilterInput = {
+      type: filter.advancedFilter.type,
+      parent_key: filter.advancedFilter.parentKey,
+      key: filter.advancedFilter.key,
+      value,
+      aggregation: filter.advancedFilter.aggregation,
+    };
+
+    if (filter.advancedFilter.lookup) {
+      newAdvancedFilterInput.lookup = {
+        from: filter.advancedFilter.lookup.from,
+        local_field: filter.advancedFilter.lookup.local_field,
+        foreign_field: filter.advancedFilter.lookup.foreign_field,
+        as: filter.advancedFilter.lookup.as,
+      };
+    }
+    emitEvent("newAdvancedFilterInput", newAdvancedFilterInput, force.value);
+    force.value = false;
+  };
+
+  watch(
+    [inputMin, inputTimeMin, inputMax, inputTimeMax],
+    emitNewAdvancedFilterInput,
+  );
+
+  onMounted(() => {
+    inputMin.value = extractDate(filter.inputFromState?.value?.min);
+    inputTimeMin.value =
+      extractTime(filter.inputFromState?.value?.min) || "00:00:00";
+
+    inputMax.value = extractDate(filter.inputFromState?.value?.max);
+
+    inputTimeMax.value = inputMax.value
+      ? extractTime(filter.inputFromState?.value?.max) || "00:00:00"
+      : undefined;
+    force.value = Boolean(filter.inputFromState);
+  });
+
+  return {
+    inputMin,
+    inputMax,
+    inputTimeMin,
+    inputTimeMax,
+    determineInputType,
+    determinePlaceholder,
+    isNumberType,
+  };
+}
