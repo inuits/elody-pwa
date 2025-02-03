c1faf2cabd12847661b664e02f1bbe1dca93fc5d
Boris Sobolev <boriss@inuits.eu>
2025-02-03 08:33:23 +0000

    feat: add composable for min max advanced filter, add tests for it
    refs: #134026

--- src/components/filters/matchers/InBetweenMatcher.vue
+++ src/components/filters/matchers/InBetweenMatcher.vue
@@ -4,19 +4,19 @@
       <span class="text-sm text-text-body">minimum</span>
       <div
         v-if="
-          Array.isArray(determineInputType) && determineInputType?.length === 2
+          Array.isArray(determineInputType) && determineInputType.length === 2
         "
       >
         <BaseInputTextNumberDatetime
           class="mb-2"
-          v-model="min"
+          v-model="inputMin"
           input-style="default"
           :type="determineInputType[0]"
           :placeholder="determinePlaceholder"
         />
         <BaseInputTextNumberDatetime
           v-if="filter.advancedFilter.showTimeForDateFilter"
-          v-model="minTime"
+          v-model="inputTimeMin"
           input-style="default"
           :type="determineInputType[1]"
           :placeholder="determinePlaceholder"
@@ -24,7 +24,7 @@
       </div>
       <div v-else>
         <BaseInputTextNumberDatetime
-          v-model="min"
+          v-model="inputMin"
           input-style="default"
           :type="determineInputType"
           :placeholder="determinePlaceholder"
@@ -35,19 +35,19 @@
       <span class="text-sm text-text-body">maximum</span>
       <div
         v-if="
-          Array.isArray(determineInputType) && determineInputType?.length === 2
+          Array.isArray(determineInputType) && determineInputType.length === 2
         "
       >
         <BaseInputTextNumberDatetime
           class="mb-2"
-          v-model="max"
+          v-model="inputMax"
           input-style="default"
           :type="determineInputType[0]"
           :placeholder="determinePlaceholder"
         />
         <BaseInputTextNumberDatetime
           v-if="filter.advancedFilter.showTimeForDateFilter"
-          v-model="maxTime"
+          v-model="inputTimeMax"
           input-style="default"
           :type="determineInputType[1]"
           :placeholder="determinePlaceholder"
@@ -55,7 +55,7 @@
       </div>
       <div v-else>
         <BaseInputTextNumberDatetime
-          v-model="max"
+          v-model="inputMax"
           input-style="default"
           :type="determineInputType"
           :placeholder="determinePlaceholder"
@@ -67,99 +67,25 @@
 
 <script lang="ts" setup>
 import type { FilterListItem } from "@/composables/useStateManagement";
-import {
-  AdvancedFilterTypes,
-  type AdvancedFilterInput,
-} from "@/generated-types/queries";
 import BaseInputTextNumberDatetime from "@/components/base/BaseInputTextNumberDatetime.vue";
-import {
-  addCurrentTimeZoneToDateTimeString,
-  extractDate,
-  extractTime,
-  isDateTime,
-} from "@/helpers";
-import { computed, defineEmits, onMounted, ref, watch } from "vue";
-import { useI18n } from "vue-i18n";
+import { useMinMaxAdvancedFilter } from "@/composables/useMinMaxAdvancedFilter";
+import type { AdvancedFilterInput } from "@/generated-types/queries";
 
-const props = defineProps<{
-  filter: FilterListItem;
-}>();
-
+const props = defineProps<{ filter: FilterListItem }>();
 const emit = defineEmits<{
   (
     event: "newAdvancedFilterInput",
     advancedFilterInput: AdvancedFilterInput,
-    force: boolean
+    force: boolean,
   ): void;
 }>();
 
-const { t } = useI18n();
-
-const min = ref<number | string>();
-const minTime = ref<number | string | undefined>(undefined);
-const max = ref<number | string>();
-const maxTime = ref<number | string | undefined>(undefined);
-const force = ref<boolean>(false);
-
-const totalMin = computed(() =>
-  minTime.value ? `${min.value}T${minTime.value}` : min.value
-);
-const totalMax = computed(() =>
-  maxTime.value ? `${max.value}T${maxTime.value}` : max.value
-);
-
-const emitNewAdvancedFilterInput = () => {
-  if (min.value && max.value) {
-    const newAdvancedFilterInput: AdvancedFilterInput = {
-      type: props.filter.advancedFilter.type,
-      parent_key: props.filter.advancedFilter.parentKey,
-      key: props.filter.advancedFilter.key,
-      value: {
-        min: isDateTime(totalMin.value)
-          ? addCurrentTimeZoneToDateTimeString(totalMin.value)
-          : totalMin.value,
-        max: isDateTime(totalMax.value)
-          ? addCurrentTimeZoneToDateTimeString(totalMax.value)
-          : totalMax.value,
-      },
-      aggregation: props.filter.advancedFilter.aggregation,
-    };
-    if (props.filter.advancedFilter.lookup)
-      newAdvancedFilterInput.lookup = {
-        from: props.filter.advancedFilter.lookup.from,
-        local_field: props.filter.advancedFilter.lookup.local_field,
-        foreign_field: props.filter.advancedFilter.lookup.foreign_field,
-        as: props.filter.advancedFilter.lookup.as,
-      };
-    emit("newAdvancedFilterInput", newAdvancedFilterInput, force.value);
-    force.value = false;
-  }
-};
-
-const determineInputType = computed<"number" | ["date", "time"]>(() => {
-  if (props.filter.advancedFilter.type === AdvancedFilterTypes.Date)
-    return ["date", "time"];
-  return "number";
-});
-const determinePlaceholder = computed(() => {
-  if (props.filter.advancedFilter.type === AdvancedFilterTypes.Number)
-    return t("filters.matcher-placeholders.number");
-  return t("filters.matcher-placeholders.date");
-});
-
-onMounted(() => {
-  const dateToday = new Date().toISOString().split("T")[0];
-  min.value = extractDate(props.filter.inputFromState?.value?.min);
-  minTime.value =
-    extractTime(props.filter.inputFromState?.value?.min) || "00:00:00";
-  max.value = extractDate(props.filter.inputFromState?.value?.max) || dateToday;
-  maxTime.value =
-    extractTime(props.filter.inputFromState?.value?.max) || "00:00:00";
-  force.value = Boolean(props.filter.inputFromState);
-});
-
-watch(min, () => emitNewAdvancedFilterInput());
-watch(minTime, () => emitNewAdvancedFilterInput());
-watch(max, () => emitNewAdvancedFilterInput());
-watch(maxTime, () => emitNewAdvancedFilterInput());
+const {
+  inputMin,
+  inputTimeMin,
+  inputMax,
+  inputTimeMax,
+  determineInputType,
+  determinePlaceholder,
+} = useMinMaxAdvancedFilter(props.filter, emit);
 </script>
--- src/components/filters/matchers/MinMatcher.vue
+++ src/components/filters/matchers/MinMatcher.vue
@@ -1,17 +1,17 @@
 <template>
   <div
-    v-if="Array.isArray(determineInputType) && determineInputType?.length === 2"
+    v-if="Array.isArray(determineInputType) && determineInputType.length === 2"
   >
     <BaseInputTextNumberDatetime
       class="mb-2"
-      v-model="input"
+      v-model="inputMin"
       input-style="default"
       :type="determineInputType[0]"
       :placeholder="determinePlaceholder"
     />
     <BaseInputTextNumberDatetime
       v-if="filter.advancedFilter.showTimeForDateFilter"
-      v-model="inputTime"
+      v-model="inputTimeMin"
       input-style="default"
       :type="determineInputType[1]"
       :placeholder="determinePlaceholder"
@@ -19,7 +19,7 @@
   </div>
   <div v-else>
     <BaseInputTextNumberDatetime
-      v-model="input"
+      v-model="inputMin"
       input-style="default"
       :type="determineInputType"
       :placeholder="determinePlaceholder"
@@ -29,90 +29,19 @@
 
 <script lang="ts" setup>
 import type { FilterListItem } from "@/composables/useStateManagement";
-import {
-  AdvancedFilterTypes,
-  type AdvancedFilterInput,
-} from "@/generated-types/queries";
+import { type AdvancedFilterInput } from "@/generated-types/queries";
 import BaseInputTextNumberDatetime from "@/components/base/BaseInputTextNumberDatetime.vue";
-import {
-  addCurrentTimeZoneToDateTimeString,
-  extractDate,
-  extractTime,
-  isDateTime,
-} from "@/helpers";
-import { computed, defineEmits, onMounted, ref, watch } from "vue";
-import { useI18n } from "vue-i18n";
+import { useMinMaxAdvancedFilter } from "@/composables/useMinMaxAdvancedFilter";
 
-const props = defineProps<{
-  filter: FilterListItem;
-}>();
-
+const props = defineProps<{ filter: FilterListItem }>();
 const emit = defineEmits<{
   (
     event: "newAdvancedFilterInput",
     advancedFilterInput: AdvancedFilterInput,
-    force: boolean
+    force: boolean,
   ): void;
 }>();
 
-const { t } = useI18n();
-
-const input = ref<number | string>();
-const inputTime = ref<number | string | undefined>(undefined);
-const totalInput = computed(() =>
-  inputTime.value
-    ? input.value
-      ? `${input.value}T${inputTime.value}`
-      : undefined
-    : input.value
-);
-
-const force = ref<boolean>(false);
-const determineInputType = computed<"number" | ["date", "time"]>(() => {
-  if (props.filter.advancedFilter.type === AdvancedFilterTypes.Date)
-    return ["date", "time"];
-  return "number";
-});
-const determinePlaceholder = computed(() => {
-  if (props.filter.advancedFilter.type === AdvancedFilterTypes.Number)
-    return t("filters.matcher-placeholders.number");
-  return t("filters.matcher-placeholders.date");
-});
-
-onMounted(() => {
-  input.value = extractDate(props.filter.inputFromState?.value?.min);
-  inputTime.value =
-    extractTime(props.filter.inputFromState?.value?.min) || "00:00:00";
-  force.value = Boolean(props.filter.inputFromState);
-  force.value = Boolean(props.filter.inputFromState);
-});
-
-const emitNewAdvancedFilterInput = () => {
-  const newAdvancedFilterInput: AdvancedFilterInput = {
-    type: props.filter.advancedFilter.type,
-    parent_key: props.filter.advancedFilter.parentKey,
-    key: props.filter.advancedFilter.key,
-    value: totalInput.value
-      ? {
-          min: isDateTime(totalInput.value)
-            ? addCurrentTimeZoneToDateTimeString(totalInput.value)
-            : totalInput.value,
-          included: true,
-        }
-      : undefined,
-    aggregation: props.filter.advancedFilter.aggregation,
-  };
-  if (props.filter.advancedFilter.lookup)
-    newAdvancedFilterInput.lookup = {
-      from: props.filter.advancedFilter.lookup.from,
-      local_field: props.filter.advancedFilter.lookup.local_field,
-      foreign_field: props.filter.advancedFilter.lookup.foreign_field,
-      as: props.filter.advancedFilter.lookup.as,
-    };
-  emit("newAdvancedFilterInput", newAdvancedFilterInput, force.value);
-  force.value = false;
-};
-
-watch(input, () => emitNewAdvancedFilterInput());
-watch(inputTime, () => emitNewAdvancedFilterInput());
+const { inputMin, inputTimeMin, determineInputType, determinePlaceholder } =
+  useMinMaxAdvancedFilter(props.filter, emit);
 </script>
