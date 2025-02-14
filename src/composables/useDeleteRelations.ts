394b7c14fd5ed58a1fca6fb098d6fef19ec82411
Berend Vandenbussche <berend@inuits.eu>
2025-02-14 16:37:30 +0100

    Chore: Make useDeleteRelations composable usable outside setup scope
    Refs #137710

--- src/composables/useDeleteRelations.ts
+++ src/composables/useDeleteRelations.ts
@@ -1,31 +1,41 @@
-import { EditStatus, TypeModals, type BaseRelationValuesInput, type Entity } from "@/generated-types/queries";
+import {
+  EditStatus,
+  TypeModals,
+  type BaseRelationValuesInput,
+  type Entity,
+} from "@/generated-types/queries";
 import { useFormHelper } from "@/composables/useFormHelper";
 import useEditMode from "@/composables/useEdit";
-import { useMutation } from "@vue/apollo-composable";
 import {
   type MutateEntityValuesMutation,
-  type MutateEntityValuesMutationVariables,
-  MutateEntityValuesDocument,
   Collection,
 } from "@/generated-types/queries";
 import type { FormContext } from "vee-validate";
-import { useI18n } from "vue-i18n";
 import { useBaseModal } from "@/composables/useBaseModal";
-import { NotificationType, useNotification } from "@/components/base/BaseNotification.vue";
-import { type Context, type InBulkProcessableItem, useBulkOperations } from "@/composables/useBulkOperations";
+import {
+  NotificationType,
+  useNotification,
+} from "@/components/base/BaseNotification.vue";
+import {
+  type Context,
+  type InBulkProcessableItem,
+  useBulkOperations,
+} from "@/composables/useBulkOperations";
+import { apolloClient } from "@/main";
 
 type SelectedItem = { key: string } | InBulkProcessableItem;
 
 export function useDeleteRelations() {
-  const { findRelation, getRelationsBasedOnType, getForm, parseFormValuesToFormInput } = useFormHelper();
+  const {
+    findRelation,
+    getRelationsBasedOnType,
+    getForm,
+    parseFormValuesToFormInput,
+  } = useFormHelper();
   const { save, disableEditMode } = useEditMode();
   const { closeModal } = useBaseModal();
   const { createNotification } = useNotification();
-  const { t } = useI18n();
   const { dequeueItemForBulkProcessing } = useBulkOperations();
-  const { mutate: mutateEntityValues } = useMutation<MutateEntityValuesMutation, MutateEntityValuesMutationVariables>(
-    MutateEntityValuesDocument,
-  );
 
   const deleteRelations = async (
     entityId: string,
@@ -36,7 +46,10 @@ export function useDeleteRelations() {
     const form = getForm(entityId) as FormContext;
     if (!form) return;
 
-    let relations: BaseRelationValuesInput[] = getRelationsBasedOnType(entityId, relationType);
+    let relations: BaseRelationValuesInput[] = getRelationsBasedOnType(
+      entityId,
+      relationType,
+    );
     selectedItems.forEach((item) => {
       const itemKey = "key" in item ? item.key : item.id;
       const relation = findRelation(itemKey, relationType, entityId);
@@ -54,11 +67,15 @@ export function useDeleteRelations() {
     await save();
   };
 
-  const submit = async (entityId: string, collection: Collection, modalType?: TypeModals) => {
+  const submit = async (
+    entityId: string,
+    collection: Collection,
+    modalType?: TypeModals,
+  ) => {
     const form = getForm(entityId) as FormContext;
     if (!form) return;
 
-    const result = await mutateEntityValues({
+    const result = await apolloClient.mutate(MutateEntityValuesMutation, {
       id: entityId,
       formInput: parseFormValuesToFormInput(entityId, form.values, true),
       collection,
@@ -86,8 +103,8 @@ export function useDeleteRelations() {
     createNotification({
       displayTime: 10,
       type: NotificationType.default,
-      title: t("notifications.success.entityUpdated.title"),
-      description: t("notifications.success.entityUpdated.description"),
+      title: "notifications.success.entityUpdated.title",
+      description: "notifications.success.entityUpdated.description",
       shown: true,
     });
 
