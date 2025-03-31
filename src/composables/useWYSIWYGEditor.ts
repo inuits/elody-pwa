f37d13ab71b5ae8439c3e375de7762f274d6b2c4
Berend Vandenbussche <berend@inuits.eu>
2025-03-31 11:36:15 +0200

    Refactor: Update dynamic imports so vite can handle them

--- src/composables/useWYSIWYGEditor.ts
+++ src/composables/useWYSIWYGEditor.ts
@@ -2,7 +2,7 @@ import { WysiwygExtensions } from "@/generated-types/queries";
 
 type ExtensionInfo = {
   importName: string;
-  from: string;
+  from: () => any;
   isNamedExport?: boolean;
   configuration?: WysiwygExtensions[];
 };
@@ -17,20 +17,20 @@ export const useWYSIWYGEditor = (): {
       // Base-extensions if you don't want to use the starterkit
       [WysiwygExtensions.Doc]: {
         importName: "Document",
-        from: "node_modules/@tiptap/extension-document",
+        from: () => import("@tiptap/extension-document"),
       },
       [WysiwygExtensions.Paragraph]: {
         importName: "Paragraph",
-        from: "node_modules/@tiptap/extension-paragraph",
+        from: () => import("@tiptap/extension-paragraph"),
       },
       [WysiwygExtensions.Text]: {
         importName: "Text",
-        from: "node_modules/@tiptap/extension-text",
+        from: () => import("@tiptap/extension-text"),
       },
       // End of base-extensions
       [WysiwygExtensions.Color]: {
         importName: "Color",
-        from: "node_modules/@tiptap/extension-color",
+        from: () => import("@tiptap/extension-color"),
         isNamedExport: true,
         configuration: [
           WysiwygExtensions.TextStyle,
@@ -39,24 +39,24 @@ export const useWYSIWYGEditor = (): {
       },
       [WysiwygExtensions.ListItem]: {
         importName: "ListItem",
-        from: "node_modules/@tiptap/extension-list-item",
+        from: () => import("@tiptap/extension-list-item"),
       },
       [WysiwygExtensions.StarterKit]: {
         importName: "StarterKit",
-        from: "node_modules/@tiptap/starter-kit",
+        from: () => import("@tiptap/starter-kit"),
       },
       [WysiwygExtensions.TextStyle]: {
         importName: "TextStyle",
-        from: "node_modules/@tiptap/extension-text-style",
+        from: () => import("@tiptap/extension-text-style"),
         configuration: [WysiwygExtensions.ListItem],
       },
       [WysiwygExtensions.Italic]: {
         importName: "Italic",
-        from: "node_modules/@tiptap/extension-italic",
+        from: () => import("@tiptap/extension-italic"),
       },
       [WysiwygExtensions.Bold]: {
         importName: "Bold",
-        from: "node_modules/@tiptap/extension-bold",
+        from: () => import("@tiptap/extension-bold"),
       },
     };
 
@@ -68,7 +68,7 @@ export const useWYSIWYGEditor = (): {
         const { from, isNamedExport, importName } =
           editorExtensionImportMapping[extension];
         try {
-          const module = await import(`../../${from}`);
+          const module = await from();
           return isNamedExport ? module[importName] : module.default;
         } catch (e) {
           console.error(`Error importing ${extension}:`, error);
