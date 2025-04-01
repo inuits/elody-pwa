f0497662c7de52769f9ac63e7650fad336ce435a
Boris Sobolev <boriss@inuits.eu>
2025-04-01 12:44:08 +0000

    feat: add handler for alternative routes
    refs: #138754

--- src/routerNavigationGuards.ts
+++ src/routerNavigationGuards.ts
@@ -9,6 +9,7 @@ import { getFromExpressEndpoint } from "@/helpers";
 import { ElodyServices } from "@/generated-types/queries";
 import { useServiceVersionManager } from "@/composables/useServiceVersionManager";
 import { getChildrenOfHomeRoutes, requiresAuthForEntity } from "@/helpers";
+import { usePermissions } from "@/composables/usePermissions";
 
 const handleRequiredAuthentication = (router: Router) => {
   if (
@@ -55,6 +56,42 @@ const handleRequiresAuthFromOverviewPage = (
   }
 };
 
+const handleAlternativeRoutes = async (
+  to: RouteLocationNormalized,
+  next: NavigationGuardNext,
+) => {
+  const toData = to.matched[to.matched.length - 1];
+  const toMeta = toData?.meta || {};
+
+  const permission: string[] | undefined = toMeta.can as string[] | undefined;
+
+  if (!permission) {
+    return next();
+  }
+
+  const { fetchAdvancedPermission } = usePermissions();
+  const isPermitted = await fetchAdvancedPermission(permission);
+
+  const alternativeRoutes: { [role: string]: string } | undefined =
+    toMeta.alternativeRoutes as { [role: string]: string } | undefined;
+
+  if (isPermitted || !alternativeRoutes) {
+    return next();
+  }
+
+  const userRole = auth.user?.role || "fallback";
+  const alternativeRoute = alternativeRoutes[userRole];
+
+  if (
+    alternativeRoute === to.fullPath ||
+    to.fullPath.includes(alternativeRoute)
+  ) {
+    return next();
+  }
+
+  return next(alternativeRoute);
+};
+
 const checkForNewVersion = async (): Promise<void> => {
   const { setVersion } = useServiceVersionManager();
   const version = await getFromExpressEndpoint("version");
@@ -69,8 +106,9 @@ export const addRouterNavigationGuards = (router: Router, config: any) => {
     checkForNewVersion();
   });
 
-  router.beforeEach((to, from, next) => {
-    handleRequiresAuthFromOverviewPage(to, config, next, router);
+  router.beforeEach(async (to, from, next) => {
+    await handleAlternativeRoutes(to, next);
+    await handleRequiresAuthFromOverviewPage(to, config, next, router);
     handleTenantParameterInUrl(to, next);
   });
 };
