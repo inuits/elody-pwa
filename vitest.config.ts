08fff16b862b9aebaf8a35152a1c8000a24a9cd4
Berend Vandenbussche <berend@inuits.eu>
2025-05-13 09:01:35 +0200

    Fix: Add vue plugin

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from "node:url";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
