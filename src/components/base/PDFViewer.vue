ee41e6ae4c81d36c3b79aef9cbfd333a55c8601c
Berend Vandenbussche <berend@inuits.eu>
2025-10-16 14:28:53 +0200

    Chore: Change non-typo to typo (intialValues) :D

--- src/components/base/PDFViewer.vue
+++ src/components/base/PDFViewer.vue
@@ -241,7 +241,8 @@ async function initialRender(): Promise<void> {
   }
 
   try {
-    const filePath = props.source.initialValues?.original_file_location;
+    const filePath = props.source.intialValues?.original_file_location;
+    console.log(filePath);
     if (!filePath) {
       error.value =
         "Mediafile has no 'original_file_location', unable to locate source file";
