2af7ce63ccbeeb4e3916372c210af04caa1fc897
Berend Vandenbussche <berend@inuits.eu>
2025-12-22 13:56:15 +0100

    Test: Add test for relations
    Refs #151511

--- src/components/metadata/tests/useMetadataWrapper.test.ts
+++ src/components/metadata/tests/useMetadataWrapper.test.ts
@@ -14,10 +14,11 @@ vi.mock("@/main", () => ({
 
 import { useMetadataWrapper } from "../useMetadataWrapper";
 import { useForm, defineRule } from "vee-validate";
-import { defineComponent, nextTick } from "vue";
+import { defineComponent, h, nextTick, type Ref } from "vue";
 import { mount } from "@vue/test-utils";
+import { useMetadataWrapperDropdownOptions } from "@/components/metadata/useMetadataWrapperDropdownOptions";
 
-const metadataWrapperProps = {
+const basicMetadataWrapperProps = {
   formId: "M-KYY3IE440Q",
   metadata: {
     key: "title",
@@ -31,30 +32,174 @@ const metadataWrapperProps = {
   showErrors: false,
 };
 
-describe("useMetadataWrapper", () => {
-  it("should initialize the field in veevalidate", async () => {
-    let capturedFieldValueProxy: any;
+const relationMetadataWrapperProps = {
+  formId: "W-YA415JLI41",
+  metadata: {
+    key: "refTargetAudiences",
+    label: "metadata.labels.target-audience",
+    value: "volwassenen",
+    inputField: {
+      type: "dropdownMultiselectRelations",
+      options: [],
+      relationType: "refTargetAudiences",
+      fromRelationType: "",
+      canCreateEntityFromOption: true,
+      metadataKeyToCreateEntityFromOption: "title",
+      advancedFilterInputForRetrievingOptions: [
+        {
+          type: "text",
+          key: ["vlacc:1|properties.title.value"],
+          value: "*",
+          match_exact: false,
+          item_types: null,
+          __typename: "AdvancedFilterInputType",
+        },
+        {
+          type: "type",
+          key: null,
+          value: "target_audience",
+          match_exact: null,
+          item_types: null,
+          __typename: "AdvancedFilterInputType",
+        },
+      ],
+      advancedFilterInputForRetrievingRelatedOptions: [],
+      advancedFilterInputForRetrievingAllOptions: [],
+      advancedFilterInputForSearchingOptions: {
+        type: "text",
+        key: null,
+        value: "*",
+        match_exact: null,
+        item_types: null,
+        __typename: "AdvancedFilterInputType",
+      },
+      fileTypes: null,
+      maxFileSize: null,
+      maxAmountOfFiles: null,
+      uploadMultiple: null,
+      isMetadataField: false,
+      relationFilter: {
+        lookup: null,
+        type: "selection",
+        key: ["vlacc:1|identifiers"],
+        value: "$relationValues.refTargetAudiences.key",
+        match_exact: true,
+        item_types: ["target_audience"],
+        __typename: "AdvancedFilterInputType",
+      },
+      dependsOn: "",
+      multiple: false,
+      lineClamp: null,
+      entityType: "target_audience",
+      hasVirtualKeyboard: false,
+      autoSelectable: false,
+      autoAllSelectable: null,
+      __typename: "InputField",
+      validation: {
+        value: ["has_required_relation"],
+        customValue: null,
+        fastValidationMessage: null,
+        required_if: null,
+        available_if: null,
+        has_required_relation: {
+          relationType: "refTargetAudiences",
+          amount: 1,
+          exact: null,
+          __typename: "RequiredRelationValidation",
+        },
+        has_one_of_required_relations: null,
+        has_one_of_required_metadata: null,
+        regex: null,
+        __typename: "Validation",
+      },
+    },
+    __typename: "PanelMetaData",
+  },
+  isEdit: false,
+  baseLibraryMode: "normalBaseLibrary",
+  formFlow: "edit",
+  showErrors: false,
+};
 
-    const TestComponent = defineComponent({
+describe("useMetadataWrapper", () => {
+  it("should initialize a basic field in veevalidate succesfully", async () => {
+    let capturedFieldValueProxy: Ref<any>;
+    let capturedFieldLabel: Ref<string>;
+    let capturedFieldKey: Ref<string>;
+
+    const basicComponent = defineComponent({
       setup() {
         useForm();
         defineRule("no_xss", () => true);
 
-        const { fieldValueProxy } = useMetadataWrapper(
-          metadataWrapperProps,
+        const { fieldValueProxy, fieldLabel, fieldKey } = useMetadataWrapper(
+          basicMetadataWrapperProps,
           () => undefined,
         );
 
         capturedFieldValueProxy = fieldValueProxy;
+        capturedFieldLabel = fieldLabel;
+        capturedFieldKey = fieldKey;
 
-        return () => null;
+        return () => h("div");
       },
     });
 
-    mount(TestComponent);
+    mount(basicComponent);
 
     await nextTick();
 
-    expect(capturedFieldValueProxy.value).toBe("Willem Frederik Hermans");
+    expect(capturedFieldValueProxy!.value).toBe("Willem Frederik Hermans");
+    expect(capturedFieldLabel!.value).toBe(
+      "metadata.labels.headtitle-manifestation",
+    );
+    expect(capturedFieldKey!.value).toBe("intialValues.title");
+  });
+
+  it("Should initialize a relation field in veevalidate succesfully", async () => {
+    let capturedFieldValueProxy: Ref<any>;
+    let capturedFieldLabel: Ref<string>;
+    let capturedFieldKey: Ref<string>;
+    let capturedFieldType: Ref<string>;
+    let capturedFiltersForRetrievingOptions: Ref<any>;
+    let capturedFiltersForRetrievingRelatedOptions: Ref<any>;
+
+    const relationComponent = defineComponent({
+      setup() {
+        useForm();
+        defineRule("no_xss", () => true);
+        defineRule("has_required_relation", () => true);
+        defineRule("required", () => true);
+
+        const { fieldValueProxy, fieldLabel, fieldKey, fieldType } =
+          useMetadataWrapper(relationMetadataWrapperProps, () => undefined);
+
+        const {
+          filtersForRetrievingOptions,
+          filtersForRetrievingRelatedOptions,
+        } = useMetadataWrapperDropdownOptions(relationMetadataWrapperProps);
+
+        capturedFieldValueProxy = fieldValueProxy;
+        capturedFieldLabel = fieldLabel;
+        capturedFieldKey = fieldKey;
+        capturedFieldType = fieldType;
+        capturedFiltersForRetrievingOptions = filtersForRetrievingOptions;
+        capturedFiltersForRetrievingRelatedOptions =
+          filtersForRetrievingRelatedOptions;
+
+        return () => h("div");
+      },
+    });
+
+    mount(relationComponent);
+
+    await nextTick();
+
+    expect(capturedFieldValueProxy!.value).toBe("volwassenen");
+    expect(capturedFieldLabel!.value).toBe("metadata.labels.target-audience");
+    expect(capturedFieldKey!.value).toBe("intialValues.refTargetAudiences");
+    expect(capturedFieldType!.value).toBe("dropdownMultiselectRelations");
+    expect(capturedFiltersForRetrievingOptions!.value).toBeTruthy();
+    expect(capturedFiltersForRetrievingRelatedOptions!.value).toBeTruthy();
   });
 });
