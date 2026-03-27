10bbf311877d2437dcc3266e4e7a0e51d49c3ced
Jonas Verschueren <jonasv@inuits.eu>
2026-03-27 12:08:29 +0100

    fix: Fix bug in form for when metadata index is not found during parsing repeatable values

--- src/composables/useFormHelper.ts
+++ src/composables/useFormHelper.ts
@@ -460,6 +460,7 @@ const useFormHelper = () => {
       const associatedMetadataItemIndex: number = metadata.findIndex(
         (metadataItem: MetadataValuesInput) => metadataItem.key === panelKey,
       );
+      if (!associatedMetadataItemIndex || associatedMetadataItemIndex === -1) return;
       metadata[associatedMetadataItemIndex].value = [
         ...Object.values(repeatableMetadataValues[panelKey]),
       ];
