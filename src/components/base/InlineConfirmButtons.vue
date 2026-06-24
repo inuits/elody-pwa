299dc89a0354f1aa1320b2ac811446131e40aa6b
Berend Vandenbussche <berend@inuits.eu>
2026-06-24 12:26:00 +0200

    Chore: Add InlineConfirmButtons component for new confirm modal handling

---
+++ src/components/base/InlineConfirmButtons.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="flex justify-between pt-8">
+    <BaseButtonNew
+      :label="confirmLabel"
+      :button-style="confirmButtonStyle ?? 'redDefault'"
+      :button-size="confirmButtonSize ?? 'small'"
+      @click="emit('confirm')"
+    />
+    <BaseButtonNew
+      :label="cancelLabel"
+      :button-style="cancelButtonStyle ?? 'default'"
+      :button-size="cancelButtonSize ?? 'small'"
+      @click="emit('cancel')"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { ButtonSize, ButtonStyle } from "@/components/base/BaseButtonNew.vue";
+import BaseButtonNew from "@/components/base/BaseButtonNew.vue";
+
+defineProps<{
+  confirmLabel: string;
+  cancelLabel: string;
+  confirmButtonStyle?: ButtonStyle;
+  cancelButtonStyle?: ButtonStyle;
+  confirmButtonSize?: ButtonSize;
+  cancelButtonSize?: ButtonSize;
+}>();
+
+const emit = defineEmits<{
+  (e: "confirm"): void;
+  (e: "cancel"): void;
+}>();
+</script>
